13a2a984f244c4af7e219c5d618efcf1707306c3
Stukova <stukova.o@gmail.com>
2019-11-29 14:55:37 +0500

    Configure rollup for multiple bundles

--- packages/vis/rollup.config.js
+++ packages/vis/rollup.config.js
@@ -1,53 +1,66 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-
-import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
+import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-// import json from 'rollup-plugin-json'
-// import ignore from 'rollup-plugin-ignore'
-// import includePaths from 'rollup-plugin-includepaths'
-// import globals from 'rollup-plugin-node-globals'
-// import requireContext from 'rollup-plugin-require-context'
-// import yaml from 'rollup-plugin-yaml'
+import { terser } from "rollup-plugin-terser"
 
-const libraryName = '@volterra/vis'
 const externals = [
   ...Object.keys(pkg.dependencies || {}),
   ...Object.keys(pkg.peerDependencies || {})
 ]
 
-export default [{
+const config = {
   input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      name: libraryName,
-      format: 'umd',
-      globals: externals,
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      externals,
-      sourcemap: true
-    }
-  ],
   external: externals,
+  output: {
+    sourcemap: true
+  },
   plugins: [
     commonjs(),
     resolve(),
-    typescript({
-      typescript: require('typescript')
-    })
-    // includePaths(),
-    // ignore([/^\.\/locale$/, /moment$/]),
-    // json(),
-    // yaml({
-    //   exclude: 'node_modules/**'
-    // }),
-    // requireContext(),
+    typescript(),
+  ],
+};
 
-  ]
-}]
+export default [
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: 'lib/volterra-vis.js',
+      name: pkg.name,
+      format: 'umd',
+      globals: externals,
+    },
+    plugins: [
+      ...config.plugins,
+    ]
+  },
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: 'lib/volterra-vis.min.js',
+      name: pkg.name,
+      format: 'umd',
+      globals: externals,
+    },
+    plugins: [
+      ...config.plugins,
+      terser()
+    ]
+  },
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: 'lib/volterra-vis.es.js',
+      format: 'es',
+      externals,
+    },
+    plugins: [
+      ...config.plugins,
+    ]
+  },
+]
