95ac188aa9068f2c5d7078fc5005cee84f639a63
Nikita Rokotyan <nrokotyan@gmail.com>
2020-01-13 20:28:02 +0500

    Package: Bundling configuration update

--- packages/vis/rollup.config.js
+++ packages/vis/rollup.config.js
@@ -1,66 +1,39 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-import pkg from './package.json'
 import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
 const externals = [
   ...Object.keys(pkg.dependencies || {}),
   ...Object.keys(pkg.peerDependencies || {}),
 ]
 
-const config = {
+export default {
   input: 'src/index.ts',
   external: externals,
-  output: {
+  output: [{
+    file: pkg.main,
     sourcemap: true,
-  },
+    globals: externals,
+    format: 'cjs',
+  }, {
+    file: pkg.module,
+    sourcemap: true,
+    format: 'esm',
+  }, {
+    file: pkg.browser,
+    format: 'iife',
+    globals: externals,
+    name: 'vv',
+    plugins: [terser()],
+  }],
   plugins: [
     commonjs(),
     resolve(),
-    typescript(),
+    typescript({
+      typescript: require('typescript'),
+    }),
   ],
 }
-
-export default [
-  {
-    ...config,
-    output: {
-      ...config.output,
-      file: 'lib/volterra-vis.js',
-      name: pkg.name,
-      format: 'umd',
-      globals: externals,
-    },
-    plugins: [
-      ...config.plugins,
-    ],
-  },
-  {
-    ...config,
-    output: {
-      ...config.output,
-      file: 'lib/volterra-vis.min.js',
-      name: pkg.name,
-      format: 'umd',
-      globals: externals,
-    },
-    plugins: [
-      ...config.plugins,
-      terser(),
-    ],
-  },
-  {
-    ...config,
-    output: {
-      ...config.output,
-      file: 'lib/volterra-vis.es.js',
-      format: 'es',
-      externals,
-    },
-    plugins: [
-      ...config.plugins,
-    ],
-  },
-]
