11bbd903b562ed21aac43afd0d735e82c608093d
Nikita Rokotyan <nrokotyan@gmail.com>
2020-01-28 16:27:01 +0500

    Utils | Color: getColor bug fix

--- packages/vis/src/utils/color.ts
+++ packages/vis/src/utils/color.ts
@@ -8,5 +8,5 @@ import { getValue } from 'utils/data'
 /** Retrieves color from data if available, fallbacks to a css variable with a specified suffix or to a null value if the suffix is not passed */
 export function getColor (d: any, accessor: any, cssVarSuffix?: number | string): string {
   const value = getValue(d, accessor)
-  return (value || cssVarSuffix ? `var(${getCSSVarName(cssVarSuffix)})` : null) as string
+  return (value || (cssVarSuffix ? `var(${getCSSVarName(cssVarSuffix)})` : null)) as string
 }
