eb47db6b5e54b2ed1f4d00720cd140dc74536972
Nikita Rokotyan <nrokotyan@gmail.com>
2020-02-13 10:24:28 +0300

    Core | Colors: New color set

--- packages/vis/src/utils/color.ts
+++ packages/vis/src/utils/color.ts
@@ -3,10 +3,10 @@
 import { getCSSVarName } from 'styles/colors'
 
 // Utils
-import { getValue } from 'utils/data'
+import { getValue, isNumber } from 'utils/data'
 
 /** Retrieves color from data if available, fallbacks to a css variable with a specified suffix or to a null value if the suffix is not passed */
 export function getColor (d: any, accessor: any, cssVarSuffix?: number | string): string {
   const value = getValue(d, accessor)
-  return (value || (cssVarSuffix ? `var(${getCSSVarName(cssVarSuffix)})` : null)) as string
+  return (value || (isNumber(cssVarSuffix) ? `var(${getCSSVarName(cssVarSuffix)})` : null)) as string
 }
