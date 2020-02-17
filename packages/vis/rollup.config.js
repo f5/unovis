aa8b0108ff948b81edfbe712be31147e6b055ff3
Nikita Rokotyan <nrokotyan@gmail.com>
2020-02-17 11:01:21 +0500

    Package: Proper module bundling
    • Defining externals and globals properly
    • Removing browser bundle to avoid webpack imprting it by default
    • Removing ts3.7-fix

--- packages/vis/rollup.config.js
+++ packages/vis/rollup.config.js
@@ -3,15 +3,33 @@ import typescript from 'rollup-plugin-typescript2'
 import transformPaths from '@zerollup/ts-transform-paths'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 import modules from './rollup.modules.json'
 
-import { tsFix } from './ts-fix'
+const d3Libs = ['d3-array', 'd3-axis', 'd3-brush', 'd3-chord', 'd3-collection', 'd3-color',
+  'd3-contour', 'd3-dispatch', 'd3-drag', 'd3-dsv', 'd3-ease', 'd3-fetch', 'd3-force',
+  'd3-format', 'd3-geo', 'd3-hierarchy', 'd3-interpolate', 'd3-path',
+  'd3-polygon', 'd3-quadtree', 'd3-random', 'd3-sankey', 'd3-scale', 'd3-scale-chromatic',
+  'd3-selection', 'd3-shape', 'd3-time', 'd3-time-format', 'd3-timer', 'd3-transition',
+  'd3-voronoi', 'd3-zoom']
+
+const lodashLibs = ['lodash/isUndefined', 'lodash/isArray', 'lodash/isEmpty', 'lodash/isEqual',
+  'lodash/isNil', 'lodash/cloneDeep', 'lodash/throttle', 'lodash/each', 'lodash/filter',
+  'lodash/get', 'lodash/without', 'lodash/find', 'lodash/isString', 'lodash/isObject',
+  'lodash/isFunction', 'lodash/isNumber', 'lodash/merge', 'lodash/isPlainObject', 'lodash/flatten']
+
+const globals = {}
+d3Libs.reduce((acc, name) => { acc[name] = 'd3'; return acc }, globals)
+lodashLibs.reduce((acc, name) => {
+  acc[name] = `_.${name.replace('lodash/', '')}`
+  return acc
+}, globals)
 
 const externals = [
   ...Object.keys(pkg.dependencies || {}),
   ...Object.keys(pkg.peerDependencies || {}),
+  ...d3Libs,
+  ...lodashLibs,
 ]
 
 const plugins = [
@@ -21,12 +39,6 @@ const plugins = [
     typescript: require('typescript'),
     transformers: [service => transformPaths(service.getProgram())],
   }),
-  {
-    name: 'ts3.7-fix',
-    writeBundle: () => {
-      tsFix()
-    },
-  },
 ]
 
 export default [{
@@ -35,18 +47,12 @@ export default [{
   output: [{
     file: pkg.main,
     sourcemap: true,
-    globals: externals,
+    globals,
     format: 'cjs',
   }, {
     file: pkg.module,
     sourcemap: true,
     format: 'esm',
-  }, {
-    file: pkg.browser,
-    format: 'iife',
-    globals: externals,
-    name: 'vv',
-    plugins: [terser()],
   }],
   plugins,
 },
--- packages/vis/src/index.ts
+++ packages/vis/src/index.ts
@@ -1,10 +1,5 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
 
-import * as containers from './containers'
-import * as components from './components'
-import * as types from './types'
-
-export { containers, components, types }
-
-export * from './components'
 export * from './containers'
+export * from './components'
+export * from './types'
