9213d45c12fde1293a86422b5bdb64534d822872
Nikita Rokotyan <nrokotyan@gmail.com>
2020-02-19 15:42:00 +0500

    Component | Map: Selection Ring fixes

--- packages/vis/src/components/map/modules/selectionRing.ts
+++ packages/vis/src/components/map/modules/selectionRing.ts
@@ -1,7 +1,15 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
+import L from 'leaflet'
+
+// Types
+import { Point } from 'types/map'
+
 // Utils
 import { find } from 'utils/data'
 import { getPointPos } from './utils'
+
+// Config
+import { MapConfigInterface } from '../config'
 
 import * as s from '../style'
 
@@ -10,7 +18,7 @@ export function createNodeSelectionRing (selection): void {
   selection.append('path').attr('class', s.nodeSelection)
 }
 
-export function updateNodeSelectionRing (selection, selectedNode, pointData, config, leafletMap): void {
+export function updateNodeSelectionRing<T> (selection, selectedNode: Point, pointData: Point[], config: MapConfigInterface<T>, leafletMap: L.Map): void {
   selection.attr('class', s.nodeSelectionRing)
   const nodeSelection = selection.select(`.${s.nodeSelection}`)
   if (selectedNode) {
@@ -29,7 +37,7 @@ export function updateNodeSelectionRing (selection, selectedNode, pointData, con
       .style('stroke-width', 1)
       .style('stroke', d => {
         const node = foundNode || selectedNode
-        return node?.stroke || config.statusStyle?.[node.properties.status]?.color
+        return node?.stroke || config.statusMap?.[node.properties.status]?.color
       })
   } else {
     nodeSelection.classed('active', false)
