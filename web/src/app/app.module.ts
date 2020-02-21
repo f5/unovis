23fc41659954d5b9362e4ce78cb238287f60e80f
Nikita Rokotyan <nrokotyan@gmail.com>
2020-02-21 16:29:44 +0500

    Demo | Single: Removing Single Chart demo examples

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-import { SingleComponent } from 'examples/single/single.component'
 import { CompositeComponent } from 'examples/composite/composite.component'
 import { SankeyComponent } from 'examples/sankey/sankey.component'
 import { WrapperUsageExampleComponent } from 'examples/wrapper-usage-example/wrapper-usage-example.component'
@@ -17,7 +16,6 @@ import { FlowLegendComponent } from './components/flow-legend/flow-legend.compon
 import { XYChartComponent } from './components/xychart/xychart.component'
 
 const appRoutes: Routes = [
-  { path: 'single', component: SingleComponent },
   { path: 'composite', component: CompositeComponent },
   { path: 'sankey', component: SankeyComponent },
   { path: 'wrapper-usage-example', component: WrapperUsageExampleComponent },
@@ -28,7 +26,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    SingleComponent,
     CompositeComponent,
     BulletLegendComponent,
     FlowLegendComponent,
