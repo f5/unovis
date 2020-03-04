5c0fbee48b663ca552725974913174f7df5ad8d0
Nikita Rokotyan <nrokotyan@gmail.com>
2020-03-04 12:00:11 +0500

    Component | Tooltip: Triggers interface update

--- packages/vis/src/core/tooltip/config.ts
+++ packages/vis/src/core/tooltip/config.ts
@@ -5,21 +5,21 @@ import { ComponentCore } from 'core/component'
 // Types
 import { Position } from 'types/position'
 
-export interface TooltipConfigInterface<T extends ComponentCore<any>, TooltipDatum = any> {
+export interface TooltipConfigInterface<T extends ComponentCore<any>, Datum = any> {
     /** Visualization Components */
     components?: T[];
     horizontalPlacement?: Position | undefined;
     verticalPlacement?: Position | undefined;
     triggers?: {
-      [selector: string]: (data: {data: TooltipDatum}, i: number, elements: any) => any;
+      [selector: string]: (data: Datum, i: number, elements: any) => any;
     };
 }
 
-export class TooltipConfig<T extends ComponentCore<any>, TooltipDatum = any> extends Config implements TooltipConfigInterface<T, TooltipDatum> {
+export class TooltipConfig<T extends ComponentCore<any>, Datum = any> extends Config implements TooltipConfigInterface<T, Datum> {
   components: T[] = []
   horizontalPlacement = Position.CENTER
   verticalPlacement = Position.TOP
   triggers: {
-    [selector: string]: (data: {data: TooltipDatum}, i: number, elements: any) => any;
+    [selector: string]: (data: Datum, i: number, elements: any) => any;
   } = {}
 }
