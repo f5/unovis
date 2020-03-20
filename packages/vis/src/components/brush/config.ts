937c274f77ca43a5b73f0ddd10eb44e43c5c103a
Nikita Rokotyan <nrokotyan@gmail.com>
2020-03-20 09:44:02 +0300

    Component | Brush: Better typings for selection II

--- packages/vis/src/components/brush/config.ts
+++ packages/vis/src/components/brush/config.ts
@@ -10,10 +10,10 @@ import { XYComponentConfigInterface, XYComponentConfig } from 'core/xy-component
 import { Arrangement } from 'types/position'
 
 export interface BrushConfigInterface<Datum> extends XYComponentConfigInterface<Datum> {
-  onBrush?: ((selection?: number[], event?: BaseEvent, userDriven?: boolean) => any);
-  onBrushStart?: ((selection?: number[], event?: BaseEvent, userDriven?: boolean) => any);
-  onBrushMove?: ((selection?: number[], event?: BaseEvent, userDriven?: boolean) => any);
-  onBrushEnd?: ((selection?: number[], event?: BaseEvent, userDriven?: boolean) => any);
+  onBrush?: ((selection?: [number, number], event?: BaseEvent, userDriven?: boolean) => any);
+  onBrushStart?: ((selection?: [number, number], event?: BaseEvent, userDriven?: boolean) => any);
+  onBrushMove?: ((selection?: [number, number], event?: BaseEvent, userDriven?: boolean) => any);
+  onBrushEnd?: ((selection?: [number, number], event?: BaseEvent, userDriven?: boolean) => any);
   /** Width of the brush handle */
   handleWidth?: number;
   /** Selection is the actual value units: [start, end] */
@@ -25,10 +25,10 @@ export interface BrushConfigInterface<Datum> extends XYComponentConfigInterface<
 }
 
 export class BrushConfig<Datum> extends XYComponentConfig<Datum> implements BrushConfigInterface<Datum> {
-  onBrush = (s: number[], e: BaseEvent, userDriven: boolean) => {}
-  onBrushStart = (s: number[], e: BaseEvent, userDriven: boolean) => {}
-  onBrushMove = (s: number[], e: BaseEvent, userDriven: boolean) => {}
-  onBrushEnd = (s: number[], e: BaseEvent, userDriven: boolean) => {}
+  onBrush = (s: [number, number], e: BaseEvent, userDriven: boolean) => {}
+  onBrushStart = (s: [number, number], e: BaseEvent, userDriven: boolean) => {}
+  onBrushMove = (s: [number, number], e: BaseEvent, userDriven: boolean) => {}
+  onBrushEnd = (s: [number, number], e: BaseEvent, userDriven: boolean) => {}
   handleWidth = 9
   selection = null
   draggable = false
