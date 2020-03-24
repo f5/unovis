28ee800e79233084b7bc4602e9a55239b24f63a6
Nikita Rokotyan <nrokotyan@gmail.com>
2020-03-24 17:10:25 +0300

    Component | Scatter: Attaching original data items to elements

--- packages/vis/src/components/scatter/modules/node.ts
+++ packages/vis/src/components/scatter/modules/node.ts
@@ -5,7 +5,7 @@ import { smartTransition } from 'utils/d3'
 import { Symbol } from 'types/symbols'
 
 export function createNodes (selection) {
-  selection.attr('transform', d => `translate(${d.x},${d.y})`)
+  selection.attr('transform', d => `translate(${d._screen.x},${d._screen.y})`)
   selection.append('text')
   selection.append('path')
 }
@@ -16,30 +16,30 @@ export function updateNodes (selection, duration) {
     const group = select(elements[i])
     const text = group.select('text')
     const path = group.select('path')
-    if (d.icon) {
+    if (d._screen.icon) {
       text.style('display', null)
       path.style('display', 'none')
       text
-        .html(d.icon)
-        .style('font-size', d.size)
+        .html(d._screen.icon)
+        .style('font-size', d._screen.size)
       smartTransition(text, duration)
-        .attr('fill', d.color)
+        .attr('fill', d._screen.color)
     } else {
       text.style('display', 'none')
       path.style('display', null)
       path.attr('d', () => {
         symbolGenerator
-          .size(d.size * d.size)
-          .type(Symbol[d.shape])
+          .size(d._screen.size * d._screen.size)
+          .type(Symbol[d._screen.shape])
         return symbolGenerator()
       })
       smartTransition(path, duration)
-        .attr('fill', d.color)
+        .attr('fill', d._screen.color)
     }
   })
 
   smartTransition(selection, duration)
-    .attr('transform', d => `translate(${d.x},${d.y})`)
+    .attr('transform', d => `translate(${d._screen.x},${d._screen.y})`)
 }
 
 export function removeNodes (selection) {
