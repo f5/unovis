913caf0cd6622ae8221f9ad18b5bad3cea06eb12
Nikita Rokotyan <nikita@ves.io>
2020-05-03 14:40:03 +0300

    Demo | Traffic Graph: Config and Data update #57

--- web/src/examples/console-traffic-graph-pg2/configuration/graph-config.ts
+++ web/src/examples/console-traffic-graph-pg2/configuration/graph-config.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
 import _sample from 'lodash/sample'
 import _uniq from 'lodash/uniq'
-import { Graph, LayoutType, GraphConfigInterface } from '@volterra/vis'
+import { Graph, LayoutType, GraphConfigInterface, SHAPE } from '@volterra/vis'
 
 const StatusMap = {
   healthy: { color: '#47e845' },
@@ -27,7 +27,7 @@ export const accessors: GraphConfigInterface<any, any> = {
   linkFlow: l => l.showTraffic,
   linkLabel: l => l.linkLabel,
   linkStroke: l => l.score === 100 ? 'rgba(49, 208, 49, 0.500)' : null,
-  linkBandWidth: 2,
+  linkBandWidth: 12,
 }
 
 export const overviewConfig = (data, onNodeClick): GraphConfigInterface<any, any> => ({
@@ -36,6 +36,7 @@ export const overviewConfig = (data, onNodeClick): GraphConfigInterface<any, any
   // Layout
   layoutType: LayoutType.PARALLEL,
   layoutGroupOrder: ['column1', 'ce01-ashburn-aws', 'column3'],
+  layoutNonConnectedAside: false,
 
   // Events
   events: {
@@ -45,18 +46,23 @@ export const overviewConfig = (data, onNodeClick): GraphConfigInterface<any, any
   },
 
   // Panels
-  panels: _uniq(data.nodes.map(d => d.site)).map(label => ({
-    label,
-    nodes: data.nodes.filter(d => d.site === label).map(d => d.id),
-    color: '#333',
-    borderWidth: 1,
-    padding: 15,
-    selectionOutline: label === 'ce01-ashburn-aws',
-    // sideLabelIcon: 'T',
-    // sideLabelShape: SHAPE.CIRCLE,
-    // sideLabelColor: '#8ee422',
-  })),
+  panels: [],
 })
+
+export const getPanels = (data) => _uniq(data.nodes.map(d => d.site)).map(label => ({
+  label,
+  nodes: data.nodes.filter(d => d.site === label).map(d => d.id),
+  color: '#333',
+  borderWidth: 1,
+  padding: 15,
+  selectionOutline: label === 'ce01-ashburn-aws',
+
+  sideLabelIcon: label === 'ce01-ashburn-aws' ? '&#xe9a0;'
+    : label === 'ce02-paris-azure' ? '&#xe9a2;'
+      : '&#xe946;',
+  sideLabelShape: ((label === 'ce01-ashburn-aws') || (label === 'ce02-paris-azure')) ? SHAPE.CIRCLE : SHAPE.SQUARE,
+  sideLabelColor: label === 'ce01-ashburn-aws' ? StatusMap.healthy.color : '#acb2b9',
+}))
 
 export const drilldownConfig = (onNodeClick): GraphConfigInterface<any, any> => ({
   ...accessors,
