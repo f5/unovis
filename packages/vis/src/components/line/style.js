4d62351006b1e20c55948038f397a6664d6e7389
Nikita Rokotyan <nikita@ves.io>
2020-05-05 09:52:56 +0300

    Component | Line: Better styling capabilities #60

--- packages/vis/src/components/line/style.js
+++ packages/vis/src/components/line/style.js
@@ -1,12 +1,19 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-import { css } from 'emotion'
+import { css, injectGlobal } from 'emotion'
+
+export const global = injectGlobal`
+  :root {
+    --vis-line-cursor: default;
+    --vis-line-stroke: var(--vis-color-main);
+  }
+`
 
 export const line = css`
   label: line;
   fill: none;
-  stroke: var(--vis-color-main);
+  stroke: var(--vis-line-stroke);
+  cursor: var(--vis-line-cursor);
 
   &:hover {
-    filter: var(--highlight-filter-id);
   }
 `
