3fe0cd4a97c95aeaef70766ebf800f2548c199ca
Nikita Rokotyan <nikita@ves.io>
2020-05-06 12:31:09 +0300

    Package | Types: Exporting Placement and Arrangement

--- packages/vis/src/types.ts
+++ packages/vis/src/types.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-import { CurveType } from './types/curves'
-import { SymbolType } from './types/symbols'
-import { Scale } from './types/scales'
-import { Projection } from './types/map-projections'
-import { Hierarchy } from './types/radial-dendrogram'
-import { LayoutType, LinkStyle, LinkArrow, NodeDatumCore, LinkDatumCore, SideLabel } from './types/graph'
-import { SHAPE } from './types/shape'
+import { CurveType } from 'types/curves'
+import { SymbolType } from 'types/symbols'
+import { Scale } from 'types/scales'
+import { Position, Arrangement } from 'types/position'
+import { Projection } from 'types/map-projections'
+import { Hierarchy } from 'types/radial-dendrogram'
+import { LayoutType, LinkStyle, LinkArrow, NodeDatumCore, LinkDatumCore, SideLabel } from 'types/graph'
+import { SHAPE } from 'types/shape'
 
-export { CurveType, SymbolType, Scale, Projection, LayoutType, LinkStyle, LinkArrow, NodeDatumCore, LinkDatumCore, SHAPE, SideLabel, Hierarchy }
+export { CurveType, SymbolType, Scale, Projection, LayoutType, LinkStyle, LinkArrow, NodeDatumCore, LinkDatumCore, SHAPE, SideLabel, Hierarchy, Position, Arrangement }
