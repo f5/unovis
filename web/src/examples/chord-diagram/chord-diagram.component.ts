b92a96da7ba969d934d12af001d4a3abe158aa24
Nikita Rokotyan <nikita@ves.io>
2020-05-18 13:06:30 +0300

    Demo: Separating Radial Dendogram and Chord Diagram demos
    ref #68

---
+++ web/src/examples/chord-diagram/chord-diagram.component.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Volterra, Inc. All rights reserved.
+import { Component } from '@angular/core'
+import { nest as d3Nest } from 'd3-collection'
+
+// Vis
+import { ChordDiagram, ChordDiagramConfigInterface, Hierarchy } from '@volterra/vis'
+
+import trafficData from './data/traffic.json'
+
+const findNode = (nodes, id) => nodes.find(n => n.id === id)
+trafficData.links.forEach((l: any) => {
+  const sourceNode = findNode(trafficData.nodes, l.source)
+  const targetNode = findNode(trafficData.nodes, l.target)
+  const value = 1 + Math.random()
+  l.value = value
+  sourceNode.value = (sourceNode.value || 0) + value
+  targetNode.value = (targetNode.value || 0) + value
+})
+
+const hierarchy = {
+  values: d3Nest<any, any>().key(d => d.site)
+    .entries(trafficData.nodes),
+}
+
+@Component({
+  selector: 'chord-diagram',
+  templateUrl: './chord-diagram.component.html',
+  styleUrls: ['./chord-diagram.component.css'],
+})
+
+export class ChordDiagramComponent<H extends Hierarchy> {
+  title = 'chord-diagram'
+
+  data = {
+    nodes: hierarchy,
+    links: trafficData.links,
+  }
+
+  margin = {}
+  config: ChordDiagramConfigInterface<H> = {
+    nodeWidth: 20,
+    nodeLabelType: 'along',
+  }
+
+  component = new ChordDiagram(this.config)
+}
--- web/src/examples/radial-dendrogram/radial-dendrogram.component.ts
+++ web/src/examples/radial-dendrogram/radial-dendrogram.component.ts
@@ -1,30 +1,11 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-/* eslint-disable */
 import { AfterViewInit, OnDestroy, Component } from '@angular/core'
-import { nest as d3Nest } from 'd3-collection'
 
 // Vis
-import { ChordDiagram, ChordDiagramConfigInterface, Hierarchy } from '@volterra/vis'
+import { RadialDendrogram, RadialDendrogramConfigInterface, Hierarchy } from '@volterra/vis'
 
 // Helpers
-// import { getHierarchyData } from '../../utils/data'
-
-import trafficData from './data/traffic.json'
-
-const findNode = (nodes, id) => nodes.find(n => n.id === id)
-trafficData.links.forEach((l: any) => {
-  const sourceNode = findNode(trafficData.nodes, l.source)
-  const targetNode = findNode(trafficData.nodes, l.target)
-  const value = 1 + Math.random()
-  l.value = value
-  sourceNode.value = (sourceNode.value || 0) + value
-  targetNode.value = (targetNode.value || 0) + value
-})
-const hierarchy = { 
-  values: d3Nest<any, any>().key(d => d.site)
-    .entries(trafficData.nodes) 
-}
-
+import { getHierarchyData } from '../../utils/data'
 
 @Component({
   selector: 'radial-dendrogram',
@@ -35,40 +16,31 @@ const hierarchy = {
 export class RadialDendrogramComponent<H extends Hierarchy> implements AfterViewInit, OnDestroy {
   title = 'radial-dendrogram'
 
-
-  data = { 
-    nodes: hierarchy,
-    // getHierarchyData(500, {
-    //   source: ['re01',  're02', 're03', 're04'],
-    //   target: ['site1', 'site2', 'site3', 'site4', 'site5', 'site6', 'site7', 'site8', 'site9', 'site10'],
-    // }), 
-    links: trafficData.links
-  }
+  data = getHierarchyData(100, {
+    source: ['re01', 're02', 're03', 're04', 're05', 're06', 're07', 're08'],
+    middle: ['vhost 1', 'vhost 2', 'vhost 3'],
+    target: ['site1', 'site2', 'site3', 'site4', 'site5', 'site6', 'site7', 'site8', 'site9', 'site10'],
+  })
 
   margin = {}
-  config: ChordDiagramConfigInterface<H> = {
+  config: RadialDendrogramConfigInterface<H> = {
     nodeWidth: 20,
-    nodeLabelType: 'along'
   }
-  
-  component = new ChordDiagram(this.config)
+
+  component = new RadialDendrogram(this.config)
   intervalId: NodeJS.Timeout
 
   ngAfterViewInit (): void {
-    console.log(trafficData, hierarchy, this.data.nodes)
-
-    // this.intervalId = setInterval(() => {
-    //   this.data = getHierarchyData(100, {
-    //     source: ['re01', 're02', 're03', 're04'],
-    //     target: ['site1', 'site2', 'site3', 'site4', 'site5', 'site6', 'site7', 'site8', 'site9', 'site10'],
-    //   })
-    // }, 3000)
+    this.intervalId = setInterval(() => {
+      this.data = getHierarchyData(100, {
+        source: ['re01', 're02', 're03', 're04', 're05', 're06', 're07', 're08'],
+        middle: ['vhost', 'vhost 2', 'vhost 3'],
+        target: ['site1', 'site2', 'site3', 'site4', 'site5', 'site6', 'site7', 'site8', 'site9', 'site10'],
+      })
+    }, 3000)
   }
 
-  ngOnDestroy () : void {
+  ngOnDestroy (): void {
     clearInterval(this.intervalId)
   }
-
 }
-
-
