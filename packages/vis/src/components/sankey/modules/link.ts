b996b1c39b497f2e4c8d40270257b2a7bd62e589
Stukova <olga@ves.io>
2020-06-18 01:19:42 +0500

    Component | Sankey - Custom link path interpolator
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/97

--- packages/vis/src/components/sankey/modules/link.ts
+++ packages/vis/src/components/sankey/modules/link.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-import { select } from 'd3-selection'
-import { interpolatePath } from 'd3-interpolate-path'
+import { interpolateNumber } from 'd3-interpolate'
 
 // Utils
 import { getColor } from 'utils/color'
@@ -15,29 +14,41 @@ import { SankeyConfig } from '../config'
 // Styles
 import * as s from '../style'
 
-export function linkPath (link, background = false): string {
-  const w = Math.max(background ? 10 : 1, link.width)
-  const centerX = (link.source.x1 + link.target.x0) / 2
+export function linkPath ({ x0, x1, y0, y1, width }): string {
+  const top0 = y0 - width / 2
+  const top1 = y1 - width / 2
+  const bottom0 = y0 + width / 2
+  const bottom1 = y1 + width / 2
+  const centerX = (x0 + x1) / 2
 
   return `
-    M ${link.source.x1}, ${link.y0 - w / 2}
+    M ${x0}, ${top0}
 
-    C ${centerX}, ${link.y0 - w / 2}
-      ${centerX}, ${link.y1 - w / 2}
-      ${link.target.x0}, ${link.y1 - w / 2}
+    C ${centerX}, ${top0}
+      ${centerX}, ${top1}
+      ${x1}, ${top1}
     
-    L ${link.target.x0}, ${link.y1 + w / 2}
+    L ${x1}, ${bottom1}
     
-    C ${centerX}, ${link.y1 + w / 2}
-      ${centerX}, ${link.y0 + w / 2}
-      ${link.source.x1}, ${link.y0 + w / 2}
+    C ${centerX}, ${bottom1}
+      ${centerX}, ${bottom0}
+      ${x0}, ${bottom0}
     z
   `
 }
 
 export function createLinks (sel): void {
   sel.append('path').attr('class', s.visibleLink)
-    .attr('d', d => linkPath(d))
+    .attr('d', (d, i, el) => {
+      el[i]._animState = {
+        x0: d.source.x1,
+        x1: d.target.x0,
+        y0: d.y0,
+        y1: d.y1,
+        width: Math.max(1, d.width),
+      }
+      return linkPath(el[i]._animState)
+    })
   sel.append('path').attr('class', s.transparentLink)
   sel.style('opacity', 0)
 }
@@ -52,16 +63,52 @@ export function updateLinks<N extends SankeyNodeDatumInterface, L extends Sankey
   if (duration) {
     linkSelectionChanged
       .attrTween('d', (d, i, el) => {
-        const previous = select(el[i]).attr('d')
-        const next = linkPath(d)
-        return interpolatePath(previous, next)
+        const previous = el[i]._animState
+        const next = {
+          x0: d.source.x1,
+          x1: d.target.x0,
+          y0: d.y0,
+          y1: d.y1,
+          width: Math.max(1, d.width),
+        }
+        const interpolator = {
+          x0: interpolateNumber(previous.x0, next.x0),
+          x1: interpolateNumber(previous.x1, next.x1),
+          y0: interpolateNumber(previous.y0, next.y0),
+          y1: interpolateNumber(previous.y1, next.y1),
+          width: interpolateNumber(previous.width, next.width),
+        }
+
+        el[i]._animState = next
+
+        return function (t) {
+          return linkPath({
+            x0: interpolator.x0(t),
+            x1: interpolator.x1(t),
+            y0: interpolator.y0(t),
+            y1: interpolator.y1(t),
+            width: interpolator.width(t),
+          })
+        }
       })
   } else {
-    linkSelection.attr('d', d => linkPath(d))
+    linkSelection.attr('d', d => linkPath({
+      x0: d.source.x1,
+      x1: d.target.x0,
+      y0: d.y0,
+      y1: d.y1,
+      width: Math.max(1, d.width),
+    }))
   }
 
   sel.select(`.${s.transparentLink}`)
-    .attr('d', d => linkPath(d, true))
+    .attr('d', d => linkPath({
+      x0: d.source.x1,
+      x1: d.target.x0,
+      y0: d.y0,
+      y1: d.y1,
+      width: Math.max(10, d.width),
+    }))
 }
 
 export function removeLinks (sel): void {
