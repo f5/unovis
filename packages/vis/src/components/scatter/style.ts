5c133a283d3fbdea4d5e4b6d839b27538bcbefa6
Nikita Rokotyan <nikita@ves.io>
2020-08-05 14:06:41 -0700

    Component | Scatter: Fill- and stroke-opacity css-variables
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/140

--- packages/vis/src/components/scatter/style.ts
+++ packages/vis/src/components/scatter/style.ts
@@ -5,10 +5,11 @@ export const global = injectGlobal`
   :root {
     --vis-scatter-cursor: default;
     --vis-scatter-fill: var(--vis-color-main);
-    --vis-scatter-stroke: none;
+    --vis-scatter-stroke: var(--vis-color-main);
     --vis-scatter-stroke-width: 0px;
+    --vis-scatter-fill-opacity: 1;
+    --vis-scatter-stroke-opacity: 1;
     --vis-scatter-hover-stroke-width: 2px;
-    --vis-scatter-hover-stroke: none;
   }
 `
 
@@ -20,11 +21,14 @@ export const point = css`
     stroke-width: var(--vis-scatter-stroke-width);
     stroke: var(--vis-scatter-stroke);
     fill: var(--vis-scatter-fill);
+
+    fill-opacity: var(--vis-scatter-fill-opacity);
+    stroke-opacity: var(--vis-scatter-stroke-opacity);
+
     user-select: none;
 
     &:hover {
       stroke-width: var(--vis-scatter-hover-stroke-width);
-      stroke: var(--vis-scatter-hover-stroke);
     }
   }
 `
