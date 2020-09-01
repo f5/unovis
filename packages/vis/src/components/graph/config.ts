e9ac2359a7ef1a4ad1e12ed01a501ba01886cf10
Nikita Rokotyan <nikita@ves.io>
2020-09-01 16:50:48 -0700

    Component | Graph: Typings fixes
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/155
    ref #128

--- packages/vis/src/components/graph/config.ts
+++ packages/vis/src/components/graph/config.ts
@@ -105,7 +105,7 @@ export interface GraphConfigInterface<N extends NodeDatumCore, L extends LinkDat
   /** Node Sublabel accessor function or value */
   nodeSubLabel?: StringAccessor<N>;
   /** Node Side Label accessor function or undefined */
-  nodeSideLabels?: ((d: N, i?: number, ...any) => SideLabel) | undefined;
+  nodeSideLabels?: ((d: N, i?: number, ...any) => SideLabel[]) | undefined;
   /** Node disabled accessor function or value */
   nodeDisabled?: BooleanAccessor<N>;
   /** Node fill color accessor function or value */
