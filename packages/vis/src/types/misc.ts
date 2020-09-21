2f025c1d064f5413bff1d9cc4bf616834f0e974f
Nikita Rokotyan <nikita@ves.io>
2020-09-21 15:04:26 -0700

    Types | Accessors: Accepting `null` as a function return value
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/164

--- packages/vis/src/types/misc.ts
+++ packages/vis/src/types/misc.ts
@@ -2,9 +2,9 @@
 import { ContiniousScale } from 'types/scales'
 
 export type NumericAccessor<Datum> = ((d: Datum, i?: number, ...any) => number | null) | number | null | undefined
-export type StringAccessor<Datum> = ((d: Datum, i?: number, ...any) => string) | string | null
-export type ColorAccessor<Datum> = ((d: Datum, i?: number, ...any) => string) | string | null
-export type BooleanAccessor<Datum> = ((d: Datum, i?: number, ...any) => boolean) | boolean
+export type StringAccessor<Datum> = ((d: Datum, i?: number, ...any) => string | null) | string | null
+export type ColorAccessor<Datum> = ((d: Datum, i?: number, ...any) => string | null) | string | null
+export type BooleanAccessor<Datum> = ((d: Datum, i?: number, ...any) => boolean | null) | boolean | null
 
 export type Dimension = {
   /** D3 scale, e.g. Scale.ScaleLinear */
