c4886d8ff492a3261e562f61433389a0e17fcf0e
Nikita Rokotyan <nikita@ves.io>
2020-09-29 16:39:29 -0700

    Project: Using Inter as the default font
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/167

--- packages/vis/src/styles/css-variables.js
+++ packages/vis/src/styles/css-variables.js
@@ -5,7 +5,7 @@ import { colors, getCSSVarName } from './colors'
 export const variables = injectGlobal`
   :root {
     label: vis-root-styles;
-    --vis-font-family: "Open Sans", Arial, "Helvetica Neue", Helvetica, sans-serif;
+    --vis-font-family: Inter, Arial, "Helvetica Neue", Helvetica, sans-serif;
     --vis-color-main: #34daa6;
     --vis-color-gray: #2a2a2a;
     ${colors.map((c, i) => `${getCSSVarName(i)}: ${c};`)}
