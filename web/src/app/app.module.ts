23e4192e8adf1462161ca29727bc6b85dc736b87
Nikita Rokotyan <nikita@ves.io>
2020-09-30 16:06:53 -0700

    Demo: Renaming SimpleMap to TopoJSONMap
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/168

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { CompositeComponent } from 'examples/composite/composite.component'
 import { WrapperUsageExampleComponent } from 'examples/wrapper-usage-example/wrapper-usage-example.component'
 import { TimelineComponent } from 'examples/timeline/timeline.component'
 import { BulletLegendExampleComponent } from 'examples/bullet-legend/bullet-legend.component'
-import { SimpleMapComponent } from 'examples/simple-map/simple-map.component'
+import { TopoJSONMapComponent } from 'examples/topojson-map/topojson-map.component'
 import { MapComponent } from 'examples/map/map.component'
 import { MapHeatmapComponent } from 'examples/map-heatmap/map-heatmap.component'
 import { RadialDendrogramComponent } from 'examples/radial-dendrogram/radial-dendrogram.component'
@@ -49,7 +49,7 @@ const appRoutes: Routes = [
   { path: 'wrapper-usage-example', component: WrapperUsageExampleComponent },
   { path: 'timeline', component: TimelineComponent },
   { path: 'bullet-legend', component: BulletLegendExampleComponent },
-  { path: 'simplemap', component: SimpleMapComponent },
+  { path: 'topojson-map', component: TopoJSONMapComponent },
   { path: 'radial-dendrogram', component: RadialDendrogramComponent },
   { path: 'graph', component: GraphComponent },
   { path: 'force-graph', component: ServiceGraphComponent },
@@ -79,7 +79,7 @@ appRoutes.push({ path: '', redirectTo: `/${appRoutes[0].path}`, pathMatch: 'full
     WrapperUsageExampleComponent,
     TimelineComponent,
     BulletLegendExampleComponent,
-    SimpleMapComponent,
+    TopoJSONMapComponent,
     MapComponent,
     MapLeafletComponent,
     MapHeatmapComponent,
