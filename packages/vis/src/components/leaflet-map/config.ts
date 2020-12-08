f8ff7e9268c1d6810fb0b4f5802035a141889d3b
Nikita Rokotyan <nikita@ves.io>
2020-12-08 13:35:55 -0800

    Component | Leaflet Map: Config clean up and new properties
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/185

--- packages/vis/src/components/leaflet-map/config.ts
+++ packages/vis/src/components/leaflet-map/config.ts
@@ -7,13 +7,23 @@ import { Tooltip } from 'core/tooltip'
 
 // Types
 import { NumericAccessor, StringAccessor, ColorAccessor } from 'types/misc'
-import { LeafletMapRenderer, ClusterOutlineType, Bounds, StatusMap, MapZoomState } from 'types/map'
+import { LeafletMapRenderer, ClusterOutlineType, Bounds, StatusMap, MapZoomState, Point } from 'types/map'
 import { LeafletMap } from './index'
 
 export interface LeafletMapConfigInterface<Datum> extends ComponentConfigInterface {
+  // General
+  /** FlyTo Duration */
+  flyToDuration?: number;
+  /** Zoom Duration */
+  zoomDuration?: number;
+  /** Default bounds that will be applid on the first map render if the bounds property is not set */
+  initialBounds?: Bounds;
+  /** Force set map bounds */
+  bounds?: Bounds;
+  /** */
   renderer?: LeafletMapRenderer | string;
   /**  */
-  tamgramRenderer?: any;
+  tangramRenderer?: any;
   /** Mapboxgl Access Token or Nextzen API key */
   accessToken?: string;
   /** Mapbox style glyphs URL */
@@ -22,6 +32,10 @@ export interface LeafletMapConfigInterface<Datum> extends ComponentConfigInterfa
   sources?: Record<string, unknown>;
   /** Tangram or Mapbox style renderer settings */
   rendererSettings?: Record<string, unknown>;
+  /** Array of attribution labels */
+  attribution?: string[];
+
+  // Map events
   /** Map Move / Zoom joint callback function. Default: `undefined` */
   onMapMoveZoom?: (({ mapCenter, zoomLevel, bounds }: MapZoomState) => any);
   /** Move Move Start callback function. Default: `undefined` */
@@ -32,6 +46,8 @@ export interface LeafletMapConfigInterface<Datum> extends ComponentConfigInterfa
   onMapZoomStart?: (({ mapCenter, zoomLevel, bounds }: MapZoomState) => any);
   /** Move Zoom End callback function. Default: `undefined` */
   onMapZoomEnd?: (({ mapCenter, zoomLevel, bounds }: MapZoomState) => any);
+
+  // Point
   /** Point longitude accessor function or value */
   pointLongitude?: NumericAccessor<Datum>;
   /** Point latitude accessor function or value */
@@ -46,24 +62,28 @@ export interface LeafletMapConfigInterface<Datum> extends ComponentConfigInterfa
   pointColor?: ColorAccessor<Datum>;
   /** Point radius accessor function or value */
   pointRadius?: NumericAccessor<Datum>;
+  /** Point value accessor function. If there's a value associated with a point, it can be displayed as a text label */
+  pointValue?: NumericAccessor<Datum>;
+  /** Point value accessor function. If there's a value associated with a point, it can be displayed as a text label */
+  pointLabel?: StringAccessor<Point>;
   /** Point stroke width accessor function or value */
   pointStrokeWidth?: NumericAccessor<Datum>;
+  /** */
+  selectedNodeId?: string;
+
+  // Cluster
   /** Cluster point outline type */
   clusterOutlineType?: ClusterOutlineType;
   /** Cluster point outline width */
   clusterOutlineWidth?: number;
   /** Use cluster background */
   clusterBackground?: boolean;
-  /** FlyTo Duration */
-  flyToDuration?: number;
-  /** Zoom Duration */
-  zoomDuration?: number;
-  /** Default bounds that will be applid on the first map render if the bounds property is not set */
-  initialBounds?: Bounds;
-  /** Force set map bounds */
-  bounds?: Bounds;
+  /** Defines whether the cluster should expand on click or not. Default: `false` */
+  clusterExpandOnClick?: boolean;
   /** Status styles */
   statusMap?: StatusMap;
+
+  // TopoJSON overlay
   /** A TopoJSON Geometry layer to be displayed on top of the map. Supports fill and stroke */
   topoJSONLayer?: {
     /** TopoJSON.Topology */
@@ -75,20 +95,30 @@ export interface LeafletMapConfigInterface<Datum> extends ComponentConfigInterfa
     strokeOpacity?: number;
     strokeWidth?: number;
   };
+
+  // Misc
   /** Tooltip component */
   tooltip?: Tooltip<LeafletMap<Datum>, Datum>;
-  /** Array of attribution labels */
-  attribution?: string[],
 }
 
 export class LeafletMapConfig<Datum> extends ComponentConfig implements LeafletMapConfigInterface<Datum> {
+  // General
+  flyToDuration = 1500
+  zoomDuration = 800
+  initialBounds = undefined
+  bounds = undefined
   renderer = LeafletMapRenderer.TANGRAM
+  attribution = []
   accessToken = ''
+
+  // Map events
   onMapMoveZoom = undefined
   onMapMoveStart = undefined
   onMapMoveEnd = undefined
   onMapZoomStart = undefined
   onMapZoomEnd = undefined
+
+  // Point
   pointLongitude = (d: Datum): number => d['longitude']
   pointLatitude = (d: Datum): number => d['latitude']
   pointId = (d: Datum): string => d['id']
@@ -96,16 +126,19 @@ export class LeafletMapConfig<Datum> extends ComponentConfig implements LeafletM
   pointShape = (d: Datum): string => d['shape']
   pointColor = (d: Datum): string => d['color']
   pointRadius = undefined
+  pointValue = undefined
+  pointLabel = (d: Point): string => `${d.properties.point_count ?? ''}`
   pointStrokeWidth = 1
+  selectedNodeId = undefined
+
+  // Cluster
   clusterOutlineType = ClusterOutlineType.DONUT
   clusterOutlineWidth = 1.25
   clusterBackground = true
-  flyToDuration = 1500
-  zoomDuration = 800
-  initialBounds = undefined
-  bounds = undefined
+  clusterExpandOnClick = true;
   statusMap = {}
-  selectedNodeId = undefined
+
+  // TopoJSON Overlay
   topoJSONLayer = {
     sources: undefined,
     fillOpacity: 0.6,
@@ -113,6 +146,6 @@ export class LeafletMapConfig<Datum> extends ComponentConfig implements LeafletM
     strokeWidth: 1,
   }
 
+  // Misc
   tooltip = undefined
-  attribution = []
 }
