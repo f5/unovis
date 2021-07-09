a08bc0bebc5f5b6823fdecb64be44e82467b2516
Nikita Rokotyan <nikita@ves.io>
2021-07-09 14:07:10 -0700

    Vis Angular | Container: XY Container Clean Up
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/243

---
+++ packages/vis-angular/src/containers/xy-container/xy-container.module.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Volterra, Inc. All rights reserved.
+import { NgModule } from '@angular/core'
+import { VisXYContainerComponent } from './xy-container.component'
+
+@NgModule({
+  imports: [],
+  declarations: [VisXYContainerComponent],
+  exports: [VisXYContainerComponent],
+})
+export class VisXYContainerModule {}
