14f0125a8da90a3aba4d1b513b2f3cfe50b50fad
Nikita Rokotyan <nikita@f5.com>
2021-08-10 19:28:18 -0700

    Vis Angular: Exporting individual modules
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/268

--- packages/vis-angular/src/ancillary.ts
+++ packages/vis-angular/src/ancillary.ts
@@ -1,2 +1,3 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
 export { VisTooltipComponent } from './core/tooltip/tooltip.component'
+export { VisTooltipModule } from './core/tooltip/tooltip.module'
--- packages/vis-angular/src/containers.ts
+++ packages/vis-angular/src/containers.ts
@@ -1,3 +1,5 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
 export { VisXYContainerComponent } from './containers/xy-container/xy-container.component'
+export { VisXYContainerModule } from './containers/xy-container/xy-container.module'
 export { VisSingleContainerComponent } from './containers/single-container/single-container.component'
+export { VisSingleContainerModule } from './containers/single-container/single-container.module'
