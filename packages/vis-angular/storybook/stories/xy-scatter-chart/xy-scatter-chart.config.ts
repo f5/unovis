d8e976a7515421099b62fe27d6864a17a9ed6e73
rbol <r.bol@f5.com>
2021-08-18 14:20:34 -0700

    feat[storybook]: add scatter chart story ref: https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/271

---
+++ packages/vis-angular/storybook/stories/xy-scatter-chart/xy-scatter-chart.config.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Volterra, Inc. All rights reserved.
+import { ScatterConfigInterface, SymbolType } from '@volterra/vis'
+import { XYChartStoryConfig } from '../../utils/content-wrappers'
+import { DataRecord, generateDataRecords } from '../../data/time-series'
+
+function getElement (this: any[], _: DataRecord, i?: number): any {
+  return this[i !== undefined ? i : Math.floor(Math.random() * this.length)]
+}
+
+export const baseConfig = (n?: number): XYChartStoryConfig & ScatterConfigInterface<DataRecord> => ({
+  x: d => d.x,
+  y: d => d.y,
+  color: () => getColor(null, 0),
+  data: generateDataRecords(n).map((d, i) => ({ ...d, y1: i, y2: i % 2, y3: i % 3 })),
+})
+
+export const labelConfig = (altColor?: boolean): XYChartStoryConfig & ScatterConfigInterface<DataRecord> => ({
+  ...baseConfig(),
+  sizeRange: [20, 30],
+  color: d => ['#5242aa', '#a0d6e5'][altColor ? d.y2 : 0],
+})
+
+const constArray = Array(10).fill(0).map(() => Math.random() * 10)
+export const customConfig = (): XYChartStoryConfig & ScatterConfigInterface<DataRecord> => ({
+  ...baseConfig(),
+  y: d => constArray[d.y1],
+  size: d => d.y3 + 1,
+  label: d => ['S', 'M', 'L'][d.y3],
+  color: d => getColor(d, d.y3),
+})
+
+export const getColor = getElement.bind(['#6A9DFF', '#1acb9a', '#8777d9', '#f88080'])
+export const getLabel = getElement.bind(['A', 'B', 'C'])
+export const getShape = getElement.bind([SymbolType.Circle, SymbolType.Square, SymbolType.Star])
