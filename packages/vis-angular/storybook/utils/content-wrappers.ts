07ed4ff3e43fd42360edfafe289054d9090a4ce0
Nikita Rokotyan <nikita@f5.com>
2021-08-25 11:42:01 -0700

    Vis Angular | Storybook: xyChartContentWrapper additional styles
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/286

--- packages/vis-angular/storybook/utils/content-wrappers.ts
+++ packages/vis-angular/storybook/utils/content-wrappers.ts
@@ -11,12 +11,13 @@ export type XYChartStoryConfig = {
   storyHeight?: number;
   storyXAxisLabel?: string;
   storyYAxisLabel?: string;
+  storyStyles?: string;
 }
 
 export function xyChartContentWrapper<T> (story: Story<XYChartStoryConfig & T>): string {
   return `
     <div style="width: 100%; position: relative" [ngStyle]="{'width.px': storyWidth, 'height.px': storyHeight ?? 300 }">
-      <vis-xy-container [data]="data">
+      <vis-xy-container [data]="data" [style]="storyStyles">
         <vis-axis *ngIf="!storyHideAxes && !storyHideXAxis" type="x" [label]="storyXAxisLabel ?? X"></vis-axis>
         <vis-axis *ngIf="!storyHideAxes && !storyHideYAxis" type="y" [label]="storyYAxisLabel ?? Y"></vis-axis>
         ${story}
