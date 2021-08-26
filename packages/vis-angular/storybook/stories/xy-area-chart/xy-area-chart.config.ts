586ee8f2c41a2083fd04eb47b4a50b5b511713e2
Nikita Rokotyan <nikita@f5.com>
2021-08-26 11:05:05 -0700

    Vis Angular | Storybook: content-wrappers.ts → xy-content-wrapper.ts
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/282

--- packages/vis-angular/storybook/stories/xy-area-chart/xy-area-chart.config.ts
+++ packages/vis-angular/storybook/stories/xy-area-chart/xy-area-chart.config.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
 import { AreaConfigInterface } from '@volterra/vis'
-import { XYChartStoryConfig } from '../../utils/content-wrappers'
+import { XYChartStoryConfig } from '../../utils/xy-content-wrapper'
 import { DataRecord, generateDataRecords } from '../../data/time-series'
 
 export const baseConfig = (n?: number): XYChartStoryConfig & AreaConfigInterface<DataRecord> => ({
