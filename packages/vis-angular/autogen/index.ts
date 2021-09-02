120f1ae05a7ea72c125836d7858574f6e38ed336
Nikita Rokotyan <nikita@f5.com>
2021-09-02 10:37:34 -0700

    Vis Angular | Wrapper Generator: Importing Tooltip from `components`
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/291

--- packages/vis-angular/autogen/index.ts
+++ packages/vis-angular/autogen/index.ts
@@ -34,7 +34,7 @@ const components: ComponentInput[] = [
   { name: 'Graph', sources: [coreComponentConfigPath, '/components/graph'], provide: 'VisCoreComponent' },
 
   // Ancillary components
-  { name: 'Tooltip', sources: ['/core/tooltip'], dataType: null, provide: 'VisGenericComponent' },
+  { name: 'Tooltip', sources: ['/components/tooltip'], dataType: null, provide: 'VisGenericComponent' },
 
   // Unique cases (you can still generate a wrapper for these components, but most likely it will require some changes)
   // { name: 'LeafletMap', sources: [coreComponentConfigPath, '/components/leaflet-map'], provide: 'VisCoreComponent' },
