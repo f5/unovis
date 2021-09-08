368b30af47972be2011ab1cb0e11006dae239099
Nikita Rokotyan <nikita@f5.com>
2021-09-08 10:52:28 -0700

    Vis | Types: Using TopoJSON namespace instead of importing the types from `topojson-specification`
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/291

--- packages/vis/src/data.ts
+++ packages/vis/src/data.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-// eslint-disable-next-line import/no-unresolved
-import { GeometryCollection, Topology } from 'topojson-specification'
 import worldMap from './data/world-simple.json'
 import worldMap110mAlpha from './data/world-110m-alpha.json'
 
-export const WorldMapTopoJSON = worldMap as unknown as Topology<{countries: GeometryCollection<{name: string; color: string}>}>
-export const WorldMap110mAlphaTopoJSON = worldMap110mAlpha as unknown as Topology<{countries: GeometryCollection<{name: string; color: string}>}>
+export const WorldMapTopoJSON = worldMap as unknown as TopoJSON.Topology<{countries: TopoJSON.GeometryCollection<{name: string; color: string}>}>
+export const WorldMap110mAlphaTopoJSON = worldMap110mAlpha as unknown as TopoJSON.Topology<{countries: TopoJSON.GeometryCollection<{name: string; color: string}>}>
