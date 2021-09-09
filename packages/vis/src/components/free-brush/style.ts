f444a38ae63a95f8087fb7c35a5db682d78e51e0
Nikita Rokotyan <nikita@f5.com>
2021-09-09 10:39:12 -0700

    Vis | Component | FreeBrush: Adding fill opacity CSS variable for selection
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/297

--- packages/vis/src/components/free-brush/style.ts
+++ packages/vis/src/components/free-brush/style.ts
@@ -4,6 +4,7 @@ import { css, injectGlobal } from 'emotion'
 export const variables = injectGlobal`
   :root {
     --vis-free-brush-selection-fill: #0b1640;
+    --vis-free-brush-selection-fill-opacity: 0.4;
     --vis-free-brush-selection-stroke: #acb2b9;
     --vis-free-brush-handle-fill: #6d778c;
     --vis-free-brush-handle-stroke: #eee;
@@ -14,29 +15,29 @@ export const brush = css`
   label: brush;
   fill: none;
   stroke: none;
-  
+
   .selection {
     fill: var(--vis-free-brush-selection-fill);
     stroke: var(--vis-free-brush-selection-stroke);
     stroke-width: 0;
     stroke-opacity: 0;
   }
-  
+
   .handle {
     fill: var(--vis-free-brush-handle-fill);
   }
 `
 
-export const hide = css`  
+export const hide = css`
   .selection, .handle {
-    display: none; 
+    display: none;
   }
 `
 
 export const unselected = css`
   label: unselected;
   fill: var(--vis-free-brush-selection-fill);
-  opacity: 0.4;
+  opacity: var(--vis-free-brush-selection-fill-opacity);
   pointer-events: none;
 `
 
