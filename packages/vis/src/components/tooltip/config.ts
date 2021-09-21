bf110aa46fa80c06b8a218791da022c55ed3f036
Nikita Rokotyan <nikita@f5.com>
2021-09-21 11:19:34 -0700

    Vis | Component | Tooltip: Minor config interface jsDoc comments update
    ref #128

--- packages/vis/src/components/tooltip/config.ts
+++ packages/vis/src/components/tooltip/config.ts
@@ -26,13 +26,13 @@ export interface TooltipConfigInterface {
    *
    * ```
    * {
-   *   [selectorString]: (d) => string | HTMLElement
+   *   [selectorString]: (d: unknown) => string | HTMLElement
    * }
    * ```
    * e.g.:
    * ```
    * {
-   *   [Area.selectors.area]: (d) => `<div>${d.value.toString()}</div>
+   *   [Area.selectors.area]: (d: AreaDatum[]) => `<div>${d.value.toString()}</div>
    * }
    * ```
    */
