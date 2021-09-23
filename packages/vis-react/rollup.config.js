6184758e4de247e67985fd6f642bbe50a7d43c6e
Nikita Rokotyan <nikita@f5.com>
2021-09-23 15:43:16 -0700

    Vis React: Project set up
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/302

---
+++ packages/vis-react/rollup.config.js
@@ -0,0 +1,33 @@
+// Copyright (c) Volterra, Inc. All rights reserved.
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import pkg from './package.json'
+
+// Array of extensions to be handled by babel
+const extensions = ['.ts', '.tsx']
+
+// Excluded dependencies
+const external = Object.keys(pkg.devDependencies)
+
+export default {
+  input: ['src/index.ts'],
+  output: {
+    dir: 'dist',
+    sourcemap: true,
+    format: 'esm',
+    preserveModules: true,
+  },
+  plugins: [
+    peerDepsExternal(),
+    resolve({
+      extensions,
+    }),
+    babel({
+      extensions,
+      babelHelpers: 'inline',
+      include: extensions.map(ext => `src/**/*${ext}`),
+    }),
+  ],
+  external,
+}
