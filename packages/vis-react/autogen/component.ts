cc51985ee82d9f022c0f9a411fc720222f2318ea
Nikita Rokotyan <nikita@f5.com>
2021-09-28 14:07:27 -0700

    Vis React | Wrapper Generator: Custom elements for Tooltip, Crosshair and Axis; utilyzing the VisComponentElement type
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/313

--- packages/vis-react/autogen/component.ts
+++ packages/vis-react/autogen/component.ts
@@ -5,7 +5,8 @@ export function getComponentCode (
   componentName: string,
   generics: GenericParameter[] | undefined,
   importStatements: { source: string; elements: string[] }[],
-  dataType: string | null = 'any'
+  dataType: string | null = 'any',
+  elementSuffix = 'component'
 ): string {
   const genericsStr = generics ? `<${generics?.map(g => g.name).join(', ')}>` : ''
   const genericsDefStr = generics
@@ -17,18 +18,19 @@ import { useEffect, useRef, useState } from 'react'
 //import { ${componentName}, ${componentName}ConfigInterface } from '@volterra/vis'
 ${importStatements.map(s => `import { ${s.elements.join(', ')} } from '${s.source}'`).join('\n')}
 
+// Types
+import { VisComponentElement } from 'src/types/dom'
+
 export type Vis${componentName}Props${genericsDefStr} = ${componentName}ConfigInterface${genericsStr} & { data?: ${dataType} }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function Vis${componentName}${genericsDefStr} (props: Vis${componentName}Props${genericsStr}): JSX.Element {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  type RefType = HTMLDivElement & { __component__: ${componentName}${genericsStr} }
-  const ref = useRef<RefType>(null)
+  const ref = useRef<VisComponentElement<${componentName}${genericsStr}>>(null)
   const [component] = useState<${componentName}${genericsStr}>(new ${componentName}(props))
 
   // On Mount
   useEffect(() => {
-    (ref.current as RefType).__component__ = component
+    (ref.current as VisComponentElement<${componentName}${genericsStr}>).__component__ = component
   }, [])
 
   // On Props Update
@@ -37,7 +39,7 @@ export function Vis${componentName}${genericsDefStr} (props: Vis${componentName}
     component?.setConfig(props)
   })
 
-  return <vis-component ref={ref} />
+  return <vis-${elementSuffix} ref={ref} />
 }
 `
 }
