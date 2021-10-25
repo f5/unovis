312bb15ad11fee54b55ee2e57849e976db7c25b1
Nikita Rokotyan <nikita@f5.com>
2021-10-25 13:13:45 -0700

    Dev Demo: XYLabels usage example
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/331

--- packages/dev-demo/src/examples/atomic-angular-wrapper/atomic-angular-wrapper.component.ts
+++ packages/dev-demo/src/examples/atomic-angular-wrapper/atomic-angular-wrapper.component.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
 import { Component, ViewChild } from '@angular/core'
 import { VisCrosshairComponent, VisTooltipComponent } from '@volterra/vis-angular'
+import { XYLabels, XYLabelCluster } from '@volterra/vis'
 
 // Helpers
 import { sampleSeriesData, SampleDatum } from '../../utils/data'
@@ -8,9 +9,8 @@ import { sampleSeriesData, SampleDatum } from '../../utils/data'
 @Component({
   selector: 'atomic-angular-wrapper',
   templateUrl: './atomic-angular-wrapper.component.html',
-  styleUrls: ['./atomic-angular-wrapper.component.css'],
+  styleUrls: ['./atomic-angular-wrapper.component.scss'],
 })
-
 export class AtomicAngularWrapperComponent {
   @ViewChild(VisCrosshairComponent, { static: false }) crosshairRef: VisCrosshairComponent<SampleDatum>
   @ViewChild(VisTooltipComponent, { static: false }) tooltipRef: VisTooltipComponent
@@ -19,8 +19,7 @@ export class AtomicAngularWrapperComponent {
 
   // Data
   data: SampleDatum[] = sampleSeriesData(100)
-  margin = { top: 10, bottom: 10, left: 10, right: 10 }
-  padding = {}
+  margin = { top: 10, bottom: 30, left: 10, right: 10 }
   palette = ['#9874f8', '#ffb541']
   crosshairTemplate = (d: SampleDatum): string => `${new Date(d.timestamp).toLocaleDateString()}`
   formatXTicks = (timestamp: number): string => (new Date(timestamp)).toLocaleDateString()
@@ -31,6 +30,17 @@ export class AtomicAngularWrapperComponent {
     (d: SampleDatum): number => -d.y3,
     (d: SampleDatum): number => -d.y1,
   ]
+
+  alerts: { timestamp: number; label: string }[] = Array(10).fill(null).map(() => ({
+    timestamp: this.data[Math.floor(Math.random() * this.data.length)].timestamp,
+    label: '',
+  }))
+
+  labelAccessor = (d: { timestamp: number; label: string }): string => d.label
+  triggers = {
+    [XYLabels.selectors.label]: (d: SampleDatum) => d.timestamp.toString(),
+    [XYLabels.selectors.cluster]: (data: XYLabelCluster<SampleDatum>) => `${data.records.length} alerts`,
+  }
 
   onBrushStart = (): void => {
     this.crosshairRef?.component.hide()
