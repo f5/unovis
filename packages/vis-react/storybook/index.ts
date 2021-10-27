c5a503f914434b32fa12b165837290165416bf14
Nikita Rokotyan <nikita@f5.com>
2021-10-27 16:15:13 -0700

    Vis | React: Wrapper generator fix
    ref #128

---
+++ packages/vis-react/storybook/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Volterra, Inc. All rights reserved.
+
+// Having a *.ts file inside the `storybook` folder is somehow required by rollup-plugin-typescript2 when running
+// the `npm run generate` command. I couldn't find the reason for that so I just gave up and added this dummy index.ts
+// here. Eventually it should be removed.
