37d29744e073a444cb54418dfa62ea2798289cec
Nikita Rokotyan <nikita@f5.com>
2021-10-28 08:06:54 -0700

    Dev Demo | Atomic Angular Wrapper: Adding `vis-bullet-legend`
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/264

--- packages/dev-demo/src/examples/atomic-angular-wrapper/atomic-angular-wrapper.module.ts
+++ packages/dev-demo/src/examples/atomic-angular-wrapper/atomic-angular-wrapper.module.ts
@@ -10,10 +10,11 @@ import {
   VisCrosshairModule,
   VisFreeBrushModule,
   VisXYLabelsModule,
+  VisBulletLegendModule,
 } from '@volterra/vis-angular'
 
 @NgModule({
-  imports: [VisXYContainerModule, VisAreaModule, VisLineModule, VisAxisModule, VisCrosshairModule, VisTooltipModule, VisFreeBrushModule, VisXYLabelsModule],
+  imports: [VisXYContainerModule, VisAreaModule, VisLineModule, VisAxisModule, VisCrosshairModule, VisTooltipModule, VisFreeBrushModule, VisXYLabelsModule, VisBulletLegendModule],
   declarations: [AtomicAngularWrapperComponent],
   exports: [AtomicAngularWrapperComponent],
 })
