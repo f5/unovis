95cfd7604fad1f54d58b4d09fc2440a799471950
Nikita Rokotyan <nikita@f5.com>
2021-12-01 15:14:44 -0800

    Vis | Core: Exporting the ContainerCore class and ContainerConfigInterface
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/355

--- packages/vis/src/containers.ts
+++ packages/vis/src/containers.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Volterra, Inc. All rights reserved.
-
+export { ContainerCore } from './core/container'
+export { ContainerConfigInterface } from './core/container/config'
 export { SingleChart } from './containers/single-chart'
 export { XYContainer } from './containers/xy-container'
 export { XYContainerConfigInterface } from './containers/xy-container/config'
