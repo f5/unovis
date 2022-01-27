8ca922b32c363c5f083de0fbdcf5d13cb4e1db53
Nikita Rokotyan <nikita@f5.com>
2022-01-27 18:42:16 +0500

    Project | Build: Returning vis-react postinstall script to fix the CI build
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/378

--- postinstall.sh
+++ postinstall.sh
@@ -3,3 +3,9 @@ npm run build:lib
 
 echo "Updating licenses"
 npm run gather-licenses
+
+# We need to install @volterra/vis-react dependencies separately otherwise Storybook won't run and build
+echo "Installing @volterra/vis-react dependencies"
+cd packages/vis-react
+npm i --silent
+rm -rf node_modules/@volterra/vis
