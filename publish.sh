e16b2cc66e89711b030a3d8487cefdb811455481
Nikita Rokotyan <nikita@f5.com>
2022-02-07 12:18:13 -0800

    Project: Updating the `publish:all` script execution
    ref #0

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
-npm version patch --workspaces
+npm version patch --workspaces &&
 
-npm run publish:lib
+npm run publish:lib &&
 
-npm i @volterra/vis --save-peer --workspace=@volterra/vis-angular --workspace=@volterra/vis-react --force
+npm i @volterra/vis --save-peer --workspace=@volterra/vis-angular --workspace=@volterra/vis-react --force &&
 
-npm run publish:vis-angular-lib
-npm run publish:vis-react-lib
+npm run publish:vis-angular-lib &&
+npm run publish:vis-react-lib &&
 
-version=$(node -e "console.log(require('./packages/vis/package.json').version)")
+version=$(node -e "console.log(require('./packages/vis/package.json').version)") &&
 
-git add .
+git add . &&
 git commit -m "Release: v${version}" -m "ref #129"
