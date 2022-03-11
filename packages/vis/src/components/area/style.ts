16ebae293fc80d504c2d71c251762e36edb57e56
Nikita Rokotyan <nikita@f5.com>
2022-03-11 09:40:31 -0800

    Vis | Component | Area: Removing useless CSS-variables
    ref https://gitlab.com/volterra/ves.io/volterra-vis/-/issues/411

--- packages/vis/src/components/area/style.ts
+++ packages/vis/src/components/area/style.ts
@@ -4,15 +4,12 @@ import { css, injectGlobal } from '@emotion/css'
 export const global = injectGlobal`
   :root {
     --vis-area-cursor: default;
-    --vis-area-fill: var(--vis-color-main);
     --vis-area-fill-opacity: 1;
-    --vis-area-stroke: none;
     --vis-area-stroke-width: 0px;
     --vis-area-stroke-dasharray: none;
     --vis-area-stroke-opacity: 1;
     --vis-area-hover-fill-opacity: var(--vis-area-fill-opacity);
     --vis-area-hover-stroke-width: var(--vis-area-stroke-width);
-    --vis-area-hover-stroke: var(--vis-area-stroke);
   }
 `
 
@@ -23,9 +20,7 @@ export const root = css`
 export const area = css`
   label: area;
   cursor: var(--vis-area-cursor);
-  fill: var(--vis-area-fill);
   fill-opacity: var(--vis-area-fill-opacity);
-  stroke: var(--vis-area-stroke);
   stroke-width: var(--vis-area-stroke-width);
   stroke-dasharray: var(--vis-area-stroke-dasharray);
   stroke-opacity: var(--vis-area-stroke-opacity);
@@ -33,6 +28,5 @@ export const area = css`
   &:hover {
     fill-opacity: var(--vis-area-hover-fill-opacity);
     stroke-width: var(--vis-area-hover-stroke-width);
-    stroke: var(--vis-area-hover-stroke);
   }
 `
