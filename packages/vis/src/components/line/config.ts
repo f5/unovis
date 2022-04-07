a12fd770d61fde834779bcea2b0a5e2412c03475
Nikita Rokotyan <nikita@f5.com>
2022-04-07 13:51:57 -0700

    Vis | Component | Line: Handling `null` values more carefully, and more (see below)
    • Handling `null` values more carefully,
    • Renaming `noDataValue` → `fallbackValue`
    • Setting `highlightOnHover` to `false` by default
    • Better descriptions in `LineConfigInterface`
    
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/21

--- packages/vis/src/components/line/config.ts
+++ packages/vis/src/components/line/config.ts
@@ -6,15 +6,20 @@ import { CurveType } from 'types/curve'
 import { GenericAccessor, StringAccessor } from 'types/accessor'
 
 export interface LineConfigInterface<Datum> extends XYComponentConfigInterface<Datum> {
-  /** Curve type from the CurveType enum */
+  /** Curve type from the CurveType enum. Default: `CurveType.MonotoneX` */
   curveType?: CurveType;
-  /** Line width in pixels */
+  /** Line width in pixels. Default: `2` */
   lineWidth?: number;
   /** Line dash array, see SVG's stroke-dasharray. Default: `undefined` */
   lineDashArray?: GenericAccessor<number[], Datum>;
-  /** Value to be used in case of no data */
-  noDataValue?: number | null;
-  /** Highlight line on hover */
+  /** When a data point has an `undefined`, `NaN`, or other no-data value, they'll be replaced with a value specified here.
+   * Setting this property to `undefined` will lead to having the line break when there's no data, and continue when
+   * the data appears again. If you set it to `null`, the values will be treated as numerical `0` values and the line
+   * won't break; however if the whole dataset consists of only `null`s, the line won't be displayed.
+   * Default: `undefined`
+  */
+  fallbackValue?: number | undefined | null;
+  /** Highlight line on hover. Default: `false` */
   highlightOnHover?: boolean;
   /** Optional link cursor. Default: `null` */
   cursor?: StringAccessor<Datum[]>;
@@ -24,7 +29,7 @@ export class LineConfig<Datum> extends XYComponentConfig<Datum> implements LineC
   curveType = CurveType.MonotoneX
   lineWidth = 2
   lineDashArray = undefined
-  noDataValue = null
-  highlightOnHover = true
+  fallbackValue = undefined
+  highlightOnHover = false
   cursor = null
 }
