e475a477ed193565a78f8cd839da749597e462e2
Rebecca Bol <r.bol@f5.com>
2022-04-22 12:21:36 -0700

    remove old files ref: #0

--- packages/website/docs/components/index.ts
+++
@@ -1,8 +0,0 @@
-import './styles.css'
-import './custom.css'
-
-export { XYCompositeDoc } from './wrappers/composite-wrapper'
-export { DynamicXYWrapper } from './wrappers/dynamic.wrapper'
-export { XYDocTabs } from './wrappers/doc-tabs.wrapper'
-export { XYWrapper } from './wrappers/xy-wrapper'
-export { XYWrapperWithInput } from './wrappers/input.wrapper'
