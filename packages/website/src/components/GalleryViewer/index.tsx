1b39eb39b204f3ce6a658975d4b15b538688c54f
Nikita Rokotyan <nikita@f5.com>
2022-05-10 14:51:49 -0700

    Website | Gallery: View Typescript version of the example code
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/72

--- packages/website/src/components/GalleryViewer/index.tsx
+++ packages/website/src/components/GalleryViewer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Tabs from '@theme/Tabs'
 import TabItem from '@theme/TabItem'
 import CodeBlock from '@theme/CodeBlock'
@@ -12,13 +12,19 @@ import s from './styles.module.css'
 
 export type GalleryViewerProps = {
   example: Example;
+  useTypescriptCode?: boolean;
 }
 
-export function GalleryViewer ({ example }: GalleryViewerProps): JSX.Element {
+export function GalleryViewer ({ example, useTypescriptCode }: GalleryViewerProps): JSX.Element {
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    if (useTypescriptCode) require(`../../examples/${example.pathname}/${example.pathname}.ts`)
+  })
+
   return (<div className={s.root}>
     <div className={s.title}><h1>{example.title}</h1></div>
-    <div className={s.example}>
-      {example.component()}
+    <div className={s.example} id="#vis-container">
+      {!useTypescriptCode && example.component()}
     </div>
     <div className={s.description}>{example.description}</div>
     <div className={s.codeBlock}>
