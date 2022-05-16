57ddee031e763c3b2ff500a578fa94086772dd36
Nikita Rokotyan <nikita@f5.com>
2022-05-16 11:21:43 -0700

    Vis | Types | Map: Removing MapPoint and MapArea types
    * Removing `MapPoint` and `MapArea` types because we don't have such constrains, they can be pure generics.
    * The `MapLink` type represents already processed (by the data model) link data with populates `source` and `target` properties.
    * The `MapData` type is already defined in `components/topojson-map/types.ts` but needed to be exported.
    
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/10

--- packages/vis/src/types/map.ts
+++ packages/vis/src/types/map.ts
@@ -1,16 +1,5 @@
-import { GenericDataRecord } from '../types/data'
-
 export type MapLink<PointDatum, LinkDatum> = LinkDatum & {
   source: PointDatum;
   target: PointDatum;
-}
-
-type MapPoint = GenericDataRecord & { id: string; latitude: number; longitude: number }
-type MapArea = { id: string } & GenericDataRecord
-
-export type MapData = {
-  points?: MapPoint[];
-  links?: MapLink<(number | string | MapPoint), any>[];
-  areas?: MapArea[];
 }
 
