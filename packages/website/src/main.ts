0d1b81baae49b3124386e07319e146854b72ad23
Nikita Rokotyan <nikita@f5.com>
2022-05-19 13:33:40 -0700

    Website | Gallery: Simple Angular examples viewer using ngc-esbuild
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/75

---
+++ packages/website/src/main.ts
@@ -0,0 +1,42 @@
+/* This file is for the Angular Gallery Viewer only. See package.json -> scripts -> gallery:angular */
+import { NgModule, Component } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { BasicGroupedBarModule } from './examples/basic-grouped-bar/basic-grouped-bar.module'
+import { BasicLeafletMapModule } from './examples/basic-leaflet-map/basic-leaflet-map.module'
+import { BasicLineChartModule } from './examples/basic-line-chart/basic-line-chart.module'
+import { MultiLineChartModule } from './examples/multi-line-chart/multi-line-chart.module'
+import { TopojsonMapModule } from './examples/topojson-map/topojson-map.module'
+import { StackedBarChartModule } from './examples/horizontal-stacked-bar-chart/horizontal-stacked-bar-chart.module'
+
+@Component({
+  selector: 'app-component',
+  template: `
+    <basic-grouped-bar></basic-grouped-bar>
+    <basic-line-chart></basic-line-chart>
+    <basic-leaflet-map></basic-leaflet-map>
+    <multi-line-chart></multi-line-chart>
+    <topojson-map></topojson-map>
+    <horizontal-stacked-bar-chart></horizontal-stacked-bar-chart>
+  `,
+})
+export class AppComponent {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor () {}
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule, BasicGroupedBarModule, BasicLeafletMapModule, BasicLineChartModule, MultiLineChartModule,
+    TopojsonMapModule, StackedBarChartModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor () {}
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule, { ngZone: 'noop' })
