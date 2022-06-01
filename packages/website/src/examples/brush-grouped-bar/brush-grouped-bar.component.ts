fd8b3f2f1cba745838191efda16c3d6ec9c74f34
Nikita Rokotyan <nikita@f5.com>
2022-06-01 13:45:50 -0700

    Website | Gallery | Brush Grouped Bar: Configuration tweaks
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/52

--- packages/website/src/examples/brush-grouped-bar/brush-grouped-bar.component.ts
+++ packages/website/src/examples/brush-grouped-bar/brush-grouped-bar.component.ts
@@ -12,20 +12,27 @@ export class BrushGroupedBarComponent {
   data = data
   domain = [1980, 1990]
   margin: { top: 20, left: 60 }
+  duration: number | undefined
+  numTicks = 10
 
   legendItems: LegendItem[] = groups.map(g => ({ ...g, inactive: false }))
 
   x = (d: DataRecord) => d.year
   y = this.legendItems.map((i: LegendItem) => (d: DataRecord) => i.inactive ? null : d[i.key])
 
+  updateDomain = (selection: [number, number], _, userDriven: boolean): void => {
+    if (userDriven) {
+      this.numTicks = Math.min(15, this.domain[1] - this.domain[0])
+      this.duration = 0 // We set duration to 0 to update the main chart immediately (without animation) after the brush event
+      this.domain = selection
+    }
+  }
+
   updateItems = (item: LegendItem, index: number): void => {
     item.inactive = !item.inactive
     this.legendItems = [...this.legendItems]
     this.legendItems[index].inactive = item.inactive
+    this.duration = undefined // Enabling default animation duration for legend interactions
     this.y = this.legendItems.map((i: LegendItem) => (d: DataRecord) => i.inactive ? null : d[i.key])
-  }
-
-  updateDomain = (selection: [number, number]): void => {
-    if (selection) this.domain = selection
   }
 }
--- packages/website/src/examples/brush-grouped-bar/brush-grouped-bar.tsx
+++ packages/website/src/examples/brush-grouped-bar/brush-grouped-bar.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { BulletLegendItemInterface } from '@volterra/vis'
 import { VisXYContainer, VisAxis, VisBrush, VisGroupedBar, VisBulletLegend } from '@volterra/vis-react'
 import { data, groups, GroupItem, DataRecord } from './data'
@@ -7,32 +7,39 @@ type LegendItem = BulletLegendItemInterface & GroupItem
 const legendItems: LegendItem[] = groups.map(g => ({ ...g, inactive: false }))
 
 export default function BrushGroupedBar (): JSX.Element {
-  const [domain, setDomain] = useState([1980, 1990])
+  const [domain, setDomain] = useState<[number, number]>([1980, 1990])
+  const [duration, setDuration] = useState<number | undefined>(undefined)
   const [items, setItems] = useState(legendItems)
   const x = useCallback((d: DataRecord) => d.year, [])
-  const y = items.map(i => useCallback((d: DataRecord) => i.inactive ? null : d[i.key], [items]))
+  const y = useMemo(() =>
+    items.map(i => (d: DataRecord) => i.inactive ? null : d[i.key])
+  , [items])
 
-  function updateItems (item: LegendItem, i: number): void {
+  const updateDomain = useCallback((selection: [number, number], _, userDriven: boolean) => {
+    if (userDriven) {
+      setDuration(0) // We set duration to 0 to update the main chart immediately (without animation) after the brush event
+      setDomain(selection)
+    }
+  }, [])
+
+  const updateItems = useCallback((item: LegendItem, i: number) => {
     const newItems = [...items]
     newItems[i] = { ...item, inactive: !item.inactive }
+    setDuration(undefined) // Enabling default animation duration for legend interactions
     setItems(newItems)
-  }
-
-  function updateDomain (selection: [number, number], _, userDriven: boolean): void {
-    if (userDriven) setDomain(selection)
-  }
+  }, [items])
 
   return (
     <>
       <VisBulletLegend items={items} onLegendItemClick={updateItems} />
-      <VisXYContainer data={data} height={300} xDomain={domain} scaleByDomain={true}>
+      <VisXYContainer duration={duration} data={data} height={300} xDomain={domain} scaleByDomain={true}>
         <VisGroupedBar x={x} y={y} groupPadding={0.2} roundedCorners barMinHeight={0} />
-        <VisAxis type='x' label='Year' numTicks={domain[1] - domain[0]} gridLine={false}/>
+        <VisAxis type='x' label='Year' numTicks={Math.min(15, domain[1] - domain[0])} gridLine={false}/>
         <VisAxis type='y' label='Cereal Production (metric tons, millions)'/>
       </VisXYContainer>
-      <VisXYContainer data={data} height={75} margin={{ top: 20 }} >
-        <VisGroupedBar x={x} y={y} roundedCorners/>
-        <VisBrush selection={domain} onBrushMove={updateDomain} draggable={true}/>
+      <VisXYContainer data={data} height={75} >
+        <VisGroupedBar x={x} y={y}/>
+        <VisBrush selection={domain} onBrush={updateDomain} draggable={true}/>
         <VisAxis type='x' numTicks={15}/>
       </VisXYContainer>
     </>
