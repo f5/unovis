007bb7565e09168663b028d435977466003b5c63
Nikita Rokotyan <nikita@f5.com>
2022-06-27 15:22:19 -0700

    Website | Svelte Gallery: Renaming related source files to `svelte-gallery.*`
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/90

--- packages/website/rollup.config.js
+++ packages/website/rollup.config.js
@@ -3,21 +3,17 @@ import resolve from '@rollup/plugin-node-resolve'
 import sveltePreprocess from 'svelte-preprocess'
 import transformPaths from '@zerollup/ts-transform-paths'
 import typescript from 'rollup-plugin-typescript2'
-import sveld from 'sveld'
 import devServer from 'rollup-plugin-dev'
 import copy from 'rollup-plugin-copy'
 import commonjs from 'rollup-plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 
-
 export default {
-  input: ['src/index.ts'],
+  input: ['src/svelte-gallery.ts'],
   output: {
     dir: 'dist-demo',
     sourcemap: true,
     format: 'esm',
-    preserveModules: true,
-    preserveModulesRoot: './src',
   },
   plugins: [
     postcss({
@@ -45,15 +41,10 @@ export default {
       abortOnError: false,
       transformers: [(service) => transformPaths(service.getProgram())],
     }),
-    sveld({
-      typesOptions: {
-        outDir: 'dist-demo',
-      },
-    }),
     devServer({ dirs: ['dist-demo'], port: 9200 }),
     copy({
       targets: [
-        { src: 'src/index.html', dest: 'dist-demo' },
+        { src: 'src/svelte-gallery.html', dest: 'dist-demo', rename: '/index.html' },
       ],
     }),
   ],
--- packages/website/src/svelte-gallery.ts
+++ packages/website/src/svelte-gallery.ts
@@ -1,5 +1,4 @@
-import App from './App.svelte'
-
+import App from './svelte-gallery.svelte'
 
 const index = new App({
   target: document.body,
