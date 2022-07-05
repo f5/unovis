979563f08d285b4f5a85c90171b7a00bd7d4c4c8
Rebecca Bol <r.bol@f5.com>
2022-07-05 13:37:54 -0700

    Graph docs wrapper ref: #97

--- packages/website/docs/wrappers/base/doc-tabs.tsx
+++ packages/website/docs/wrappers/base/doc-tabs.tsx
@@ -183,6 +183,10 @@ export function DocFrameworkTabs ({
     declarations.data = `data: ${dataType.includes(',') ? `${dataType.split(/(?=[A-Z])/)[0]}Data` : `${dataType}[]`}`
   }
 
+  if (showData) {
+    declarations.data = `data: ${dataType.includes(',') ? `${dataType.split(/(?=[A-Z])/)[0]}Data` : `${dataType}[]`}`
+  }
+
   const importedProps = imports ? Object.values(imports).flatMap(i => i) : []
   const tabConfig = {
     container: (context === ContextLevel.Container || context === ContextLevel.Full) && {
