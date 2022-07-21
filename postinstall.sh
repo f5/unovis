f46974b510f53b85443aac70e63323f5186e0b17
Nikita Rokotyan <nikita@f5.com>
2022-07-21 13:29:48 -0700

    Project: Updating publishing scripts

--- postinstall.sh
+++ postinstall.sh
@@ -7,8 +7,6 @@ npm run build:vis-angular-lib
 npm run build:vis-react-lib
 npm run build:vis-svelte-lib
 
-echo "Updating licenses"
-npm run gather-licenses
 
 # We need to install @volterra/website dependencies separately otherwise it won't run and build
 echo "Installing @volterra/website dependencies"
@@ -16,3 +14,6 @@ cd packages/website
 npm i --silent
 rm -rf node_modules/react
 rm -rf node_modules/react-dom
+
+echo "Updating licenses"
+npm run gather-licenses
