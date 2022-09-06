eb1a02390af25bf87d94333497648c5c9866860f
Nikita Rokotyan <nikita@f5.com>
2022-09-06 08:09:57 -0700

    Vis React | Wrapper Generator: Adding ref support
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/124

--- packages/vis-react/autogen/component.ts
+++ packages/vis-react/autogen/component.ts
@@ -12,7 +12,7 @@ export function getComponentCode (
     ? `<${generics?.map(g => g.name + (g.extends ? ` extends ${g.extends}` : '') + (g.default ? ` = ${g.default}` : '')).join(', ')}>`
     : ''
   return `// !!! This code was automatically generated. You should not change it !!!
-import React, { useEffect, useRef, useState } from 'react'
+import React, { ForwardedRef, Ref, useImperativeHandle, useEffect, useRef, useState } from 'react'
 ${importStatements.map(s => `import { ${s.elements.join(', ')} } from '${s.source}'`).join('\n')}
 
 // Utils
@@ -21,10 +21,17 @@ import { arePropsEqual } from 'src/utils/react'
 // Types
 import { VisComponentElement } from 'src/types/dom'
 
-export type Vis${componentName}Props${genericsDefStr} = ${componentName}ConfigInterface${genericsStr} & { data?: ${dataType} }
+export type Vis${componentName}Ref${genericsDefStr} = {
+    component: ${componentName}${genericsStr}
+}
+
+export type Vis${componentName}Props${genericsDefStr} = ${componentName}ConfigInterface${genericsStr} & {
+  data?: ${dataType};
+  ref?: Ref<Vis${componentName}Ref${genericsStr}>
+}
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-function Vis${componentName}FC${genericsDefStr} (props: Vis${componentName}Props${genericsStr}): JSX.Element {
+function Vis${componentName}FC${genericsDefStr} (props: Vis${componentName}Props${genericsStr}, fRef: ForwardedRef<Vis${componentName}Ref${genericsStr}>): JSX.Element {
   const ref = useRef<VisComponentElement<${componentName}${genericsStr}>>(null)
   const [component] = useState<${componentName}${genericsStr}>(new ${componentName}(props))
 
@@ -39,11 +46,12 @@ function Vis${componentName}FC${genericsDefStr} (props: Vis${componentName}Props
     component?.setConfig(props)
   })
 
+  useImperativeHandle(fRef, () => ({ component }))
   return <vis-${elementSuffix} ref={ref} />
 }
 
 // We export a memoized component to avoid unnecessary re-renders
 //  and define its type explicitly to help react-docgen-typescript to extract information about props
-export const Vis${componentName}: (${genericsDefStr}(props: Vis${componentName}Props${genericsStr}) => JSX.Element | null) = React.memo(Vis${componentName}FC, arePropsEqual)
+export const Vis${componentName}: (${genericsDefStr}(props: Vis${componentName}Props${genericsStr}) => JSX.Element | null) = React.memo(React.forwardRef(Vis${componentName}FC), arePropsEqual)
 `
 }
