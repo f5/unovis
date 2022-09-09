60f0b34f54e0aa9cf8b2aba710df1413f2561ac9
Nikita Rokotyan <nikita@f5.com>
2022-09-09 09:25:03 -0700

    Project: Publish config clean up
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/1

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 echo "Activating git hooks"
 npx husky install
 
-echo "Building @unovis/ts"
+echo "Building @unovis"
 npm run build:lib
 npm run build:vis-angular-lib
 npm run build:vis-react-lib
@@ -9,11 +9,12 @@ npm run build:vis-svelte-lib
 
 
 # We need to install @unovis/website dependencies separately otherwise it won't run and build
-echo "Installing @unovis/website dependencies"
+echo "🟡 Installing @unovis/website dependencies"
 cd packages/website
 npm i --silent
 rm -rf node_modules/react
 rm -rf node_modules/react-dom
 
 echo "📄 Updating licenses (will be done in the background)"
+cd ../../
 npm run gather-licenses > /dev/null 2>&1 &
