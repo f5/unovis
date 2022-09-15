168d8e825a1dbfd0e47762cfaffac60d09515e2d
Rebecca Bol <r.bol@f5.com>
2022-09-15 16:08:43 -0700

    Vis Svelte | Delete demo app and simplify package ref: #127

--- packages/vis-svelte/rollup.config.js
+++ packages/vis-svelte/rollup.config.js
@@ -1,18 +1,14 @@
-import svelte from 'rollup-plugin-svelte'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
-import sveltePreprocess from 'svelte-preprocess'
 import transformPaths from '@zerollup/ts-transform-paths'
+import commonjs from 'rollup-plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import svelte from 'rollup-plugin-svelte'
+import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 import sveld from 'sveld'
-import devServer from 'rollup-plugin-dev'
-import copy from 'rollup-plugin-copy'
-import commonjs from 'rollup-plugin-commonjs'
+import sveltePreprocess from 'svelte-preprocess'
 
 import pkg from './package.json'
-
-const dev = process.env.ROLLUP_WATCH
 
 // Excluded dependencies
 const externals = Object.keys(pkg.devDependencies)
@@ -24,13 +20,13 @@ const regexesOfPackages = externals // To prevent having node_modules in the bui
 const extensions = ['.js', '.ts', '.svelte', '.css']
 
 export default {
-  input: [dev ? 'src-demo/index.ts' : 'src/index.ts'],
+  input: 'src/index.ts',
   output: {
-    dir: dev ? 'dist-demo' : 'dist',
+    dir: 'dist',
     sourcemap: true,
     format: 'esm',
     preserveModules: true,
-    preserveModulesRoot: dev ? './src-demo' : './src',
+    preserveModulesRoot: './src',
   },
   plugins: [
     commonjs(),
@@ -42,10 +38,7 @@ export default {
     }),
     svelte({
       emitCss: false,
-      preprocess: sveltePreprocess({ sourceMap: dev }),
-      compilerOptions: {
-        dev: dev,
-      },
+      preprocess: sveltePreprocess(),
     }),
     typescript({
       typescript: require('typescript'),
@@ -53,15 +46,11 @@ export default {
     }),
     sveld({
       typesOptions: {
-        outDir: dev ? 'dist-demo' : 'dist',
+        outDir: 'dist',
       },
     }),
-    dev ? devServer({ dirs: ['dist-demo'], port: 9200 }) : terser(),
-    dev ? copy({
-      targets: [
-        { src: 'src-demo/index.html', dest: 'dist-demo' },
-      ],
-    }) : peerDepsExternal(),
+    terser(),
+    peerDepsExternal(),
   ],
-  external: dev ? undefined : regexesOfPackages,
+  external: regexesOfPackages,
 }
--- packages/vis-svelte/src-demo/index.ts
+++
@@ -1,7 +0,0 @@
-import App from './App.svelte'
-
-const index = new App({
-  target: document.body,
-})
-
-export { index }
