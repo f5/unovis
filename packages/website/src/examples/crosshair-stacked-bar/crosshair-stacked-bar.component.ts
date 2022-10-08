c0b2957642c2a028cc92088a64ed56e50fa67d97
Rebecca Bol <r.bol@f5.com>
2022-10-07 18:02:45 -0700

    Website | Gallery: Crosshair example
    ref https://gitswarm.f5net.com/f5-innersourcing/unovis/-/issues/148

---
+++ packages/website/src/examples/crosshair-stacked-bar/crosshair-stacked-bar.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core'
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'
+import { data, labels, DataRecord, FormatConfig } from './data'
+
+@Component({
+  selector: 'crosshair-stacked-bar',
+  templateUrl: './crosshair-stacked-bar.component.html',
+})
+export class CrosshairStackedBarComponent {
+  height = 500
+  data = data
+  x = (d: DataRecord): number => d.year
+  y = labels.map((l: FormatConfig) => (d: DataRecord): number => d[l.format])
+
+  legendHTML: SafeHtml
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.legendHTML = sanitizer.bypassSecurityTrustHtml(
+      labels.map(l => `<span style="margin-right: 10px;">${this.getIcon(l)}</span>`).join('')
+    )
+  }
+
+  getIcon = (f: FormatConfig): string => {
+    return `<span class="bi bi-${f.icon}" style="color:${f.color}; margin: 0 4px"></span>${f.label}`
+  }
+
+  tooltipTemplate = (d: DataRecord): string => {
+    const numberFormat = Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      notation: 'compact',
+    }).format
+    const dataLegend = labels.filter(f => d[f.format] > 0)
+      .reverse()
+      .map(f => `<span>${this.getIcon({ ...f, label: numberFormat(d[f.format] * Math.pow(10, 10) ) })}`)
+      .join('</span>')
+    return `<div><b>${d.year}</b>: ${dataLegend}</div>`
+  }
+}
---
+++ packages/website/src/examples/crosshair-stacked-bar/crosshair-stacked-bar.svelte
@@ -0,0 +1,35 @@
+<script lang='ts'>
+  import { Position } from '@unovis/ts'
+  import { VisAxis, VisCrosshair, VisStackedBar, VisTooltip,  VisXYContainer } from '@unovis/svelte'
+  import { data, labels, DataRecord, FormatConfig } from './data'
+
+  const height = 500
+  const x = (d: DataRecord) => d.year
+  const y= labels.map((l: FormatConfig) => (d: DataRecord) => d[l.format])
+
+  function getIcon (f: FormatConfig): string {
+    return `<span class="bi bi-${f.icon}" style="color:${f.color}; margin:0 4px"></span>${f.label}\t`
+  }
+
+  function tooltipTemplate (d: DataRecord): string {
+    const numberFormat = Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      notation: 'compact'
+    }).format
+    const dataLegend = labels.filter(f => d[f.format] > 0)
+      .reverse()
+      .map(f => `<span>${getIcon({ ...f, label: numberFormat(d[f.format] * Math.pow(10, 10) ) })}`)
+      .join('</span>')
+    return `<div><b>${d.year}</b>: ${dataLegend}</div>`
+  }
+</script>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/>
+<div>{@html labels.map(l => `<span style="margin-right: 20px">${getIcon(l)}</span>`).join('')}</div>
+<VisXYContainer {data} {height}>
+  <VisStackedBar {x} {y}/>
+  <VisCrosshair template={tooltipTemplate}/>
+  <VisTooltip verticalShift={height} horizontalPlacement={Position.Center}/>
+  <VisAxis type="x" />
+</VisXYContainer>
