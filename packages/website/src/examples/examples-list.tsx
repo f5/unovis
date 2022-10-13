288e3500ef79baed8a5ba6b135f4b2243f02f21b
Rebecca Bol <r.bol@f5.com>
2022-10-12 17:55:05 -0700

    Website | Gallery: Step area chart example ref: #139

--- packages/website/src/examples/examples-list.tsx
+++ packages/website/src/examples/examples-list.tsx
@@ -65,6 +65,7 @@ export const examples: ExampleCollection[] = [
       require('./crosshair-stacked-bar').default,
       require('./brush-grouped-bar').default,
       require('./free-brush-scatters').default,
+      require('./advanced-tooltip').default,
     ],
   },
   /* {} */
