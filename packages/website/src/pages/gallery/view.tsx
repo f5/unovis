1883e779f92f0bea5fe2052ee1fe01a084707cb4
Nikita Rokotyan <nikita@f5.com>
2022-12-06 10:05:21 -0800

    Website | Gallery | Viewer: Tweaking the description

--- packages/website/src/pages/gallery/view.tsx
+++ packages/website/src/pages/gallery/view.tsx
@@ -17,7 +17,7 @@ export default function Home (): JSX.Element {
   return (
     <Layout
       title="Gallery"
-      description="A modular data visualization framework for React, Angular and vanilla TypeScript"
+      description="A modular data visualization framework for React, Angular, Svelte, and vanilla TypeScript or JavaScript"
     >
       <BrowserOnly>{() => {
         const location = useLocation()
