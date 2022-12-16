afb4aa69d7a0185314fc5a110f03a310ea0b5902
Rebecca Bol <r.bol@f5.com>
2022-12-16 12:45:36 -0800

    @unovis/dev: Adding Timeline example

--- packages/dev/src/utils/data.ts
+++ packages/dev/src/utils/data.ts
@@ -4,6 +4,12 @@ export type XYDataRecord = {
   y1: number;
   y2: number;
 }
+export interface TimeDataRecord {
+  timestamp: number;
+  value: number;
+  length: number;
+  type?: string;
+}
 
 export function generateXYDataRecords (n = 10): XYDataRecord[] {
   return Array(n).fill(0).map((_, i) => ({
@@ -11,5 +17,15 @@ export function generateXYDataRecords (n = 10): XYDataRecord[] {
     y: 5 + 5 * Math.random(),
     y1: 1 + 3 * Math.random(),
     y2: 2 * Math.random(),
+  }))
+}
+
+export function generateTimeSeries (n = 10, types = n): TimeDataRecord[] {
+  const groups = Array(types).fill(0).map((_, i) => String.fromCharCode(i + 65))
+  return Array(n).fill(0).map((_, i: number) => ({
+    timestamp: Date.now() + i * 1000 * 60 * 60 * 24,
+    value: i / 10 + Math.sin(i / 5) + Math.cos(i / 3),
+    length: Math.round(1000 * 60 * 60 * 24) * Math.random(),
+    type: groups[i % groups.length],
   }))
 }
