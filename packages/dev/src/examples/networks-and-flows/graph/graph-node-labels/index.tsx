a0aa0b89bfe0821bed67b5e9d6add051037ca412
Nikita Rokotyan <nikita@f5.com>
2023-02-03 12:49:02 -0800

    Dev | Examples | Graph labels trimming: Adding trim mode and trim length
    #120

--- packages/dev/src/examples/networks-and-flows/graph/graph-node-labels/index.tsx
+++ packages/dev/src/examples/networks-and-flows/graph/graph-node-labels/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { VisSingleContainer, VisGraph } from '@unovis/react'
+import { TrimMode } from '@unovis/ts'
 import { generateNodeLinkData, NodeDatum, LinkDatum } from '@src/utils/data'
 import { sample } from '@src/utils/array'
 
@@ -12,19 +13,28 @@ export const component = (): JSX.Element => {
   const regions = ['Australian', 'South American', 'Siberian', 'European', 'Asian']
   const colors = ['Vermilion', 'Verdigris', 'Bisque', 'Cattleya']
   const animals = ['Elephant', 'Mountain Lion', 'Sea Otter', 'Bear']
+  const trimModes = Object.values(TrimMode)
   const labels = data.nodes.map((d, i) => ({
     label: `${sample(animals)} ${sample(colors)}`,
     subLabel: `${sample(regions)} ${sample(colors)} ${sample(animals)}`,
-    labelTrim: Math.random() > 0.5,
-    subLabelTrim: Math.random() > 0.5,
+    labelTrim: Math.random() > 0.2,
+    labelTrimMode: sample(trimModes),
+    labelTrimLength: Math.round(3 + 12 * Math.random()),
+    subLabelTrim: Math.random() > 0.2,
+    subLabelTrimMode: sample(trimModes),
+    subLabelTrimLength: Math.round(3 + 12 * Math.random()),
   }))
   return (
     <VisSingleContainer data={data} height={600}>
       <VisGraph<NodeDatum, LinkDatum>
         nodeLabel={(_, i) => labels[i].label}
         nodeLabelTrim={(_, i) => labels[i].labelTrim}
+        nodeLabelTrimMode={(_, i) => labels[i].labelTrimMode}
+        nodeLabelTrimLength={(_, i) => labels[i].labelTrimLength}
         nodeSubLabel={(_, i) => labels[i].subLabel}
         nodeSubLabelTrim={(_, i) => labels[i].subLabelTrim}
+        nodeSubLabelTrimMode={(_, i) => labels[i].subLabelTrimMode}
+        nodeSubLabelTrimLength={(_, i) => labels[i].subLabelTrimLength}
       />
     </VisSingleContainer>
   )
