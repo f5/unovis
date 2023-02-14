fbb3e8a93c1c0af6926a9cbc766292899c8dc087
Nikita Rokotyan <nikita@f5.com>
2023-02-14 09:51:44 -0800

    Component | FlowLegend: Configurable arrow padding
    #0

--- packages/ts/src/components/flow-legend/style.ts
+++ packages/ts/src/components/flow-legend/style.ts
@@ -13,6 +13,8 @@ export const globalStyles = injectGlobal`
     --vis-flow-legend-label-color: #71788a;
     --vis-flow-legend-link-color: #E5E9F7;
     --vis-flow-legend-arrow-color: #E5E9F7;
+    --vis-flow-legend-label-padding: '5px 15px';
+    --vis-flow-legend-arrow-padding: '0 10px';
 
     --vis-dark-flow-legend-label-background: #292b34;
     --vis-dark-flow-legend-label-color: #E5E9F7;
@@ -71,7 +73,7 @@ export const label = (labelFontSize: number, labelColor: string): string => css`
   transform: translate(-50%, 0%);
   margin-left: 7px;
   background-color: var(--vis-flow-legend-label-background);
-  padding: 5px 15px;
+  padding: var(--vis-flow-legend-legend-padding);
   font-size: ${labelFontSize}px;
   color: ${labelColor || 'var(--vis-flow-legend-label-color)'};
   display: inline-table;
@@ -84,4 +86,9 @@ export const arrow = (arrowColor: string): string => css`
   font-size: 9px;
   vertical-align: middle;
   color: ${arrowColor || 'var(--vis-flow-legend-arrow-color)'};
+  background-color: var(--vis-flow-legend-label-background);
+  padding: var(--vis-flow-legend-arrow-padding);
+  transform: translate(-25%, 0%);
+  display: inline-table;
+  text-align: center;
 `
