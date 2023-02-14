54d3f934e9e399b771d80a819a99f577db6d4ec2
Nikita Rokotyan <nikita@f5.com>
2023-02-14 09:51:44 -0800

    Component | FlowLegend: Configurable arrow padding

--- packages/ts/src/components/flow-legend/style.ts
+++ packages/ts/src/components/flow-legend/style.ts
@@ -14,8 +14,8 @@ export const globalStyles = injectGlobal`
     --vis-flow-legend-label-color: #71788a;
     --vis-flow-legend-link-color: #E5E9F7;
     --vis-flow-legend-arrow-color: #E5E9F7;
-    --vis-flow-legend-label-padding: '5px 15px';
-    --vis-flow-legend-arrow-padding: '0 10px';
+    --vis-flow-legend-label-padding: 5px 15px;
+    --vis-flow-legend-arrow-padding: 0 10px;
     /* --vis-flow-legend-arrow-font-family: Undefined by default to allow proper fallback to var(DEFAULT_ICON_FONT_FAMILY)*/
 
     --vis-dark-flow-legend-label-background: #292b34;
@@ -75,7 +75,7 @@ export const label = (labelFontSize: number, labelColor: string): string => css`
   transform: translate(-50%, 0%);
   margin-left: 7px;
   background-color: var(--vis-flow-legend-label-background);
-  padding: var(--vis-flow-legend-legend-padding);
+  padding: var(--vis-flow-legend-label-padding);
   font-size: ${labelFontSize}px;
   color: ${labelColor || 'var(--vis-flow-legend-label-color)'};
   display: inline-table;
