2c05a0065b0c4651ef24de92dbfa53bc0390ed9f
Nikita Rokotyan <nikita@f5.com>
2023-02-21 14:30:17 -0800

    Dev | Examples: Vector LeafletMap with show/hide button
    #132

---
+++ packages/dev/src/examples/maps/leaflet/leaflet-map-vector/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react'
+import { VectorSourceSpecification } from 'maplibre-gl'
+import { VisLeafletMap, VisLeafletMapRef } from '@unovis/react'
+import { MapLibreArcticDark, MapLibreArcticLight } from '@unovis/ts'
+
+// Data
+import cities from './cities.json'
+
+// Style
+import s from './style.module.css'
+
+export const title = 'Vector Map'
+export const subTitle = 'Vector rendering with MapLibre'
+export const category = 'Leaflet Map'
+
+type MapPointDatum = typeof cities[0]
+
+export const component = (): JSX.Element => {
+  const mapRef = useRef<VisLeafletMapRef<MapPointDatum> | null>(null)
+  const [isMapVisible, setMapVisible] = useState(true)
+  const mapSources = {
+    sources: {
+      openmaptiles: {
+        type: 'vector',
+        url: `${UNOVIS_MAP_TILE_SERVER_URL}/data/v3.json`,
+      } as VectorSourceSpecification,
+    },
+    glyphs: `${UNOVIS_MAP_TILE_SERVER_URL}/fonts/{fontstack}/{range}.pbf`,
+  }
+
+  const handleClick = (): void => {
+    setMapVisible(!isMapVisible)
+    if (isMapVisible) mapRef.current?.component?.fitView()
+  }
+
+  return (<>
+    <button className={s.showHideButton} onClick={handleClick}>{isMapVisible ? 'Hide' : 'Show'} Map</button>
+    { isMapVisible ? <VisLeafletMap<MapPointDatum>
+      ref={mapRef}
+      data={cities}
+      pointColor={'#3556FF'}
+      pointBottomLabel={d => d.city}
+      style={{ ...MapLibreArcticLight, ...mapSources }}
+      styleDarkTheme={{ ...MapLibreArcticDark, ...mapSources }}
+      attribution={[
+        '<a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>',
+        '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>',
+      ]}
+    /> : null }
+  </>)
+}
---
+++ packages/dev/src/global.d.ts
@@ -0,0 +1,6 @@
+declare global {
+  const UNOVIS_MAP_TILE_SERVER_API_KEY: string
+  const UNOVIS_MAP_TILE_SERVER_URL: string
+}
+
+export {}
