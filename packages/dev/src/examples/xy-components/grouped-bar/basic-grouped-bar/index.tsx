8a0df041cf8606e6f3d3fd5db9908335dd76e5eb
Nikita Rokotyan <nikita@f5.com>
2023-04-04 11:21:06 -0700

    Dev | Examples: GroupedBar example with domain set
    #178

---
+++ packages/dev/src/examples/xy-components/grouped-bar/basic-grouped-bar/index.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from 'react'
+import { VisXYContainer, VisGroupedBar, VisAxis, VisTooltip, VisCrosshair } from '@unovis/react'
+
+import { XYDataRecord, generateXYDataRecords } from '@src/utils/data'
+
+export const title = 'Basic Grouped Bar Chart'
+export const subTitle = 'Generated Data'
+export const category = 'Grouped Bar'
+export const component = (): JSX.Element => {
+  const tooltipRef = useRef(null)
+  const accessors = [
+    (d: XYDataRecord) => d.y,
+    (d: XYDataRecord) => d.y1,
+    (d: XYDataRecord) => d.y2,
+  ]
+
+  return (
+    <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)} margin={{ top: 5, left: 5 }} xDomain={[-1, 15]}>
+      <VisGroupedBar x={d => d.x} y={accessors} />
+      <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`}/>
+      <VisAxis type='y' tickFormat={(y: number) => `${y}`}/>
+      <VisCrosshair template={(d: XYDataRecord) => `${d.x}`}/>
+      <VisTooltip ref={tooltipRef}/>
+    </VisXYContainer>
+  )
+}
