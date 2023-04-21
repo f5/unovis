78b5cfdfc57eeba95a0f5d77a96ae6b8f167194e
Rebecca Bol <r.bol@f5.com>
2023-04-21 16:27:35 -0700

    Website | Gallery: Fix expandable sankey data model #180

--- packages/website/src/examples/expandable-sankey/expandable-sankey.component.ts
+++ packages/website/src/examples/expandable-sankey/expandable-sankey.component.ts
@@ -1,27 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
+import { VisSingleContainerComponent } from '@unovis/angular'
 import { Sankey, SankeyLink, SankeyNode } from '@unovis/ts'
 
-import { getColor, getChildren, LinkDatum, NodeDatum, sankeyData, sourceNode } from './data'
+import { sankeyData, root, Node, Link } from './data'
 
 @Component({
   selector: 'expandable-sankey',
   templateUrl: './expandable-sankey.component.html',
 })
 export class ExpandableSankeyComponent {
-  data = {
-    nodes: [sourceNode, ...getChildren(sourceNode)],
-    links: sankeyData.links,
-  }
+  @ViewChild('vis') vis: VisSingleContainerComponent<{ nodes: Node[]; links: Link[] }>
 
-  nodeColor = getColor
-  nodeIcon = (d: NodeDatum): string => !d.expandable ? '' : (d.expanded ? '-' : '+')
-  nodeCursor = (d: SankeyNode<NodeDatum, LinkDatum>): string => d.expandable ? 'pointer' : null
-  subLabel = (d: SankeyNode<NodeDatum, LinkDatum>): string => {
-    if (d.expanded || d.depth === 0) return ''
-    return `${((d.value / sourceNode.value) * 100).toFixed(1)}%`
-  }
-
-  linkColor = (d: SankeyLink<NodeDatum, LinkDatum>): string => getColor(d.source)
+  data = { nodes: sankeyData.nodes, links: sankeyData.links }
 
   events = {
     [Sankey.selectors.node]: {
@@ -29,19 +19,23 @@ export class ExpandableSankeyComponent {
     },
   }
 
-  toggleGroup (n: SankeyNode<NodeDatum, LinkDatum>): void {
-    if (!n.expandable) return
-    const nodes = [...this.data.nodes]
-    nodes[n.index].expanded = !nodes[n.index].expanded
-    n.sourceLinks.forEach(d => {
-      nodes[d.index].expanded = false
-    })
+  linkColor = (d: SankeyLink<Node, Link>): string => d.source.color ?? null
+  nodeCursor = (d: Node): string => d.expandable ? 'pointer' : null
+  nodeIcon = (d: Node): string => !d.expandable ? '' : (d.expanded ? '-' : '+')
+  subLabel = (d: SankeyNode<Node, Link>): string => {
+    if (d.expanded || d.depth === 0) return ''
+    return `${((d.value / root.value) * 100).toFixed(1)}%`
+  }
 
-    this.data = {
-      nodes: n.expanded
-        ? nodes.filter(d => !d.id.startsWith(n.id) || d.level <= n.layer)
-        : [...nodes, ...getChildren(n)],
-      links: this.data.links,
+  toggleGroup (n: Node): void {
+    if (n.expandable) {
+      if (n.expanded) {
+        sankeyData.collapse(n)
+      } else {
+        sankeyData.expand(n)
+      }
+      n.expanded = !n.expanded
+      this.vis.chart.setData(sankeyData)
     }
   }
 }
