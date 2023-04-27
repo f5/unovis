806aa201218ffc60c9db4a96bc596fcf614fc1cf
Rebecca Bol <r.bol@f5.com>
2023-04-27 16:47:40 -0700

    Dev Demo | Adding Nested Donut exmples #172

---
+++ packages/dev/src/examples/misc/nested-donut/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { VisSingleContainer, VisNestedDonut } from '@unovis/react'
+import { GeneratedComponent } from '@src/examples'
+import { NestedDatum, generateNestedData } from '@src/utils/data'
+
+export const transitionComponent: GeneratedComponent<NestedDatum[]> = {
+  data: () => generateNestedData(100, 3),
+  dataSeries: {
+    noData: () => [],
+    singleLayer: data => data.map(d => ({ group: d.group })),
+  },
+  component: (props) => (
+    <VisSingleContainer data={props.data}>
+      <VisNestedDonut
+        direction={'outwards'}
+        layers={[
+          (d: NestedDatum) => d.group,
+          (d: NestedDatum) => d.subgroup,
+          (d: NestedDatum) => d.value,
+        ]}/>
+    </VisSingleContainer>
+  ),
+}
+
