602d1034c86ce983629f4c2789d7cb9548706d71
Rebecca Bol <r.bol@f5.com>
2023-04-27 16:47:40 -0700

    Dev Demo | Adding Nested Donut exmples #172

---
+++ packages/dev/src/examples/misc/nested-donut/basic-nested-donut/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NestedDonutSegment } from '@unovis/ts'
+import { VisSingleContainer, VisNestedDonut } from '@unovis/react'
+import { NestedDatum, generateNestedData } from '@src/utils/data'
+
+export const title = 'Basic Nested Donut'
+export const subTitle = 'with custom colors'
+
+const data = generateNestedData(100, 3, ['A1', 'B2'])
+
+export const component = (): JSX.Element => {
+  return (
+    <VisSingleContainer height={500}>
+      <VisNestedDonut
+        data={data}
+        centralLabel='Central Label'
+        centralSubLabel='Sub-label'
+        layers={[
+          (d: NestedDatum) => d.group,
+          (d: NestedDatum) => d.subgroup,
+          (d: NestedDatum) => d.value,
+        ]}
+        showBackground={true}
+        segmentColor={(d: NestedDonutSegment<NestedDatum>) => {
+          switch (d.data.root) {
+            case 'A': return '#cc2211'
+            case 'B': return '#22ee33'
+            case 'C': return '#2200cc'
+          }
+        }}
+        segmentLabelColor={(d: NestedDonutSegment<NestedDatum>) => {
+          if (Number(d.data.key)) return '#ffaa44'
+        }}
+      />
+    </VisSingleContainer>
+  )
+}
+
