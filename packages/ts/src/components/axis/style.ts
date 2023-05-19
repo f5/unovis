a5538f7fba49c24162e065b5dc7b6d739810205f
Nikita Rokotyan <nikita@f5.com>
2023-05-19 12:00:40 -0700

    Component | Axis: Adding CSS variables for domain line color and width

--- packages/ts/src/components/axis/style.ts
+++ packages/ts/src/components/axis/style.ts
@@ -9,6 +9,7 @@ export const globalStyles = injectGlobal`
     // Undefined by default to allow proper fallback to var(--vis-font-family)
     /* --vis-axis-font-family: */
     --vis-axis-tick-color: #e8e9ef;
+    /* --vis-axis-domain-color: // Undefined by default to allow fallback to var(--vis-axis-tick-color) */
     --vis-axis-tick-label-color: #6c778c;
     --vis-axis-grid-color: #e8e9ef;
     --vis-axis-label-color: #6c778c;
@@ -16,15 +17,18 @@ export const globalStyles = injectGlobal`
     --vis-axis-label-font-size: 14px;
     --vis-axis-tick-line-width: 1px;
     --vis-axis-grid-line-width: 1px;
+    /* --vis-axis-domain-line-width: // Undefined by default to allow fallback to var(--vis-axis-grid-line-width) */
 
     --vis-dark-axis-tick-color: #6c778c;
+    /* --vis-dark-axis-domain-color: // Undefined by default to allow fallback to var(--vis-dark-axis-tick-color) */
     --vis-dark-axis-tick-label-color: #e8e9ef;
     --vis-dark-axis-grid-color: #6c778c;
-    --vis-dark-axis-label-color:#fefefe;
+    --vis-dark-axis-label-color: #fefefe;
   }
 
   body.theme-dark ${`.${root}`} {
     --vis-axis-tick-color: var(--vis-dark-axis-tick-color);
+    --vis-axis-domain-color: var(--vis-dark-axis-domain-color);
     --vis-axis-tick-label-color: var(--vis-dark-axis-tick-label-color);
     --vis-axis-grid-color: var(--vis-dark-axis-grid-color);
     --vis-axis-label-color: var(--vis-dark-axis-label-color);
@@ -45,8 +49,8 @@ export const axis = css`
   user-select: none;
 
   .domain {
-    stroke: var(--vis-axis-tick-color);
-    stroke-width: var(--vis-axis-grid-line-width);
+    stroke: var(--vis-axis-domain-color, var(--vis-axis-tick-color));
+    stroke-width: var(--vis-axis-domain-line-width, var(--vis-axis-grid-line-width));
   }
 
   &${`.${hideTickLine}`} {
