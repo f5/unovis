c9f2e873d2879b0d409f54addb6c82c60179f715
Rebecca Bol <r.bol@f5.com>
2023-09-11 14:41:53 -0700

    Dev | Adding theme selector to demo page #0

---
+++ packages/dev/src/components/ThemeSelector/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// Styles
+import s from './style.module.css'
+
+export function ThemeSelector (): JSX.Element {
+  const toggleTheme = (e: React.FormEvent): void => {
+    document.body.classList.toggle((e.target as Element).id)
+  }
+
+  return (
+    <div onChange={toggleTheme} className={s.themeSelector}>
+      <label className={s.colorTheme}>
+        <input type='checkbox' id='theme-dark'/>
+        <div className='slider'>
+          <span className='fa-solid fa-sun'/>
+          <span className='fa-solid fa-moon'/>
+        </div>
+      </label>
+      <label className={s.patternTheme}>
+        <input type='checkbox' id='theme-patterns'/>
+        <span className='fa-solid fa-magic-wand-sparkles'/>
+      </label>
+    </div>
+  )
+}
