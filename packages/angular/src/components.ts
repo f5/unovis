ccd0bc5e57897251965663760de92f351c29c494
Rebecca Bol <r.bol@f5.com>
2023-11-16 09:47:18 -0800

    Angular | Build: Adding chord diagram to angular wrapper #0

--- packages/angular/src/components.ts
+++ packages/angular/src/components.ts
@@ -2,9 +2,6 @@
 export * from './core'
 
 // SVG Components
-export { VisLineComponent } from './components/line/line.component'
-export { VisLineModule } from './components/line/line.module'
-
 export { VisAreaComponent } from './components/area/area.component'
 export { VisAreaModule } from './components/area/area.module'
 
@@ -14,8 +11,8 @@ export { VisAxisModule } from './components/axis/axis.module'
 export { VisBrushComponent } from './components/brush/brush.component'
 export { VisBrushModule } from './components/brush/brush.module'
 
-export { VisFreeBrushComponent } from './components/free-brush/free-brush.component'
-export { VisFreeBrushModule } from './components/free-brush/free-brush.module'
+export { VisChordDiagramComponent } from './components/chord-diagram/chord-diagram.component'
+export { VisChordDiagramModule } from './components/chord-diagram/chord-diagram.module'
 
 export { VisCrosshairComponent } from './components/crosshair/crosshair.component'
 export { VisCrosshairModule } from './components/crosshair/crosshair.module'
@@ -23,8 +20,23 @@ export { VisCrosshairModule } from './components/crosshair/crosshair.module'
 export { VisDonutComponent } from './components/donut/donut.component'
 export { VisDonutModule } from './components/donut/donut.module'
 
+export { VisFreeBrushComponent } from './components/free-brush/free-brush.component'
+export { VisFreeBrushModule } from './components/free-brush/free-brush.module'
+
+export { VisGraphComponent } from './components/graph/graph.component'
+export { VisGraphModule } from './components/graph/graph.module'
+
 export { VisGroupedBarComponent } from './components/grouped-bar/grouped-bar.component'
 export { VisGroupedBarModule } from './components/grouped-bar/grouped-bar.module'
+
+export { VisLineComponent } from './components/line/line.component'
+export { VisLineModule } from './components/line/line.module'
+
+export { VisNestedDonutComponent } from './components/nested-donut/nested-donut.component'
+export { VisNestedDonutModule } from './components/nested-donut/nested-donut.module'
+
+export { VisSankeyComponent } from './components/sankey/sankey.component'
+export { VisSankeyModule } from './components/sankey/sankey.module'
 
 export { VisScatterComponent } from './components/scatter/scatter.component'
 export { VisScatterModule } from './components/scatter/scatter.module'
@@ -40,15 +52,6 @@ export { VisXYLabelsModule } from './components/xy-labels/xy-labels.module'
 
 export { VisTopoJSONMapComponent } from './components/topojson-map/topojson-map.component'
 export { VisTopoJSONMapModule } from './components/topojson-map/topojson-map.module'
-
-export { VisSankeyComponent } from './components/sankey/sankey.component'
-export { VisSankeyModule } from './components/sankey/sankey.module'
-
-export { VisGraphComponent } from './components/graph/graph.component'
-export { VisGraphModule } from './components/graph/graph.module'
-
-export { VisNestedDonutComponent } from './components/nested-donut/nested-donut.component'
-export { VisNestedDonutModule } from './components/nested-donut/nested-donut.module'
 
 // HTML Components
 export { VisLeafletMapComponent } from './html-components/leaflet-map/leaflet-map.component'
