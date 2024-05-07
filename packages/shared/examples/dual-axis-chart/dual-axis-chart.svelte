db1835137dc4c29ed5c6fb0e7ca85e85239dbde2
lee00678 <qi.liu@f5.com>
2024-05-07 15:09:29 -0700

    Website | Gallery: Add new section for composite chart

---
+++ packages/shared/examples/dual-axis-chart/dual-axis-chart.svelte
@@ -0,0 +1,65 @@
+<script lang='ts'>
+
+  import { VisXYContainer, VisArea, VisAxis, VisLine } from '@unovis/svelte'
+  import { XYDataRecord, generateXYDataRecords } from './data'
+
+  const margin = { left: 100, right: 100, top: 40, bottom: 60 }
+  const style: React.CSSProperties = { position: 'absolute', top: 0, left: 0, width: '100%', height: '40vh' }
+  const chartX = d => d.x
+  const chartAY = (d: XYDataRecord, i: number) => i * (d.y || 0)
+  const chartBY = (d: XYDataRecord) => 20 + 10 * (d.y2 || 0)
+  const xTicks = (x: number) => `${x}ms`
+  const chartAYTicks = (y: number) => `${y}bps`
+  const chartBYTicks = (y: number) => `${y}db`
+
+</script>
+
+<div>
+  <VisXYContainer
+    data={generateXYDataRecords(150)}
+    autoMargin={false}
+    margin={margin}
+    width= {'100%'}
+    height= {'40vh'}
+  >
+    <VisArea x={chartX} y={chartAY} opacity={0.9} color={'#FF6B7E'}/>
+    <VisAxis type='x' numTicks={3} tickFormat={xTicks} label='Time'/>
+    <VisAxis type='y'
+      tickFormat={chartAYTicks}
+      tickTextWidth={60}
+      tickTextColor={'#FF6B7E'}
+      labelColor={'#FF6B7E'}
+      label='Traffic'
+    />
+  </VisXYContainer>
+  <VisXYContainer
+    data={generateXYDataRecords(150)}
+    yDomain={[0, 150]}
+    margin={margin}
+    autoMargin={false}
+    width= {'100%'}
+    height= {'40vh'}
+    class={'chartContainer'}
+  >
+    <VisLine x={chartX} y={chartBY}/>
+    <VisAxis
+      type='y'
+      position={'right'}
+      tickFormat={chartBYTicks}
+      gridLine={false}
+      tickTextColor={'#4D8CFD'}
+      labelColor={'#4D8CFD'}
+      label='Signal Strength'
+    />
+  </VisXYContainer>
+</div>
+
+<style>
+  div :global(.chartContainer) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 40vh;
+  }
+</style>
