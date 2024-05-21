24185e128e463d7d6c3cd962577f6b2ee06200f9
Nikita Rokotyan <nikita@exaforce.com>
2024-05-21 09:54:22 -0700

    Component | Brush: More CSS variables, new way of defining them
    #0

--- packages/ts/src/components/brush/style.ts
+++ packages/ts/src/components/brush/style.ts
@@ -1,44 +1,50 @@
-import { css, injectGlobal } from '@emotion/css'
+import { css } from '@emotion/css'
+import { getCssVarNames, injectGlobalCssVariables } from 'utils/style'
 
 export const root = css`
   label: brush-component;
 `
 
-export const variables = injectGlobal`
-  :root {
-    --vis-brush-selection-fill-color: #0b1640;
-    --vis-brush-selection-stroke-color: #acb2b9;
-    --vis-brush-handle-fill-color: #6d778c;
-    --vis-brush-handle-stroke-color: #eee;
+export const cssVarDefaults = {
+  '--vis-brush-selection-fill-color': 'none',
+  '--vis-brush-selection-stroke-color': 'none',
+  '--vis-brush-selection-stroke-width': '0',
+  '--vis-brush-selection-opacity': '0',
+  '--vis-brush-unselected-fill-color': '#0b1640',
+  '--vis-brush-unselected-stroke-color': '#acb2b9',
+  '--vis-brush-unselected-stroke-width': '0',
+  '--vis-brush-unselected-opacity': '0.4',
+  '--vis-brush-handle-fill-color': '#6d778c',
+  '--vis-brush-handle-stroke-color': '#eee',
 
-    --vis-dark-brush-selection-fill-color:#acb2b9;
-    --vis-dark-brush-selection-stroke-color: #0b1640;
-    --vis-dark-brush-handle-fill-color: #acb2b9;
-    --vis-dark-brush-handle-stroke-color: var(--vis-color-grey);
-  }
+  /* Dark Theme */
+  '--vis-dark-brush-selection-fill-color': 'none',
+  '--vis-dark-brush-selection-stroke-color': 'none',
+  '--vis-dark-brush-selection-stroke-width': '0',
+  '--vis-dark-brush-selection-opacity': '0',
+  '--vis-dark-brush-unselected-fill-color': '#acb2b9',
+  '--vis-dark-brush-unselected-stroke-color': '#0b1640',
+  '--vis-dark-brush-unselected-stroke-width': '0',
+  '--vis-dark-brush-unselected-opacity': '0.4',
+  '--vis-dark-brush-handle-fill-color': '#acb2b9',
+  '--vis-dark-brush-handle-stroke-color': 'var(--vis-color-grey)',
+}
 
-  body.theme-dark ${`.${root}`} {
-    --vis-brush-selection-fill-color: var(--vis-dark-brush-selection-fill-color);
-    --vis-brush-selection-stroke-color: var(--vis-dark-brush-selection-stroke-color);
-    --vis-brush-handle-fill-color: var(--vis-dark-brush-handle-fill-color);
-    --vis-brush-handle-stroke-color: var(--vis-dark-brush-handle-stroke-color);
-  }
-`
+export const variables = getCssVarNames(cssVarDefaults)
+injectGlobalCssVariables(cssVarDefaults, root)
 
 export const brush = css`
   label: brush;
-  fill: none;
-  stroke: none;
 
   .selection {
-    fill: none;
-    stroke: var(--vis-brush-selection-stroke-color);
-    stroke-width: 0;
-    stroke-opacity: 0;
+    fill: var(${variables.brushSelectionFillColor});
+    stroke: var(${variables.brushSelectionStrokeColor});
+    stroke-width: var(${variables.brushSelectionStrokeWidth});
+    opacity: var(${variables.brushSelectionOpacity});
   }
 
   .handle {
-    fill: var(--vis-brush-handle-fill-color);
+    fill: var(${variables.brushHandleFillColor});
   }
 
   &.non-draggable {
@@ -50,14 +56,17 @@ export const brush = css`
 
 export const unselected = css`
   label: unselected;
-  fill: var(--vis-brush-selection-fill-color);
-  opacity: 0.4;
+  fill: var(${variables.brushUnselectedFillColor});
+  stroke: var(${variables.brushUnselectedStrokeColor});
+  stroke-width: var(${variables.brushUnselectedStrokeWidth});
+  stroke-opacity: var(${variables.brushUnselectedOpacity});
+  opacity: var(${variables.brushUnselectedOpacity});
   pointer-events: none;
 `
 
 export const handleLine = css`
   label: handle-line;
-  stroke: var(--vis-brush-handle-stroke-color);
+  stroke: var(${variables.brushHandleStrokeColor});
   stroke-width: 1;
   fill: none;
   pointer-events: none;
