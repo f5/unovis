632673c6878c575807c8e68864d5fc8f8c180a83
lee00678 <qi.liu@f5.com>
2024-05-28 11:59:51 -0700

    Dev | Examples | Axis: Add tick rotation example

---
+++ packages/dev/src/examples/xy-components/axis/axis-rotation/index.tsx
@@ -0,0 +1,73 @@
+import React, { useRef } from 'react'
+import { VisXYContainer, VisAxis, VisLine, VisGroupedBar } from '@unovis/react'
+import { XYDataRecord, generateXYDataRecords } from '@src/utils/data'
+import { Scale } from '@unovis/ts'
+
+export const title = 'Axis with Ticks Rotation'
+export const subTitle = 'Generated Data'
+
+export const component = (): JSX.Element => {
+  const accessors = [
+    (d: XYDataRecord) => d.y,
+    (d: XYDataRecord) => d.y1,
+    (d: XYDataRecord) => d.y2,
+    () => Math.random(),
+    () => Math.random(),
+  ]
+  return (
+    <>
+      <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)}
+        xScale={Scale.scaleTime()}
+        margin={{ top: 5, left: 5, bottom: 40, right: 5 }}>
+        <VisLine x={d => d.x} y={accessors}/>
+        <VisAxis type='x'
+          numTicks={15}
+          tickFormat={(x: number) => `${Intl.DateTimeFormat().format(x)}`}
+          tickTextAngle={60}
+          tickTextAlign={'left'}
+        />
+        <VisAxis type='y'
+          tickFormat={(y: number) => `${y * 10000}`}
+          tickTextAngle={40}
+          position={'right'}
+        />
+      </VisXYContainer>
+
+      <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)}
+        xScale={Scale.scaleTime()}
+        margin={{ top: 35, left: 65, bottom: 5, right: 5 }}>
+        <VisLine x={d => d.x} y={accessors}/>
+        <VisAxis type='x'
+          numTicks={15}
+          tickFormat={(x: number) => `${x}`}
+          tickTextAngle={30}
+          tickTextFitMode='trim'
+          tickTextAlign={'right'}
+          position='top'
+        />
+        <VisAxis type='y'
+          tickFormat={(y: number) => `${y}`}
+          tickTextAngle={40}
+          position={'right'}
+        />
+      </VisXYContainer>
+
+      <VisXYContainer<XYDataRecord> ariaLabel='A simple example of a Grouped Bar chart' data={generateXYDataRecords(15)} margin={{ top: 5, left: 5 }} xDomain={[1, 10]}>
+        <VisGroupedBar x={d => d.x} y={accessors} />
+        <VisAxis
+          type='x'
+          tickFormat= { d => new Date(d).toDateString()}
+          tickTextAngle={-30}
+          tickTextWidth={20}
+          tickTextFitMode='wrap'
+          tickTextAlign={'right'}
+        />
+        <VisAxis
+          type='y'
+          tickFormat={(y: number) => `${y * 100000000}`}
+          tickTextAngle={-60}
+        />
+      </VisXYContainer>
+    </>
+  )
+}
--- packages/dev/src/examples/xy-components/line/multi-line-chart/index.tsx
+++ packages/dev/src/examples/xy-components/line/multi-line-chart/index.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react'
 import { VisXYContainer, VisAxis, VisTooltip, VisCrosshair, VisBulletLegend, VisLine } from '@unovis/react'
 import { XYDataRecord, generateXYDataRecords } from '@src/utils/data'
-import { Scale } from '@unovis/ts'
 
 export const title = 'Multi Line Chart'
 export const subTitle = 'Generated Data'
@@ -17,26 +16,10 @@ export const component = (): JSX.Element => {
   ]
   return (
     <>
-      <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)}
-        xScale={Scale.scaleTime()}
-        margin={{ top: 5, left: 5, bottom: 40, right: 5 }}>
+      <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)}>
         <VisLine x={d => d.x} y={accessors}/>
-        <VisAxis type='x'
-          numTicks={15}
-          tickFormat={(x: number) => `${Intl.DateTimeFormat().format(x)}`}
-          // tickTextAngle={60}
-          tickTextWidth={10}
-          tickTextFitMode='wrap'
-          // maxWidth={20}
-          // position={'bottom'}
-          tickTextAlign={'left'}
-        />
-        <VisAxis type='y'
-          tickFormat={(y: number) => `${y * 100000000}`}
-          // tickTextAngle={40}
-          tickTextFitMode={'wrap'}
-          // position={'right'}
-        />
+        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} />
+        <VisAxis type='y' tickFormat={(y: number) => `${y}`} />
         <VisCrosshair template={(d: XYDataRecord) => `${d.x}`} />
         <VisTooltip ref={tooltipRef} />
       </VisXYContainer>
