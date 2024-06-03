984977471248a38ecc86bba536ad48e752d7d620
lee00678 <qi.liu@f5.com>
2024-06-03 14:51:51 -0700

    Component | Graph: Add x/y for user to set inital positions, update doc

---
+++ packages/dev/src/examples/networks-and-flows/graph/graph-node-positions/index.tsx
@@ -0,0 +1,24 @@
+import React, { useCallback, useMemo, useRef } from 'react'
+import { VisSingleContainer, VisGraph, VisGraphRef } from '@unovis/react'
+import { generatePrecalculatedNodeLinkData, NodeDatum, LinkDatum } from '@src/utils/data'
+
+export const title = 'Graph: Node Positions'
+export const subTitle = 'Pass in node positions'
+
+export const component = (): JSX.Element => {
+  const data = useMemo(() => generatePrecalculatedNodeLinkData(), [])
+  const ref = useRef<VisGraphRef<NodeDatum, LinkDatum>>(null)
+
+
+  return (
+    <VisSingleContainer data={data} height={600}>
+      <VisGraph
+        ref={ref}
+        layoutType="precalculated"
+        linkCurvature={1}
+        nodeIcon={useCallback((n: NodeDatum) => n.id, [])}
+      />
+    </VisSingleContainer>
+  )
+}
+
