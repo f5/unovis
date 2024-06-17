7e5757d9f00cf390217bb1521f711231411add6b
Nikita Rokotyan <nikita@exaforce.com>
2024-06-17 15:44:10 -0700

    Component | Graph: Custom node rendering pipeline

--- packages/ts/src/components/graph/config.ts
+++ packages/ts/src/components/graph/config.ts
@@ -1,6 +1,7 @@
 import { D3BrushEvent } from 'd3-brush'
 import { D3DragEvent } from 'd3-drag'
 import { D3ZoomEvent } from 'd3-zoom'
+import { Selection } from 'd3-selection'
 
 // Config
 import { ComponentConfigInterface, ComponentDefaultConfig } from 'core/component/config'
@@ -195,6 +196,12 @@ export interface GraphConfigInterface<N extends GraphInputNode, L extends GraphI
   nodeExitPosition?: GenericAccessor<[number, number], N> | undefined;
   /** Specify the destination scale for exiting nodes in the range [0,1]. Default: `0.75` */
   nodeExitScale?: NumericAccessor<N> | undefined;
+  /** Custom "enter" function for node rendering. Default: `undefined` */
+  nodeEnterCustomRenderFunction?: (datum: GraphNode<N, L>, nodeGroupElement: SVGGElement, config: GraphConfigInterface<N, L>, duration: number, zoomLevel: number) => void;
+  /** Custom "update" function for node rendering. Default: `undefined` */
+  nodeUpdateCustomRenderFunction?: (datum: GraphNode<N, L>, nodeGroupElement: SVGGElement, config: GraphConfigInterface<N, L>, duration: number, zoomLevel: number) => void;
+  /** Custom "exit" function for node rendering. Default: `undefined` */
+  nodeExitCustomRenderFunction?: (datum: GraphNode<N, L>, nodeGroupElement: SVGGElement, config: GraphConfigInterface<N, L>, duration: number, zoomLevel: number) => void;
   /** Set selected node by unique id. Default: `undefined` */
   selectedNodeId?: number | string;
   /** Set selected nodes by unique id. Default: `undefined` */
@@ -218,6 +225,15 @@ export interface GraphConfigInterface<N extends GraphInputNode, L extends GraphI
   onNodeSelectionBrush?: (selectedNodes: GraphNode<N, L>[], event: D3BrushEvent<SVGGElement> | undefined) => void;
   /** Graph multiple node drag callback function. Default: `undefined` */
   onNodeSelectionDrag?: (selectedNodes: GraphNode<N, L>[], event: D3DragEvent<SVGGElement, GraphNode<N, L>, unknown>) => void;
+  /** Callback function to be called when the graph rendering is complete. Default: `undefined` */
+  onRenderComplete?: (
+    g: Selection<SVGGElement, unknown, null, undefined>,
+    nodes: GraphNode<N, L>[],
+    links: GraphLink<N, L>[],
+    config: GraphConfigInterface<N, L>,
+    duration: number,
+    zoomLevel: number
+  ) => void;
 }
 
 export const GraphDefaultConfig: GraphConfigInterface<GraphInputNode, GraphInputLink> = {
@@ -312,4 +328,5 @@ export const GraphDefaultConfig: GraphConfigInterface<GraphInputNode, GraphInput
   onLayoutCalculated: undefined,
   onNodeSelectionBrush: undefined,
   onNodeSelectionDrag: undefined,
+  onRenderComplete: undefined,
 }
