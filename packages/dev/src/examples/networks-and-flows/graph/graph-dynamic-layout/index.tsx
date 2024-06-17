6ec5ba18076f8f2ac10ce57fb36ec6f1052e2682
Nikita Rokotyan <nikita@exaforce.com>
2024-06-17 15:20:31 -0700

    Dev | Graph | Dynamic Layout: More complex example

--- packages/dev/src/examples/networks-and-flows/graph/graph-dynamic-layout/index.tsx
+++ packages/dev/src/examples/networks-and-flows/graph/graph-dynamic-layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { VisSingleContainer, VisGraph } from '@unovis/react'
 import { GraphLayoutType } from '@unovis/ts'
 import { generateNodeLinkData } from '@src/utils/data'
@@ -7,10 +7,29 @@ export const title = 'Dynamic Layout'
 export const subTitle = 'Select Layout From Dropdown'
 
 export const component = (): JSX.Element => {
-  const data = useMemo(() => generateNodeLinkData(50, () => 1), [])
+  const [data, setData] = useState(generateNodeLinkData(50))
   const layouts = Object.values(GraphLayoutType)
   const initial = GraphLayoutType.Circular
   const [layout, setLayout] = useState<string>(initial)
+
+  // Generate new data every 2 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      const newData = generateNodeLinkData(10 + Math.floor(Math.random() * 50))
+
+      // Adding some random x, y values to the nodes for `GraphLayoutType.Precalculated`
+      newData.nodes.forEach(n => {
+        n.x = Math.random() * 1000
+        n.y = Math.random() * 1000
+      })
+
+      setData(newData)
+    }, 2000)
+  }, [data])
+
+  const forceLayoutSettings = useMemo(() => ({
+    fixNodePositionAfterSimulation: true,
+  }), [])
 
   return (
     <>
@@ -18,7 +37,7 @@ export const component = (): JSX.Element => {
         {layouts.map(l => <option key={l} value={l}>{l}</option>)}
       </select>
       <VisSingleContainer data={data} height={900}>
-        <VisGraph layoutType={layout}/>
+        <VisGraph layoutType={layout} forceLayoutSettings={forceLayoutSettings}/>
       </VisSingleContainer>
     </>
   )
