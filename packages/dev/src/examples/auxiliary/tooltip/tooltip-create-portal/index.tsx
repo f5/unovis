242022a0e68fa8bfbb0173a7b187580726a68ada
Nikita Rokotyan <nikita@exaforce.com>
2024-06-20 14:16:22 -0700

    Dev | Examples | Tooltip: Rendered in a Portal

---
+++ packages/dev/src/examples/auxiliary/tooltip/tooltip-create-portal/index.tsx
@@ -0,0 +1,71 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { Position, Scatter, Tooltip } from '@unovis/ts'
+import { VisXYContainer, VisAxis, VisScatter, VisTooltip } from '@unovis/react'
+
+import { XYDataRecord } from '@src/utils/data'
+import { createPortal } from 'react-dom'
+
+export const title = 'Tooltip Rendered in Portal'
+export const subTitle = 'Dynamic content in tooltip'
+
+export const TooltipComponent = (props: { x: number; y: number }): JSX.Element => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 600)
+
+    return () => clearTimeout(timer)
+  }, [props.x, props.y])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return <div>
+    <div>X: {props.x}</div>
+    <div>Y: {props.y}</div>
+  </div>
+}
+
+export const component = (): JSX.Element => {
+  const [hoveredDatum, setHoveredDatum] = React.useState<XYDataRecord | null>(null)
+  const data: XYDataRecord[] = useMemo(() => [
+    { x: 1, y: 3 },
+    { x: 3, y: 8 },
+    { x: 4, y: 9 },
+    { x: 9, y: 5 },
+  ], [])
+
+  const tooltipContainerNode = document.querySelector(`.${Tooltip.selectors.root}`)
+  return (<>
+    <VisXYContainer<XYDataRecord> data={data}>
+      <VisScatter
+        x={useCallback((d: XYDataRecord) => d.x, [])}
+        y={useCallback((d: XYDataRecord) => d.y, [])}
+        size={40}
+        color={'#5d30c9'}
+      />
+      <VisTooltip
+        followCursor={false}
+        container={document.body}
+        horizontalPlacement={Position.Auto}
+        verticalPlacement={Position.Auto}
+        triggers={useMemo(() => ({
+          [Scatter.selectors.point]: (d: XYDataRecord) => {
+            setHoveredDatum(d)
+          },
+        }), [])} />
+      <VisAxis type='x' />
+      <VisAxis type='y' />
+    </VisXYContainer>
+    {tooltipContainerNode && hoveredDatum && createPortal(
+      <TooltipComponent
+        x={hoveredDatum.x}
+        y={hoveredDatum.y as number}
+      />, tooltipContainerNode)}
+  </>
+  )
+}
