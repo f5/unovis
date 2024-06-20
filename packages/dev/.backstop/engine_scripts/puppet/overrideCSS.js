d51330cea752356fbc9b9b6185eb417b1388ebfa
lee00678 <qi.liu@f5.com>
2024-06-20 11:21:05 -0700

    Core | Text: Regression test

--- packages/dev/.backstop/engine_scripts/puppet/overrideCSS.js
+++ packages/dev/.backstop/engine_scripts/puppet/overrideCSS.js
@@ -1,4 +1,14 @@
-const BACKSTOP_TEST_CSS_OVERRIDE = 'html {background-image: none;}';
+const BACKSTOP_TEST_CSS_OVERRIDE = `
+html {
+  background-image: none;
+  -webkit-font-smoothing: none !important;
+  --font-render-hinting: none;
+
+::-webkit-scrollbar {
+  display: none; /* Chrome Safari */
+  -ms-overflow-style: none; /* IE 10+ */
+  scrollbar-width: none; /* Firefox */
+}}`;
 
 module.exports = async (page, scenario) => {
   // inject arbitrary css to override styles
@@ -9,6 +19,7 @@ module.exports = async (page, scenario) => {
     const styleNode = document.createTextNode(window._styleData);
     style.appendChild(styleNode);
     document.head.appendChild(style);
+    document.getElementsByTagName('body')[0].style['-webkit-font-smoothing'] = 'none';
   });
 
   console.log('BACKSTOP_TEST_CSS_OVERRIDE injected for: ' + scenario.label);
