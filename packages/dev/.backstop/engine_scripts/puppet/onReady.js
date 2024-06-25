bdfcb190e2260a482a8e86d2a2a75d3e514e0f9a
lee00678 <qi.liu@f5.com>
2024-06-25 13:11:41 -0700

    Core | Test: Regression

--- packages/dev/.backstop/engine_scripts/puppet/onReady.js
+++ packages/dev/.backstop/engine_scripts/puppet/onReady.js
@@ -1,5 +1,6 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
+  await page.waitForFunction(() => document.fonts.ready);
   // add more ready handlers here...
 };
