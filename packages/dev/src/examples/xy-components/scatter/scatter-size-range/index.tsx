4312ec9100336dc92c500619adfaca93db79e29f
Rebecca Bol <r.bol@f5.com>
2024-07-01 13:51:37 -0700

    Dev | Example: Scatter size range comparison

---
+++ packages/dev/src/examples/xy-components/scatter/scatter-size-range/index.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react'
+import { VisXYContainer, VisScatter, VisAxis } from '@unovis/react'
+import { generateXYDataRecords, XYDataRecord } from '@src/utils/data'
+
+export const title = 'Point Sizes'
+export const subTitle = 'Varied sizeRange comparison'
+
+const data = generateXYDataRecords(100)
+
+export const component = (): JSX.Element => {
+  const scatterProps = {
+    x: useCallback((d: XYDataRecord) => d.x, []),
+    y: useCallback((d: XYDataRecord) => d.y, []),
+    size: useCallback((d: XYDataRecord) => d.x, []),
+  }
+
+  const sizeRanges: ([number, number] | undefined)[] = [
+    [2, 10],
+    [4, 32],
+    [12, 50],
+  ]
+
+  return (
+    <>
+      {sizeRanges.map((s, i) => (
+        <VisXYContainer key={`s${i}`} data={data}>
+          <VisScatter {...scatterProps} sizeRange={s}/>
+          <VisAxis type='x'/>
+          <VisAxis type='y'/>
+        </VisXYContainer>
+      ))}
+    </>
+  )
+}
