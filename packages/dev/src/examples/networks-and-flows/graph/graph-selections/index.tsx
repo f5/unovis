e80d1c3e147945ce93bc06103fc082dcb835d1ca
Nikita Rokotyan <nikita@rokotyan.com>
2024-07-18 15:30:20 -0700

    Dev | Examples | Graph Node Selection: Hold Cmd to select multiple nodes

--- packages/dev/src/examples/networks-and-flows/graph/graph-selections/index.tsx
+++ packages/dev/src/examples/networks-and-flows/graph/graph-selections/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { VisSingleContainer, VisGraph, VisGraphRef } from '@unovis/react'
-import { Graph } from '@unovis/ts'
+import { Graph, GraphNodeSelectionHighlightMode } from '@unovis/ts'
 import { generateNodeLinkData, NodeDatum, LinkDatum } from '@src/utils/data'
 
 export const title = 'Graph: Node Selection'
@@ -12,13 +12,13 @@ export const component = (): JSX.Element => {
   const [selectedNodes, setSelectedNodes] = useState<string[] | undefined>()
   const [text, setText] = useState<string>()
 
-  const selectNode = useCallback((n: NodeDatum) => {
-    setSelectedNodes([...selectedNodes ?? [], n.id])
+  const selectNode = useCallback((n: NodeDatum, e: PointerEvent) => {
+    setSelectedNodes([...(selectedNodes && e.metaKey ? selectedNodes : []), n.id])
   }, [selectedNodes])
 
   useEffect(() => {
     const external = selectedNodes ?? 'undefined'
-    const internal = ref.current?.component?.selectedNodes ?? 'undefined'
+    const internal = ref.current?.component?.selectedNodes?.map(d => d.id) ?? 'undefined' // Delayed by a render
     setText([external, internal].join(' | '))
   }, [selectedNodes])
 
@@ -31,17 +31,17 @@ export const component = (): JSX.Element => {
           forceLayoutSettings={useMemo(() => ({
             fixNodePositionAfterSimulation: true,
           }), [])}
+          nodeSelectionHighlightMode={GraphNodeSelectionHighlightMode.Greyout}
           linkCurvature={1}
           nodeIcon={useCallback((n: NodeDatum) => n.id, [])}
           events={useMemo(() => ({
             [Graph.selectors.node]: {
-              click: (n: NodeDatum) => { selectNode(n) },
+              click: selectNode,
             },
             [Graph.selectors.background]: {
               click: () => { setSelectedNodes(undefined) },
             },
           }), [selectedNodes])}
-          // selectedNodeId={'1'}
           selectedNodeIds={selectedNodes}
         />
       </VisSingleContainer>
