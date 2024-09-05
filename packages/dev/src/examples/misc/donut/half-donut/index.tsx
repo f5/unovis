65c868df23e473765001bb68130ac2eb55400161
Curran <ckelleher@exaforce.com>
2024-09-05 16:20:52 -0400

    Component | Donut: Start on half donut

---
+++ packages/dev/src/examples/misc/donut/half-donut/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { VisSingleContainer, VisDonut } from '@unovis/react'
+import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
+
+export const title = 'Donut: Half Donut'
+export const subTitle = ''
+
+const halfDonutAngle: [number, number] = [-Math.PI / 2, Math.PI / 2]
+
+export const component = (props: ExampleViewerDurationProps): JSX.Element => {
+  const data = [0, 2, 0, 4, 0, 1]
+  return (
+    <VisSingleContainer height={400}>
+      <VisDonut
+        value={d => d}
+        data={data}
+        showEmptySegments={true}
+        padAngle={0.02}
+        arcWidth={100}
+        duration={props.duration}
+        angleRange={halfDonutAngle}
+      />
+    </VisSingleContainer>
+  )
+}
+
