7e240844acc98ade2b5ba41482edaa4f6715cb5c
Curran <ckelleher@exaforce.com>
2024-09-10 16:54:21 -0400

    Component | Donut: Put labels in the half donut example

--- packages/dev/src/examples/misc/donut/half-donut/index.tsx
+++ packages/dev/src/examples/misc/donut/half-donut/index.tsx
@@ -3,14 +3,15 @@ import { VisSingleContainer, VisDonut } from '@unovis/react'
 import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
 
 export const title = 'Donut: Half Donut'
-export const subTitle = ''
+export const subTitle = 'with labels and scaling'
 
-const halfDonutAngle: [number, number] = [-Math.PI / 2, Math.PI / 2]
+const halfDonutAngleRange: [number, number] = [-Math.PI / 2, Math.PI / 2]
 
 export const component = (props: ExampleViewerDurationProps): JSX.Element => {
   const data = [0, 2, 0, 4, 0, 1]
   return (
     <VisSingleContainer height={400}>
+      {/* TODO add label and sub-label here */}
       <VisDonut
         value={d => d}
         data={data}
@@ -18,7 +19,9 @@ export const component = (props: ExampleViewerDurationProps): JSX.Element => {
         padAngle={0.02}
         arcWidth={100}
         duration={props.duration}
-        angleRange={halfDonutAngle}
+        angleRange={halfDonutAngleRange}
+        centralLabel='Central Label'
+        centralSubLabel='Sub Label'
       />
     </VisSingleContainer>
   )
