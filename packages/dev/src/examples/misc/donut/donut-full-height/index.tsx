10f5998e18feb1ebcb54a86b6eb32afbbd25f025
Nikita Rokotyan <nikita@exaforce.com>
2024-10-21 11:45:36 -0700

    Dev | Examples | Donut: Add full height donut example
    - Adds a new example for a full height donut chart
    - Tests the resize behavior of the donut chart component
    
    #455

---
+++ packages/dev/src/examples/misc/donut/donut-full-height/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { VisSingleContainer, VisDonut } from '@unovis/react'
+import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
+
+export const title = 'Donut: Full Height'
+export const subTitle = 'Testing the resize behavior'
+
+export const component = (props: ExampleViewerDurationProps): JSX.Element => {
+  const data = [3, 2, 5, 4, 0, 1]
+  return (
+    <VisSingleContainer style={{ height: '100%' }}>
+      <VisDonut
+        value={d => d}
+        data={data}
+        padAngle={0.02}
+        duration={props.duration}
+        arcWidth={80}
+      />
+    </VisSingleContainer>
+  )
+}
+
