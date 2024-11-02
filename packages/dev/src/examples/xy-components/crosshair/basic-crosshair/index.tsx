27463511501e519e3bb4808982943cd204a592c5
Nikita Rokotyan <nikita@rokotyan.com>
2024-11-02 12:53:34 -0700

    Dev | Examples | Crosshair: Add basic crosshair example
    - Adds a new example for a basic crosshair component
    - Demonstrates the usage of the VisCrosshair and VisTooltip components
    - Removes the duration prop for Crosshair in the basic area example

--- packages/dev/src/examples/xy-components/crosshair/basic-crosshair/index.tsx
+++ packages/dev/src/examples/xy-components/crosshair/basic-crosshair/index.tsx
@@ -4,21 +4,21 @@ import { VisXYContainer, VisArea, VisAxis, VisTooltip, VisCrosshair } from '@uno
 import { XYDataRecord, generateXYDataRecords } from '@src/utils/data'
 import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
 
-export const title = 'Basic Area Chart'
-export const subTitle = 'Generated Data'
+export const title = 'Simple Crosshair'
+export const subTitle = 'Basic configuration'
 export const component = (props: ExampleViewerDurationProps): JSX.Element => {
   const tooltipRef = useRef(null)
   const accessors = [
-    (d: XYDataRecord) => d.y,
+    (d: XYDataRecord, i: number) => (d.y || 0) * i / 100,
     (d: XYDataRecord) => d.y1,
     (d: XYDataRecord) => d.y2,
   ]
 
   return (
-    <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)} margin={{ top: 5, left: 5 }}>
+    <VisXYContainer<XYDataRecord> data={generateXYDataRecords(150)} margin={{ top: 5, left: 5 }}>
       <VisArea x={d => d.x} y={accessors} duration={props.duration}/>
-      <VisAxis type='x' numTicks={3} tickFormat={(x: number) => `${x}ms`} duration={props.duration}/>
-      <VisAxis type='y' tickFormat={(y: number) => `${y}bps`} duration={props.duration}/>
+      <VisAxis type='x' duration={props.duration}/>
+      <VisAxis type='y' duration={props.duration}/>
       <VisCrosshair template={(d: XYDataRecord) => `${d.x}`} />
       <VisTooltip ref={tooltipRef} container={document.body}/>
     </VisXYContainer>
