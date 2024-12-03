1f790678de7f5c1346bd02c395efc6fadbde759a
sadeghbarati <sadeghbaratiwork@gmail.com>
2024-12-03 15:23:01 +0330

    Vue: update `useForwardProps` function to latest implementation

--- packages/vue/src/utils/props.ts
+++ packages/vue/src/utils/props.ts
@@ -1,5 +1,11 @@
 import { isEqual } from '@unovis/ts'
-import { ComponentInternalInstance, camelize, computed, getCurrentInstance } from 'vue'
+import { type MaybeRefOrGetter, camelize, computed, getCurrentInstance, toRef } from 'vue'
+
+interface PropOptions {
+  type?: any
+  required?: boolean
+  default?: any
+}
 
 export function arePropsEqual<PropTypes> (prevProps: PropTypes, nextProps: PropTypes): boolean {
   return isEqual(prevProps, nextProps)
@@ -7,18 +13,31 @@ export function arePropsEqual<PropTypes> (prevProps: PropTypes, nextProps: PropT
 
 // source: https://www.radix-vue.com/utilities/use-forward-props.html
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
-export function useForwardProps<T extends Record<string, any>> (props: T) {
+export function useForwardProps<T extends Record<string, any>>(props: MaybeRefOrGetter<T>) {
   const vm = getCurrentInstance()
-  const attrs = vm.attrs
+  // Default value for declared props
+  const defaultProps = Object.keys(vm?.type.props ?? {}).reduce((prev, curr) => {
+    const defaultValue = (vm?.type.props[curr] as PropOptions).default
+    if (defaultValue !== undefined)
+      prev[curr as keyof T] = defaultValue
+    return prev
+  }, {} as T)
 
+  const refProps = toRef(props)
   return computed(() => {
     const preservedProps = {} as T
     const assignedProps = vm?.vnode.props ?? {}
 
     Object.keys(assignedProps).forEach((key) => {
-      preservedProps[camelize(key) as keyof T] = props[camelize(key)]
+      preservedProps[camelize(key) as keyof T] = assignedProps[key]
     })
-    return { ...preservedProps, ...attrs }
+
+    // Only return value from the props parameter
+    return Object.keys({ ...defaultProps, ...preservedProps }).reduce((prev, curr) => {
+      if (refProps.value[curr] !== undefined)
+        prev[curr as keyof T] = refProps.value[curr]
+      return prev
+    }, {} as T)
   })
 }
 
@@ -32,4 +51,3 @@ export function parseProps <T extends object> (props: T, instance: ComponentInte
   })
   return preservedProps
 }
-
