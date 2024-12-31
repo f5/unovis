ec786f52ea1309704671b3f5f213a35f92f5a8ec
Curran <ckelleher@exaforce.com>
2024-12-31 13:15:29 -0500

    Component | Treemap: First pass implementation sketch

--- packages/dev/src/examples/misc/treemap/basic/index.tsx
+++ packages/dev/src/examples/misc/treemap/basic/index.tsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import { VisSingleContainer, VisTreemap } from '@unovis/react'
+import { TreemapNode } from '@unovis/ts'
 import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
 
 export const title = 'Treemap: Basic'
-export const subTitle = 'Simple hierarchical data visualization'
+export const subTitle = 'Hierarchical data visualization with custom colors'
 
-type TreemapExampleDatum = { name: string; value: number }
-
+type TreemapExampleDatum = {
+  name: string;
+  value: number;
+  group?: string;
+}
 
 export const component = (props: ExampleViewerDurationProps): JSX.Element => {
   const data: TreemapExampleDatum[] = [
-    { name: 'A', value: 20 },
-    { name: 'B', value: 15 },
-    { name: 'C', value: 10 },
-    { name: 'D', value: 5 },
+    { name: 'A', value: 20, group: 'Group 1' },
+    { name: 'B', value: 15, group: 'Group 1' },
+    { name: 'C', value: 10, group: 'Group 2' },
+    { name: 'D', value: 5, group: 'Group 2' },
+    { name: 'E', value: 8, group: 'Group 3' },
+    { name: 'F', value: 12, group: 'Group 3' },
   ]
+
   return (
     <VisSingleContainer height={400}>
       <VisTreemap
+        data={data}
         value={(d: TreemapExampleDatum) => d.value}
         layers={[
+          (d: TreemapExampleDatum) => d.group,
           (d: TreemapExampleDatum) => d.name,
         ]}
-        data={data}
+        tileColor={(d: TreemapNode<TreemapExampleDatum>) => {
+          switch (d.data.datum.group) {
+            case 'Group 1': return '#cc2211'
+            case 'Group 2': return '#22ee33'
+            case 'Group 3': return '#2200cc'
+            default: return '#999999'
+          }
+        }}
+        tileLabel={(d: TreemapNode<TreemapExampleDatum>) => d.data.datum.name}
+        tileLabelColor="#ffffff"
         duration={props.duration}
       />
     </VisSingleContainer>
--- packages/ts/src/components/treemap/index.ts
+++ packages/ts/src/components/treemap/index.ts
@@ -1,14 +1,15 @@
 import { Selection } from 'd3-selection'
-// import { treemap } from 'd3-hierarchy'
+import { hierarchy, treemap } from 'd3-hierarchy'
+import { group } from 'd3-array'
 
 import { ComponentCore } from 'core/component'
 import { SeriesDataModel } from 'data-models/series'
-import { isNumber } from 'utils/data'
+import { getColor, getString, getNumber, isNumber } from 'utils/data'
+import { smartTransition } from 'utils/d3'
 import { TreemapConfigInterface, TreemapDefaultConfig } from './config'
-import { TreemapDatum } from './types'
+import { TreemapDatum, TreemapNode } from './types'
 
 import * as s from './style'
-
 
 export class Treemap<Datum> extends ComponentCore<Datum[], TreemapConfigInterface<Datum>> {
   static selectors = s
@@ -25,19 +26,73 @@ export class Treemap<Datum> extends ComponentCore<Datum[], TreemapConfigInterfac
   }
 
   _render (customDuration?: number): void {
-    const { datamodel, config } = this
+    const { config, datamodel: { data } } = this
     const duration = isNumber(customDuration) ? customDuration : config.duration
 
-    // Basic render implementation to be extended
-    const data: TreemapDatum<Datum>[] = datamodel.data.map((d, i) => ({
-      index: i,
-      datum: d,
-    }))
+    if (!config.layers?.length) {
+      console.warn('Unovis | Treemap: No layers defined')
+      return
+    }
 
-    // eslint-disable-next-line no-console
-    console.log(data)
+    const layerAccessors = config.layers.map(layerAccessor => (i: number) => getString(data[i], layerAccessor, i))
+    const nestedData = group(data.keys(), ...layerAccessors)
 
-    // eslint-disable-next-line no-console
-    console.log(this.config)
+    const root = config.value !== undefined
+      ? hierarchy(nestedData).sum(index => typeof index === 'number' && getNumber(data[index], config.value, index))
+      : hierarchy(nestedData).count()
+
+    const treemapLayout = treemap<[string, number[]]>()
+      .size([this._width, this._height])
+      .round(true)
+      .padding(1)
+
+    const nodes = treemapLayout(root)
+      .descendants()
+      .filter(d => d.depth > 0)
+      .map(n => {
+        const node = n as TreemapNode<Datum>
+        node.data = {
+          datum: Array.isArray(n.data[1]) ? data[n.data[1][0]] : undefined,
+          index: Array.isArray(n.data[1]) ? n.data[1][0] : -1,
+        }
+        return node
+      })
+
+    // Render tiles
+    const tiles = this.tiles.selectAll<SVGGElement, TreemapNode<Datum>>('g')
+      .data(nodes, d => d.data.index.toString())
+
+    const tilesEnter = tiles.enter().append('g')
+
+    // Rectangles
+    tilesEnter.append('rect')
+    tiles.merge(tilesEnter).select('rect')
+      .style('fill', d => getColor(d, config.tileColor))
+      .call(selection => smartTransition(selection, duration)
+        .attr('x', d => d.x0)
+        .attr('y', d => d.y0)
+        .attr('width', d => d.x1 - d.x0)
+        .attr('height', d => d.y1 - d.y0)
+      )
+
+    // Labels
+    if (config.tileLabel) {
+      tilesEnter.append('text')
+      tiles.merge(tilesEnter).select('text')
+        .style('fill', d => getColor(d, config.tileLabelColor))
+        .text(d => getString(d, config.tileLabel))
+        .call(selection => smartTransition(selection, duration)
+          .attr('x', d => (d.x0 + d.x1) / 2)
+          .attr('y', d => (d.y0 + d.y1) / 2)
+          .attr('dy', '0.35em')
+          .attr('text-anchor', 'middle')
+        )
+    }
+
+    // Exit
+    const tilesExit = tiles.exit()
+    smartTransition(tilesExit, duration)
+      .style('opacity', 0)
+      .remove()
   }
 }
