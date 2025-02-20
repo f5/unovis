fd49adc5c494592ef8df9963e557cc4cc9fd6def
Nikita Rokotyan <nikita@exaforce.com>
2025-02-20 09:22:45 -0800

    Dev | Examples | Timeline: Refactoring

--- packages/dev/src/examples/xy-components/timeline/timeline-empty-segments/index.tsx
+++ packages/dev/src/examples/xy-components/timeline/timeline-empty-segments/index.tsx
@@ -17,7 +17,14 @@ export const component = (props: ExampleViewerDurationProps): React.ReactNode =>
   return (<>
     <label>Show empty segments: <input type='checkbox' checked={showEmptySegments} onChange={e => toggleEmptySegments(e.target.checked)}/></label>
     <VisXYContainer<TimeDataRecord> data={data} height={200}>
-      <VisTimeline x={(d: TimeDataRecord) => d.timestamp} rowHeight={50} showEmptySegments={showEmptySegments} showLabels duration={props.duration}/>
+      <VisTimeline
+        lineRow={(d: TimeDataRecord) => d.type as string}
+        x={(d: TimeDataRecord) => d.timestamp}
+        rowHeight={50}
+        showEmptySegments={showEmptySegments}
+        showRowLabels
+        duration={props.duration}
+      />
       <VisAxis type='x' numTicks={3} tickFormat={(x: number) => new Date(x).toDateString()} duration={props.duration}/>
       <VisCrosshair template={(d: TimeDataRecord) => `${Intl.DateTimeFormat().format(d.timestamp)}: ${Math.round(d.length / Math.pow(10, 7))}m`}/>
       <VisTooltip/>
