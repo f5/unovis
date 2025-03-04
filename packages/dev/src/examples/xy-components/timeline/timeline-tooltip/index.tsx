797ba71de680f0fe3d256c046380473f4928519c
Nikita Rokotyan <nikita@exaforce.com>
2025-03-04 09:49:48 -0800

    Dev | Examples | Timeline: Add animation example, update tooltip, and fix duration fallback
    - Introduce a new timeline animation example with configurable enter/exit animation line positions.
    - Update the timeline tooltip example to use TimelineRowLabel for enhanced type safety in tooltips.
    - Adjust timeline component to apply a default duration of 1000 when none is provided.

--- packages/dev/src/examples/xy-components/timeline/timeline-tooltip/index.tsx
+++ packages/dev/src/examples/xy-components/timeline/timeline-tooltip/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { VisXYContainer, VisTimeline, VisAxis, VisTooltip } from '@unovis/react'
-import { Timeline } from '@unovis/ts'
+import { Timeline, TimelineRowLabel } from '@unovis/ts'
 
 import { TimeDataRecord, generateTimeSeries } from '@src/utils/data'
 import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
@@ -26,7 +26,7 @@ export const component = (props: ExampleViewerDurationProps): JSX.Element => {
       <VisTooltip triggers={{
         [Timeline.selectors.line]: (d: TimeDataRecord) =>
           `${(new Date(d.timestamp)).toDateString()} — ${(new Date(d.timestamp + d.length)).toDateString()}`,
-        [Timeline.selectors.row]: (label: string) => `Timeline Row ${label}`,
+        [Timeline.selectors.row]: (l: TimelineRowLabel<TimeDataRecord>) => `Timeline Row ${l.label}`,
       }}/>
     </VisXYContainer>
   </>
