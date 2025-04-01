6ba615223ba49fc5dd8b44f5d4b5ea368461f063
Dima Semyushkin <git@devg.ru>
2025-04-01 23:38:42 +0300

    Component | Rolling Pin Legend: Introduce new component

---
+++ packages/ts/src/components/rolling-pin-legend/config.ts
@@ -0,0 +1,26 @@
+import { RollingPinLegendItem } from 'components/rolling-pin-legend/types'
+
+export interface RollingPinLegendConfigInterface {
+  /** Rects forming a legend. Array of `string`, representing colors.
+   *
+   * Default: `[]` */
+  rects: RollingPinLegendItem[];
+  /** Label on the left side of the legend. Default: `undefined` */
+  leftLabelText?: string;
+  /** Label on the right side of the legend. Default: `undefined` */
+  rightLabelText?: string;
+  /** Apply a specific class to the labels. Default: `''` */
+  labelClassName?: string;
+  /** Label text (<span> element) font-size CSS. Default: `null` */
+  labelFontSize?: string | null;
+  /** If set to true, the legend will be rendered directly into the HTML element provided to the constructor
+   * without creating additional `div` element. Default: `false` */
+  renderIntoProvidedDomNode?: boolean;
+}
+
+export const RollingPinLegendDefaultConfig: RollingPinLegendConfigInterface = {
+  rects: [],
+  labelClassName: '',
+  labelFontSize: null,
+  renderIntoProvidedDomNode: false,
+}
---
+++ packages/ts/src/components/rolling-pin-legend/index.ts
@@ -0,0 +1,86 @@
+import { select, Selection } from 'd3-selection'
+
+// Utils
+import { merge } from 'utils/data'
+
+// Config
+import { RollingPinLegendConfigInterface, RollingPinLegendDefaultConfig } from './config'
+
+// Styles
+import * as s from './style'
+
+export class RollingPinLegend {
+  static selectors = s
+  protected _defaultConfig = RollingPinLegendDefaultConfig as RollingPinLegendConfigInterface
+  public config: RollingPinLegendConfigInterface = this._defaultConfig
+
+  div: Selection<HTMLElement, unknown, null, undefined>
+  element: HTMLElement
+  prevConfig: RollingPinLegendConfigInterface
+  protected _container: HTMLElement
+
+  constructor (element: HTMLElement, config?: RollingPinLegendConfigInterface) {
+    this._container = element
+
+    this.div = config?.renderIntoProvidedDomNode ? select(this._container) : select(this._container).append<HTMLElement>('div')
+    this.div.classed(s.root, true)
+
+    this.element = this.div.node()
+
+    if (config) this.setConfig(config)
+  }
+
+  setConfig (config: RollingPinLegendConfigInterface): void {
+    this.prevConfig = this.config
+    this.config = merge(this._defaultConfig, config)
+    this.render()
+  }
+
+  render (): void {
+    const { config } = this
+
+    const newRoot = this.div
+      .selectAll<HTMLDivElement, unknown>(`.${s.root}`)
+      .data([0])
+      .enter()
+      .append('div')
+      .attr('class', s.root)
+
+    newRoot
+      .append('span')
+      .attr('class', s.label)
+
+    newRoot
+      .append('div')
+      .attr('class', s.rectsContainer)
+
+    newRoot
+      .append('span')
+      .attr('class', s.label)
+
+    const root = this.div
+      .select<HTMLDivElement>(`.${s.root}`)
+
+    root.selectAll<HTMLDivElement, unknown>(`.${s.label}`)
+      .data([config.leftLabelText, config.rightLabelText])
+      .text(d => d)
+
+    const rectsContainer = root.select<HTMLDivElement>(`.${s.rectsContainer}`)
+
+    const rects = rectsContainer.selectAll<HTMLDivElement, unknown>(`.${s.rect}`).data(config.rects)
+
+    const rectsEnter = rects.enter()
+      .append('div')
+      .attr('class', s.rect)
+
+    const rectsMerged = rectsEnter.merge(rects)
+    rectsMerged
+      .style('background-color', d => d)
+
+    rects.exit().remove()
+  }
+
+  public destroy (): void {
+    if (this.element !== this._container) this.div.remove()
+  }
+}
