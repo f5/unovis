fc7e0863e7980390c16c01f8b204e2b6647e0807
Soryn <soryngitlan@gmail.com>
2025-05-01 22:17:33 +0300

    Component | Plotline: Add label prop

--- packages/ts/src/components/plotline/types.ts
+++ packages/ts/src/components/plotline/types.ts
@@ -1,1 +1,11 @@
 export type PlotlineLineStylePresets = 'solid' | 'shortDash' | 'shortDot' | 'shortDashDot' | 'shortDashDotDot' | 'dot' | 'dash' | 'longDash' | 'dashDot' | 'longDashDot' | 'longDashDotDot'
+export type PlotlineLegendPosition = 'top-left'| 'top'| 'top-right'| 'right'| 'bottom-right'| 'bottom'| 'bottom-left'| 'left'
+export type PlotlineLegendOrientation = 'horizontal'| 'vertical'
+export type PlotlineLabelOptions = {
+  text?: string;
+  position: PlotlineLegendPosition;
+  offsetX: number;
+  offsetY: number;
+  orientation: PlotlineLegendOrientation;
+  style?: string;
+};
