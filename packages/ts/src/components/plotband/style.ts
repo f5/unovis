4f1586e132bf65a3f7e9756bdfc499567bc1b910
Soryn <soryngitlan@gmail.com>
2025-05-13 16:09:02 +0300

    Component | Plotband: add label customization props

--- packages/ts/src/components/plotband/style.ts
+++ packages/ts/src/components/plotband/style.ts
@@ -4,12 +4,16 @@ import { css, injectGlobal } from '@emotion/css'
 export const globalStyles = injectGlobal`
   :root {
     --vis-plotband-color: rgba(255, 255, 90, 0.2);
+    --vis-plotband-label-font-size: 12px;
+    --vis-plotband-label-color: #000;
 
     --vis-dark-plotband-color: rgba(220, 220, 90, 0.2);
+    --vis-dark-plotband-label-color: #e5e9f7;
   }
 
   body.theme-dark {
     --vis-plotband-color: var(--vis-dark-plotband-color);
+    --vis-plotband-label-color: var(--vis-dark-tooltip-text-color);
   }
 `
 
@@ -18,13 +22,16 @@ export const root = css`
 `
 
 export const plotband = css`
-  label: plotband;
+  label: plotband-area;
   transition: opacity 200ms;
   cursor: var(--vis-line-cursor);
   fill: var(--vis-plotband-color);
 `
 
-export const plotbandPath = css`
-  label: plotbandPath;
-  fill: none;
+export const label = css`
+  label: plotline-label;
+  text-anchor: middle;
+  pointer-events: none;
+  font-size: var(--vis-plotline-label-font-size);
+  fill: var(--vis-plotline-label-color);
 `
