87f7c1421a0579888ce5ac50c4036d0cda5b56a1
Soryn <soryngitlan@gmail.com>
2025-05-13 23:03:56 +0300

    Examples | Component | Plotband: Add example for each type of framework

--- packages/vue/src-demo/App.vue
+++ packages/vue/src-demo/App.vue
@@ -32,6 +32,7 @@ const imports = [
   defineAsyncComponent(() => import('@unovis/shared/examples/basic-donut-chart/basic-donut-chart.vue')),
   defineAsyncComponent(() => import('@unovis/shared/examples/shaped-scatter-plot/shaped-scatter-plot.vue')),
   defineAsyncComponent(() => import('@unovis/shared/examples/basic-annotations/basic-annotations.vue')),
+  defineAsyncComponent(() => import('@unovis/shared/examples/basic-plotband/basic-plotband.vue')),
 ]
 </script>
 
