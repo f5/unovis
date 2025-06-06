7e13c8aa33f47e89a53bc9e0a93eb34aeac8ae06
Nikita Rokotyan <nikita@exaforce.com>
2025-06-06 11:04:48 -0700

    Angular | Component | Treemap: Generating component wrapper

---
+++ packages/angular/src/components/treemap/treemap.module.ts
@@ -0,0 +1,10 @@
+// !!! This code was automatically generated. You should not change it !!!
+import { NgModule } from '@angular/core'
+import { VisTreemapComponent } from './treemap.component'
+
+@NgModule({
+  imports: [],
+  declarations: [VisTreemapComponent],
+  exports: [VisTreemapComponent],
+})
+export class VisTreemapModule {}
