d90d180805eb95c6d29ab0ce5ce13877a2432581
Nikita Rokotyan <nikita@exaforce.com>
2025-06-16 14:48:27 -0700

    Dev | Examples | Timeline: Adding "Generate Data" button to the Tooltip and Scrolling example

--- packages/dev/src/examples/xy-components/timeline/timeline-tooltip/index.tsx
+++ packages/dev/src/examples/xy-components/timeline/timeline-tooltip/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { VisXYContainer, VisTimeline, VisAxis, VisTooltip } from '@unovis/react'
-import { Timeline, TimelineRowLabel } from '@unovis/ts'
+import { Position, Timeline, TimelineRowLabel } from '@unovis/ts'
 
 import { TimeDataRecord, generateTimeSeries } from '@src/utils/data'
 import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
@@ -9,11 +9,23 @@ export const title = 'Tooltip and Scrolling'
 export const subTitle = 'Generated Data'
 
 export const component = (props: ExampleViewerDurationProps): React.ReactNode => {
-  const data = generateTimeSeries(25, 10, 10).map((d, i) => ({
+  const [data, setData] = useState(() => generateTimeSeries(45, 20, 10).map((d, i) => ({
     ...d,
-  }))
+  })))
+
+  const regenerateData = (): void => {
+    const numLines = Math.floor(Math.random() * 20) + 10
+    const numRows = Math.ceil(Math.random() * 10)
+    setData(generateTimeSeries(numLines, numRows, 10).map((d, i) => ({
+      ...d,
+    })))
+  }
+
   return (<>
-    <VisXYContainer<TimeDataRecord> data={data} height={300}>
+    <div style={{ marginBottom: '1rem' }}>
+      <button onClick={regenerateData}>Generate New Data</button>
+    </div>
+    <VisXYContainer<TimeDataRecord> data={data} height={600}>
       <VisTimeline
         lineRow={(d: TimeDataRecord) => d.type as string}
         x={(d: TimeDataRecord) => d.timestamp}
@@ -22,12 +34,13 @@ export const component = (props: ExampleViewerDurationProps): React.ReactNode =>
         showRowLabels
         duration={props.duration}
       />
-      <VisAxis type='x' numTicks={3} tickFormat={(x: number) => new Date(x).toDateString()} duration={props.duration}/>
-      <VisTooltip triggers={{
-        [Timeline.selectors.line]: (d: TimeDataRecord) =>
-          `${(new Date(d.timestamp)).toDateString()} — ${(new Date(d.timestamp + d.length)).toDateString()}`,
-        [Timeline.selectors.row]: (l: TimelineRowLabel<TimeDataRecord>) => `Timeline Row ${l.label}`,
-      }}/>
+      <VisAxis type='x' numTicks={3} tickFormat={(tick: number | Date) => new Date(tick).toDateString()} duration={props.duration}/>
+      <VisTooltip
+        triggers={{
+          [Timeline.selectors.line]: (d: TimeDataRecord) =>
+            `${(new Date(d.timestamp)).toDateString()} — ${(new Date(d.timestamp + d.length)).toDateString()}`,
+          [Timeline.selectors.row]: (l: TimelineRowLabel<TimeDataRecord> | undefined) => `Timeline Row ${l?.label || ''}`,
+        }}/>
     </VisXYContainer>
   </>
   )
