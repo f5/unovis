9050ba3e3ba1a45ec29524b966ca8d5837f23cc4
Nikita Rokotyan <nikita@exaforce.com>
2025-06-20 15:53:55 -0700

    Container | XY: Handle edge cases when `scaleByDomain` is `true`
    There could be situations when there are no data points, or only a single data point remains after `filterDataByRange` when calculating the domain. This was causing XY charts to go off-screen because the domain was calculated improperly.
    
    This change addresses the issue by including neighboring data points in the filtered results.

--- packages/ts/src/types/data.ts
+++ packages/ts/src/types/data.ts
@@ -3,3 +3,8 @@ export type GenericDataRecord = Record<string, unknown>
 
 /** Extension of a numbers array that carries additional information required for plotting stacked data */
 export type StackValuesRecord = Array<[number, number]> & { isMostlyNegative: boolean }
+
+export enum FindNearestDirection {
+  Left = 'left',
+  Right = 'right',
+}
