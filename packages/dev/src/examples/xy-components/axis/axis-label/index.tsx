d3468ccf70c3f9ba551ffef13aefd15a5c2808d6
Qian Liu <qi.liu@f5.com>
2025-07-28 16:22:03 -0700

    Dev | Axis: Adding long label with brush example

--- packages/dev/src/examples/xy-components/axis/axis-label/index.tsx
+++ packages/dev/src/examples/xy-components/axis/axis-label/index.tsx
@@ -19,20 +19,20 @@ export const component = (props: ExampleViewerDurationProps): React.ReactNode =>
     <>
       <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)} width={600}>
         <VisLine x={d => d.x} y={accessors} duration={props.duration}/>
-        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration} label='S little longer X Axis X Axis supe r super suepra lskdjgaljgal fjglaijhlaidaij ijsdjflak  sladkjfalfgjlfijg jhglijhlij' labelTextFitMode={FitMode.Trim} position='bottom' />
-        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} label='X Axis Xasdlfkjalsgjl alskdjfla sjgliajflijg alskdjflsajgldgjfldgj' labelTextFitMode={FitMode.Wrap} position='left' labelTextTrimType={TrimMode.Start} />
+        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration} label='When you have a really really long label, you can use the labelTextFitMode to trim/wrap the label' labelTextFitMode={FitMode.Trim} position='bottom' />
+        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} label='When you have a really really long label, you can use the labelTextFitMode to trim/wrap the label' labelTextFitMode={FitMode.Wrap} position='left' labelTextTrimType={TrimMode.Start} />
       </VisXYContainer>
 
       <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)} width={600}>
         <VisLine x={d => d.x} y={accessors} duration={props.duration}/>
-        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration} label='S little longer X Axis X Axis supe r super suepra lskdjgaljgal fjglaijhlaidaij ijsdjflak  sladkjfalfgjlfijg jhglijhlij' labelTextFitMode={FitMode.Wrap} position='bottom' />
-        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} label='X Axis Xasdlfkjalsgjl alskdjfla sjgliajflijg alskdjflsajgldgjfldgj' labelTextFitMode={FitMode.Wrap} position='right' labelTextTrimType={TrimMode.Middle} />
+        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration} label='When you have a really really long label, you can use the labelTextFitMode to trim/wrap the label' labelTextFitMode={FitMode.Wrap} position='bottom' />
+        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} label='When you have a really really long label, you can use the labelTextFitMode to trim/wrap the label' labelTextFitMode={FitMode.Wrap} position='right' labelTextTrimType={TrimMode.Middle} />
       </VisXYContainer>
 
       <VisXYContainer<XYDataRecord> data={generateXYDataRecords(15)} width={600}>
         <VisLine x={d => d.x} y={accessors} duration={props.duration}/>
-        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration} label='S little longer X Axis X Axis supe r super suepra lskdjgaljgal fjglaijhlaidaij ijsdjflak  sladkjfalfgjlfijg jhglijhlij' labelTextFitMode={FitMode.Wrap} position='top' labelFontSize={ '18px'} />
-        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} label='X Axis Xasdlfkjalsgjl alskdjfla sjgliajflijg alskdjflsajgldgjfldgj' labelTextFitMode={FitMode.Trim} position='right' labelColor={'red'} labelFontSize={'18px'} />
+        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration} label='When you have a really really long label, you can use the labelTextFitMode to trim/wrap the label' labelTextFitMode={FitMode.Wrap} position='top' labelFontSize={ '18px'} />
+        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} label='When you have a really really long label, you can use the labelTextFitMode to trim/wrap the label' labelTextFitMode={FitMode.Trim} position='right' labelColor={'red'} labelFontSize={'18px'} />
       </VisXYContainer>
     </>
   )
