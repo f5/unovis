6b0eaf5cbdfd865d3c09e42f0fd34a337ea2a10e
Nikita Rokotyan <nikita@exaforce.com>
2025-08-25 12:59:49 -0700

    Dev | Examples | Axis | Min Max Ticks Only: No grid line when too close

--- packages/dev/src/examples/xy-components/axis/axis-min-max-ticks-only/index.tsx
+++ packages/dev/src/examples/xy-components/axis/axis-min-max-ticks-only/index.tsx
@@ -25,15 +25,18 @@ export const component = (props: ExampleViewerDurationProps): React.ReactNode =>
         <VisAxis type='y' tickFormat={tickFormatter} duration={props.duration} minMaxTicksOnly={true} />
       </VisXYContainer>
 
-      <code>minMaxTicksOnlyShowGridLines: true</code>
+      <code><b>minMaxTicksOnlyShowGridLines: true</b></code>
       <VisXYContainer<XYDataRecord> data={data}>
         <VisLine x={d => d.x} y={accessors} duration={props.duration}/>
         <VisAxis type='x' tickFormat={tickFormatter} duration={props.duration} minMaxTicksOnly={true} minMaxTicksOnlyShowGridLines={true}/>
         <VisAxis type='y' tickFormat={tickFormatter} duration={props.duration} minMaxTicksOnly={true} minMaxTicksOnlyShowGridLines={true}/>
       </VisXYContainer>
 
-      <code>minMaxTicksOnlyShowGridLines: true. Custom `tickValues` for xAxis: [0, 2, 4, 6]</code>
-      <VisXYContainer<XYDataRecord> data={data}>
+      <code><b>minMaxTicksOnlyShowGridLines: true</b><br />
+      Custom tickValues for xAxis: [0, 2, 4, 6]. <br />
+      No grid line at 10.2, because it's too close to the previous grid line.
+      </code>
+      <VisXYContainer<XYDataRecord> data={data} yDomain={[0, 10.2]}>
         <VisLine x={d => d.x} y={accessors} duration={props.duration}/>
         <VisAxis
           type='x'
