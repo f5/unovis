e417520a39bb18cbdecc06380ea198e3eec0a49e
Qian Liu <qi.liu@f5.com>
2025-09-09 14:14:45 -0700

    Component | Axis: Fix autoMargin _getYTickTextTranslate undefined issue

--- packages/dev/src/examples/xy-components/grouped-bar/basic-grouped-bar/index.tsx
+++ packages/dev/src/examples/xy-components/grouped-bar/basic-grouped-bar/index.tsx
@@ -15,12 +15,24 @@ export const component = (props: ExampleViewerDurationProps): React.ReactNode =>
   ]
 
   return (
-    <VisXYContainer<XYDataRecord> ariaLabel='A simple example of a Grouped Bar chart' data={generateXYDataRecords(15)} margin={{ top: 5, left: 5 }} xDomain={[-1, 15]}>
-      <VisGroupedBar x={d => d.x} y={accessors} duration={props.duration}/>
-      <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration}/>
-      <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration}/>
-      <VisCrosshair template={(d: XYDataRecord) => `${d.x}`}/>
-      <VisTooltip ref={tooltipRef}/>
-    </VisXYContainer>
+    <>
+      <div>AutoMargin True</div>
+      <VisXYContainer<XYDataRecord> ariaLabel='A simple example of a Grouped Bar chart' data={generateXYDataRecords(15)} xDomain={[-1, 15]}>
+        <VisGroupedBar x={d => d.x} y={accessors} duration={props.duration}/>
+        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration}/>
+        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} />
+        <VisCrosshair template={(d: XYDataRecord) => `${d.x}`}/>
+        <VisTooltip ref={tooltipRef}/>
+      </VisXYContainer>
+
+      <div style={{ marginTop: 20 }}>AutoMargin False</div>
+      <VisXYContainer<XYDataRecord> ariaLabel='A simple example of a Grouped Bar chart' data={generateXYDataRecords(15)} margin={{ top: 5, right: 10, left: 30, bottom: 40 }} xDomain={[-1, 15]} autoMargin={false}>
+        <VisGroupedBar x={d => d.x} y={accessors} duration={props.duration}/>
+        <VisAxis type='x' numTicks={15} tickFormat={(x: number) => `${x}`} duration={props.duration}/>
+        <VisAxis type='y' tickFormat={(y: number) => `${y}`} duration={props.duration} position='left' numTicks={6} tickPadding={8} tickTextWidth={50} tickTextAngle={0} tickTextAlign='center' tickLine={false} gridLine={true} domainLine={true} />
+        <VisCrosshair template={(d: XYDataRecord) => `${d.x}`}/>
+        <VisTooltip ref={tooltipRef}/>
+      </VisXYContainer>
+    </>
   )
 }
