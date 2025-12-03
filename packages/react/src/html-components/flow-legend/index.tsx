13391625796d557030b60fa53c4fc072ccf7781d
Nikita Rokotyan <nikita@exaforce.com>
2025-12-03 15:30:36 -0800

    React: Add Flow Legend wrapper

--- packages/react/src/html-components/flow-legend/index.tsx
+++ packages/react/src/html-components/flow-legend/index.tsx
@@ -1,6 +1,6 @@
 // !!! This code was automatically generated. You should not change it !!!
 import React, { ForwardedRef, ReactElement, Ref, useImperativeHandle, useEffect, useRef, useState } from 'react'
-import { BulletLegend, BulletLegendConfigInterface } from '@unovis/ts'
+import { FlowLegend, FlowLegendConfigInterface } from '@unovis/ts'
 
 // Utils
 import { arePropsEqual } from 'src/utils/react'
@@ -8,27 +8,27 @@ import { arePropsEqual } from 'src/utils/react'
 // Types
 import { VisComponentElement } from 'src/types/dom'
 
-export type VisBulletLegendRef = {
-  component?: BulletLegend;
+export type VisFlowLegendRef = {
+  component?: FlowLegend;
 }
 
-export type VisBulletLegendProps = BulletLegendConfigInterface & {
-  ref?: Ref<VisBulletLegendRef>;
+export type VisFlowLegendProps = FlowLegendConfigInterface & {
+  ref?: Ref<VisFlowLegendRef>;
   className?: string;
 }
 
-export const VisBulletLegendSelectors = BulletLegend.selectors
+export const VisFlowLegendSelectors = FlowLegend.selectors
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-function VisBulletLegendFC (props: VisBulletLegendProps, fRef: ForwardedRef<VisBulletLegendRef>): ReactElement {
-  const ref = useRef<VisComponentElement<BulletLegend, HTMLDivElement>>(null)
-  const componentRef = useRef<BulletLegend | undefined>(undefined)
+function VisFlowLegendFC (props: VisFlowLegendProps, fRef: ForwardedRef<VisFlowLegendRef>): ReactElement {
+  const ref = useRef<VisComponentElement<FlowLegend, HTMLDivElement>>(null)
+  const componentRef = useRef<FlowLegend | undefined>(undefined)
 
   // On Mount
   useEffect(() => {
-    const element = (ref.current as VisComponentElement<BulletLegend, HTMLDivElement>)
+    const element = (ref.current as VisComponentElement<FlowLegend, HTMLDivElement>)
 
-    const c = new BulletLegend(ref.current as VisComponentElement<BulletLegend, HTMLDivElement>, { ...props, renderIntoProvidedDomNode: true })
+    const c = new FlowLegend(ref.current as VisComponentElement<FlowLegend, HTMLDivElement>, { ...props, renderIntoProvidedDomNode: true })
     componentRef.current = c
     element.__component__ = c
 
@@ -51,4 +51,4 @@ function VisBulletLegendFC (props: VisBulletLegendProps, fRef: ForwardedRef<VisB
 
 // We export a memoized component to avoid unnecessary re-renders
 //  and define its type explicitly to help react-docgen-typescript to extract information about props
-export const VisBulletLegend: ((props: VisBulletLegendProps) => JSX.Element | null) = React.memo(React.forwardRef(VisBulletLegendFC), arePropsEqual)
+export const VisFlowLegend: ((props: VisFlowLegendProps) => JSX.Element | null) = React.memo(React.forwardRef(VisFlowLegendFC), arePropsEqual)
