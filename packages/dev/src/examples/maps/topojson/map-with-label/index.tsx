73b38600fb240649b45dbfb45dcb39694cd4bd27
lee00678 <qi.liu@f5.com>
2025-12-08 11:58:30 -0800

    Dev | Example: Add example with area labels

---
+++ packages/dev/src/examples/maps/topojson/map-with-label/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { VisSingleContainer, VisTopoJSONMap } from '@unovis/react'
+import { TopoJSONMap } from '@unovis/ts'
+import { WorldMapTopoJSON } from '@unovis/ts/maps'
+
+export const title = 'World Map'
+export const subTitle = 'Default WorldMapTopoJSON'
+
+type AreaDatum = {
+  id: string;
+  age: number[];
+};
+
+export const component = (): React.ReactNode => {
+  const data = {
+    areas: [
+      'AW', 'AI', 'AS', 'AG', 'BM', 'BB', 'CK', 'CW', 'KY', 'FM', 'GG', 'GD', 'GU', 'HM', 'IM',
+      'CC', 'CX', 'IO', 'JE', 'KI', 'KN', 'MO', 'MC', 'MV', 'MH', 'MT', 'MP', 'MS', 'NF', 'NU',
+      'PN', 'PW', 'SH', 'SM', 'PM', 'SC', 'TC', 'TO', 'VC', 'VG', 'VI', 'WF',
+    ].map(c => ({ id: c })),
+  }
+
+  return (
+    <div>
+      <VisSingleContainer data={data} height={'90vh'}>
+        <VisTopoJSONMap
+          topojson={WorldMapTopoJSON}
+          duration={0}
+          areaColor='red'
+          areaLabel={(d: AreaDatum) => d?.id}
+          events={{
+            [TopoJSONMap.selectors.feature]: {
+              // click: c => console.log(c),
+            },
+          }}
+        />
+      </VisSingleContainer>
+    </div>
+  )
+}
