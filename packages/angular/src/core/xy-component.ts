461ef68d16a7798d332cc204eabd361fa246f178
Surya Teja Hanumandla <s.hanumandla@f5.com>
2026-01-20 19:24:14 +0530

    Misc | Angular: Upgrade angular to 16

--- packages/angular/src/core/xy-component.ts
+++ packages/angular/src/core/xy-component.ts
@@ -2,6 +2,6 @@ import { XYComponentCore, XYContainer } from '@unovis/ts'
 import { VisCoreComponent } from './core-component'
 
 export class VisXYComponent extends VisCoreComponent {
-  component: XYComponentCore<any>
-  componentContainer: XYContainer<any>
+  declare component: XYComponentCore<any>
+  declare componentContainer: XYContainer<any>
 }
