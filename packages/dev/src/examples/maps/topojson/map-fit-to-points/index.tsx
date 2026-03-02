b54b252ac022e6d37c9386c9940a38c51cf51ee8
lee00678 <qi.liu@f5.com>
2026-03-02 15:17:59 -0800

    Component | TopoJSON: FitToPoint doesn't handle single point, now it will center the single point

--- packages/dev/src/examples/maps/topojson/map-fit-to-points/index.tsx
+++ packages/dev/src/examples/maps/topojson/map-fit-to-points/index.tsx
@@ -7,6 +7,7 @@ export const subTitle = 'Adjust number of points to change view'
 
 export const component = (): React.ReactNode => {
   const points = [
+    { id: 'Los Angeles', latitude: 34.0522, longitude: -118.2437 },
     { id: 'Qormi', latitude: 35.8985, longitude: 14.4705 },
     { id: 'Syracuse', latitude: 37.0755, longitude: 15.2860 },
     { id: 'Tripoli', latitude: 32.8872, longitude: 13.1913 },
@@ -14,7 +15,6 @@ export const component = (): React.ReactNode => {
     { id: 'London', latitude: 51.5074, longitude: -0.1278 },
     { id: 'Reykjavik', latitude: 64.1265, longitude: -21.8174 },
     { id: 'New York', latitude: 40.7128, longitude: -74.0060 },
-    { id: 'Los Angeles', latitude: 34.0522, longitude: -118.2437 },
   ]
   const [data, setData] = useState<{ points: typeof points[0][] }>({ points: [] })
   const [numPoints, setNumPoints] = useState(5)
@@ -28,7 +28,7 @@ export const component = (): React.ReactNode => {
         <input
           type='range'
           step={1}
-          min={2}
+          min={1}
           max={points.length}
           value={numPoints}
           onChange={e => setNumPoints(Number(e.target.value))}
