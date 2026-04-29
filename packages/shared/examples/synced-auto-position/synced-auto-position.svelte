33e7b2c8a9d1f1d9412f25d1f31849306ef99968
Soryn <soryngitlan@gmail.com>
2026-04-29 19:56:02 +0300

    Dev | Examples | SyncedAutoPosition: Stress-test demo for plotline + plotband label collision

---
+++ packages/shared/examples/synced-auto-position/synced-auto-position.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { VisXYContainer, VisLine, VisAxis, VisPlotline } from '@unovis/svelte'
+  import { LabelOverflow } from '@unovis/ts'
+  import { data, thresholds, DataRecord } from './data'
+
+  const margin = { top: 10, right: 200, bottom: 30, left: 40 }
+  const x = (d: DataRecord): number => d.x
+  const y = (d: DataRecord): number => d.y
+</script>
+
+<VisXYContainer {data} height={400} {margin} yDomain={[5.0, 7.0]}>
+  <VisLine {x} {y}/>
+  <VisAxis type="x"/>
+  <VisAxis type="y"/>
+  {#each thresholds as t (t.value)}
+    <VisPlotline
+      value={t.value}
+      labelText={t.label}
+      labelPosition="top-right"
+      labelAutoPosition={true}
+      labelOverflow={LabelOverflow.Smart}
+      color="#666"
+      lineStyle="dash"
+    />
+  {/each}
+</VisXYContainer>
