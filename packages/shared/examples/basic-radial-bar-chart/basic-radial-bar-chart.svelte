a5c32b8527b68e23357a8c2b97aab506df2af453
Nikita Rokotyan <nikita@exaforce.com>
2026-05-19 16:13:05 -0700

    Website: Radial Bar Docs and Gallery example

---
+++ packages/shared/examples/basic-radial-bar-chart/basic-radial-bar-chart.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { VisSingleContainer, VisRadialBar, VisTooltip, VisBulletLegend } from '@unovis/svelte'
+  import { RadialBar } from '@unovis/ts'
+  import type { RadialBarArcDatum } from '@unovis/ts'
+  import { data, DataRecord, maxValue, completion } from './data'
+
+  const legendItems = data.map(d => ({ name: d.key }))
+
+  const triggers = {
+    [RadialBar.selectors.bar]: (d: RadialBarArcDatum<DataRecord>) => {
+      const max = maxValue[d.index] ?? '—'
+      return `<strong>${d.data.key}</strong><br/>${d.value} / ${max} ${d.data.unit}`
+    },
+  }
+</script>
+
+<VisBulletLegend items={legendItems}/>
+<VisSingleContainer height={400}>
+  <VisRadialBar
+    value={d => d.value}
+    {data}
+    maxValue={maxValue}
+    trackWidth={18}
+    trackPadding={4}
+    cornerRadius={9}
+    centralLabel={`${completion}%`}
+    centralSubLabel="daily goals"
+  />
+  <VisTooltip {triggers}/>
+</VisSingleContainer>
