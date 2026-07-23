2df083f1377b1d68b0c42c9c416beab11e5bd614
Surya Teja Hanumandla <s.hanumandla@f5.com>
2026-07-23 21:15:49 +0530

    Misc | Angular: Fix dual-axis-chart dev example

--- packages/shared/examples/dual-axis-chart/dual-axis-chart.component.ts
+++ packages/shared/examples/dual-axis-chart/dual-axis-chart.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core'
+import { Component, ViewEncapsulation } from '@angular/core'
 import { XYDataRecord, generateXYDataRecords } from './data'
 
 @Component({
   selector: 'dual-axis-chart',
   templateUrl: './dual-axis-chart.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styles: [`
+    dual-axis-chart .chart-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+    }
+  `],
 })
 export class DualAxisChartComponent {
   data = generateXYDataRecords(150)
 
   margin = { left: 100, right: 100, top: 40, bottom: 60 }
-  style = { position: 'absolute', top: 0, left: 0, width: '100%', height: '40vh' }
 
   chartX = (d: XYDataRecord): number => d.x
   chartAY = (d: XYDataRecord, i: number): number => i * (d.y || 0)
