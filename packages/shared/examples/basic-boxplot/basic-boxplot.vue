e26221f0760d5f402503a3031f2f5527716dd80e
Surya Teja Hanumandla <s.hanumandla@f5.com>
2026-07-23 21:24:30 +0530

    Misc | Vue: Fix basic boxplot dev example

--- packages/shared/examples/basic-boxplot/basic-boxplot.vue
+++ packages/shared/examples/basic-boxplot/basic-boxplot.vue
@@ -4,8 +4,8 @@ import { data } from './data'
 </script>
 
 <template>
-  <VisXYContainer :height="600">
-    <VisBoxplot :data :x="d => d.x" :median="d => d.median" :quartiles="d => d.quartiles" :whiskers="d => d.whiskers" />
+  <VisXYContainer :data :height="600">
+    <VisBoxplot :x="d => d.x" :median="d => d.median" :quartiles="d => d.quartiles" :whiskers="d => d.whiskers" />
     <VisAxis type="x" />
     <VisAxis type="y" />
   </VisXYContainer>
