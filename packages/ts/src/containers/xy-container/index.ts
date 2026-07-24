71edf508b41b05610c1dfe2396164d0f53b82d5f
Qian Liu <qi.liu@f5.com>
2026-07-24 10:21:29 -0700

    Misc | Dev: Fix getMargin when margin is not defined bug

--- packages/ts/src/containers/xy-container/index.ts
+++ packages/ts/src/containers/xy-container/index.ts
@@ -433,12 +433,14 @@ export class XYContainer<Datum> extends ContainerCore {
 
   private _getMargin (): Spacing {
     const { config: { margin } } = this
+    const defaultMargin = { top: 0, bottom: 0, left: 0, right: 0 }
+    const m = margin || defaultMargin
 
     return {
-      top: margin.top + this._axisMargin.top,
-      bottom: margin.bottom + this._axisMargin.bottom,
-      left: margin.left + this._axisMargin.left,
-      right: margin.right + this._axisMargin.right,
+      top: m.top + this._axisMargin.top,
+      bottom: m.bottom + this._axisMargin.bottom,
+      left: m.left + this._axisMargin.left,
+      right: m.right + this._axisMargin.right,
     }
   }
 
