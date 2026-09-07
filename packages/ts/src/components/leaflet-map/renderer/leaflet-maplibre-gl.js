c78544a87c9c8b8296edb8530e249fe5d3cec180
Surya Teja Hanumandla <s.hanumandla@f5.com>
2026-09-07 20:03:41 +0530

    Component | LeafletMap: Update maplibre-gl version to 6.7.0 to fix vulnerability issue CVE-2026-85061

--- packages/ts/src/components/leaflet-map/renderer/leaflet-maplibre-gl.js
+++ packages/ts/src/components/leaflet-map/renderer/leaflet-maplibre-gl.js
@@ -1,3 +1,8 @@
+// maplibre-gl >= 6 no longer exposes `Map#transform` publicly; it lives on the internal `_camera`
+function getGLTransform (gl) {
+  return gl.transform || gl._camera?.transform
+}
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function MaplibreGLLayer (leaflet, maplibre, options) {
   // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -116,9 +121,10 @@ export function MaplibreGLLayer (leaflet, maplibre, options) {
 
       this._glMap = new maplibre.Map(options)
 
-      // allow GL base map to pan beyond min/max latitudes
-      this._glMap.transform.latRange = null
-      this._glMap.transform.maxValidLatitude = Infinity
+      // allow GL base map to pan beyond min/max latitudes (not supported by all maplibre-gl versions)
+      const tr = getGLTransform(this._glMap)
+      try { tr.latRange = null } catch (e) { /* no-op */ }
+      try { tr.maxValidLatitude = Infinity } catch (e) { /* no-op */ }
 
       this._transformGL(this._glMap)
 
@@ -159,7 +165,8 @@ export function MaplibreGLLayer (leaflet, maplibre, options) {
 
       this._transformGL(gl)
 
-      if (gl.transform.width !== size.x || gl.transform.height !== size.y) {
+      const tr = getGLTransform(gl)
+      if (tr.width !== size.x || tr.height !== size.y) {
         container.style.width = `${size.x}px`
         container.style.height = `${size.y}px`
         if (gl._resize !== null && gl._resize !== undefined) {
@@ -183,9 +190,14 @@ export function MaplibreGLLayer (leaflet, maplibre, options) {
       // gl.setView([center.lat, center.lng], this._map.getZoom() - 1, 0);
       // calling setView directly causes sync issues because it uses requestAnimFrame
 
-      const tr = gl.transform
-      tr.center = maplibre.LngLat.convert([center.lng, center.lat])
-      tr.zoom = this._map.getZoom() - 1
+      const tr = getGLTransform(gl)
+      if (tr.setCenter) {
+        tr.setCenter(maplibre.LngLat.convert([center.lng, center.lat]))
+        tr.setZoom(this._map.getZoom() - 1)
+      } else {
+        tr.center = maplibre.LngLat.convert([center.lng, center.lat])
+        tr.zoom = this._map.getZoom() - 1
+      }
     },
 
     // update the map constantly during a pinch zoom
