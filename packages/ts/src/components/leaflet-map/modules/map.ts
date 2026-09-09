62b0331162e017cecdbf61f741cc3591ba3aba36
Surya Teja Hanumandla <s.hanumandla@f5.com>
2026-09-10 01:18:57 +0530

    Release: 1.4.3-beta.1

--- packages/ts/src/components/leaflet-map/modules/map.ts
+++ packages/ts/src/components/leaflet-map/modules/map.ts
@@ -134,9 +134,9 @@ export async function setupMap<T extends GenericDataRecord> (mapContainer: HTMLE
       //   (same query marker) to get it emitted next to the worker. Its URL is unused on purpose.
       if (!maplibre.getWorkerUrl()) {
         const workerUrl = new URL('maplibre-gl/dist/maplibre-gl-worker.mjs?maplibreWorkerAsset', import.meta.url)
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const sharedUrl = new URL('maplibre-gl/dist/maplibre-gl-shared.mjs?maplibreWorkerAsset', import.meta.url)
         maplibre.setWorkerUrl(workerUrl.toString())
+        sharedUrl.toString()
       }
       // eslint-disable-next-line no-case-declarations
       const { getMaplibreGLLayer } = await import('../renderer/mapboxgl-layer')
