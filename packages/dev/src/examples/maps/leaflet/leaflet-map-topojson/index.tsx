f4f8e6c450c370c898a8a8a3ecec0d184893b996
Surya Teja Hanumandla <s.hanumandla@f5.com>
2026-09-09 12:00:16 +0530

    Misc: Update leaflet map examples

---
+++ packages/dev/src/examples/maps/leaflet/leaflet-map-topojson/index.tsx
@@ -0,0 +1,67 @@
+import React, { useCallback } from 'react'
+import { VisLeafletMap } from '@unovis/react'
+import { WorldMapTopoJSON } from '@unovis/ts/maps'
+import { mapStyleDark, mapStyleLight } from '../leaflet-map-colorMap/data'
+
+export const title = 'TopoJSON Overlay'
+export const subTitle = 'WorldMapTopoJSON on top of a Leaflet map'
+
+
+export const component = (): React.ReactNode => {
+  const data = [
+    { id: 'Seattle', latitude: 47.6062, longitude: -122.3321 },
+    { id: 'Billerica', latitude: 42.5584, longitude: -71.2689 },
+    { id: 'Boulder', latitude: 40.0150, longitude: -105.2705 },
+    { id: 'San Jose', latitude: 37.7749, longitude: -122.4194 },
+    { id: 'Spokane', latitude: 47.6588, longitude: -117.4260 },
+    { id: 'Guadalajara', latitude: 20.6597, longitude: -103.3496 },
+    { id: 'Chertsey', latitude: 51.3883, longitude: -0.5071 },
+    { id: 'Warsaw', latitude: 52.2297, longitude: 21.0122 },
+    { id: 'Tel Aviv', latitude: 32.0853, longitude: 34.7818 },
+    { id: 'Beijing', latitude: 39.9042, longitude: 116.4074 },
+    { id: 'Hyderabad', latitude: 17.3850, longitude: 78.4867 },
+    { id: 'Singapore', latitude: 1.3521, longitude: 103.8198 },
+    { id: 'Sydney', latitude: -33.8688, longitude: 151.2093 },
+    { id: 'Tokyo', latitude: 35.6895, longitude: 139.6917 },
+  ]
+  type MapPointDataRecord = typeof data[0]
+  const pointId = (d: MapPointDataRecord): string => d.id
+  const pointLatitude = (d: MapPointDataRecord): number => d.latitude
+  const pointLongitude = (d: MapPointDataRecord): number => d.longitude
+  const pointLabel = (d: MapPointDataRecord): string => d.id
+  const highlightedCountries = WorldMapTopoJSON.objects.countries.geometries
+    .filter(c => ['US', 'MX', 'GB', 'CN', 'AU', 'IN', 'JP', 'IL', 'PL', 'SG'].includes(c.id as string))
+    .map(c => ({ ...c, properties: { color: '#98df8a' } }))
+
+  const topoJSONLayer = {
+    sources: {
+      type: 'Topology',
+      arcs: WorldMapTopoJSON.arcs,
+      transform: WorldMapTopoJSON.transform,
+      objects: {
+        countries: {
+          type: 'GeometryCollection',
+          geometries: highlightedCountries,
+        },
+      },
+    },
+    featureName: 'countries',
+    strokeProperty: 'color',
+  }
+
+  return (<>
+    <VisLeafletMap<MapPointDataRecord>
+      height={550}
+      data={data}
+      style={mapStyleLight}
+      styleDarkTheme={mapStyleDark}
+      pointId={useCallback(pointId, [])}
+      pointLatitude={useCallback(pointLatitude, [])}
+      pointLongitude={useCallback(pointLongitude, [])}
+      pointColor='#e21d38'
+      pointBottomLabel={pointLabel}
+      clusteringDistance={20}
+      topoJSONLayer={topoJSONLayer}
+    />
+  </>)
+}
--- packages/dev/src/examples/maps/leaflet/leaflet-map-vector/index.tsx
+++ packages/dev/src/examples/maps/leaflet/leaflet-map-vector/index.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react'
-import { VectorSourceSpecification } from 'maplibre-gl'
 import { VisLeafletMap, VisLeafletMapRef } from '@unovis/react'
-import { MapLibreArcticDark, MapLibreArcticLight } from '@unovis/ts'
+import { ExampleViewerDurationProps } from '@src/components/ExampleViewer/index'
 
 // Data
 import cities from './cities.json'
+import { mapKey } from '../constants'
 
 // Style
 import s from './style.module.css'
@@ -14,18 +14,9 @@ export const subTitle = 'Vector rendering with MapLibre'
 
 
 type MapPointDatum = typeof cities[0]
-export const component = (): JSX.Element => {
+export const component = (props: ExampleViewerDurationProps): React.ReactNode => {
   const mapRef = useRef<VisLeafletMapRef<MapPointDatum> | null>(null)
   const [isMapVisible, setMapVisible] = useState(true)
-  const mapSources = {
-    sources: {
-      openmaptiles: {
-        type: 'vector',
-        url: `${UNOVIS_MAP_TILE_SERVER_URL}/data/v3.json`,
-      } as VectorSourceSpecification,
-    },
-    glyphs: `${UNOVIS_MAP_TILE_SERVER_URL}/fonts/{fontstack}/{range}.pbf`,
-  }
 
   const handleClick = (): void => {
     setMapVisible(!isMapVisible)
@@ -39,10 +30,13 @@ export const component = (): JSX.Element => {
       data={cities}
       pointColor={'#3556FF'}
       pointBottomLabel={d => d.city}
-      style={{ ...MapLibreArcticLight, ...mapSources }}
-      styleDarkTheme={{ ...MapLibreArcticDark, ...mapSources }}
+      style={`https://api.maptiler.com/maps/basic-v2-light/style.json?key=${mapKey}`}
+      styleDarkTheme={`https://api.maptiler.com/maps/basic-v2-dark/style.json?key=${mapKey}`}
+      duration={props.duration}
+      flyToDuration={props.duration}
+      zoomDuration={props.duration}
       attribution={[
-        '<a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>',
+        '<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>',
         '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>',
       ]}
     /> : null }
